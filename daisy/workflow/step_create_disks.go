b402c36fb88c024c782f781f81f4f310c0831e02
Andrew Jackura <ajackura@google.com>
2017-03-29 10:32:30 -0700

    fix typo

--- daisy/workflow/step_create_disks.go
+++ daisy/workflow/step_create_disks.go
@@ -77,7 +77,7 @@ func (c *CreateDisks) run(w *Workflow) error {
 			var imageLink string
 			var image *resource
 			var err error
-			if cd.SourceImage == "" || sLink(cd.SourceImage) {
+			if cd.SourceImage == "" || isLink(cd.SourceImage) {
 				imageLink = cd.SourceImage
 			} else if image, err = w.getImage(cd.SourceImage); err == nil {
 				imageLink = image.link
