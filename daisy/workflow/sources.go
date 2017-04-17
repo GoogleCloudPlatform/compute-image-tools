1ead7b230f14c5ee8d78caeef244ba6c69bda50f
Andrew Jackura <ajackura@google.com>
2017-04-17 10:09:59 -0700

    ensure gcs objects don't contain '\'

--- daisy/workflow/sources.go
+++ daisy/workflow/sources.go
@@ -37,7 +37,7 @@ func (w *Workflow) sourceExists(s string) bool {
 }
 
 func (w *Workflow) uploadFile(src, obj string) error {
-	dstPath := w.StorageClient.Bucket(w.bucket).Object(path.Join(w.sourcesPath, obj))
+	dstPath := w.StorageClient.Bucket(w.bucket).Object(path.Join(w.sourcesPath, filepath.ToSlash(obj)))
 	gcs := dstPath.NewWriter(w.Ctx)
 	f, err := os.Open(src)
 	if err != nil {
