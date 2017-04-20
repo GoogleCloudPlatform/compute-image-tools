078de80740c270f6919079f2f990ab056ea3a5b2
Scott Crunkleton <crunk1@gmail.com>
2017-04-20 12:35:19 -0700

    Fixed SubWorkflow cleanup issue. (#36)
    SubWorkflows weren't cleaning up if they were running when the root workflow exits from another step failing. Also was happening if the SubWorkflow step times out.
    Added a redundant backstop Subworkflow cleanup() call in the parent workflows' cleanup().
    
    Also, fixed TestUploadSources on Windows.

--- daisy/workflow/sources_test.go
+++ daisy/workflow/sources_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"path/filepath"
 	"reflect"
+	"runtime"
 	"strings"
 	"testing"
 )
@@ -29,6 +30,10 @@ func TestUploadSources(t *testing.T) {
 		t.Fatal(err)
 	}
 
+	localDNEErr := "stat this/file/dne: no such file or directory"
+	if runtime.GOOS == "windows" {
+		localDNEErr = "GetFileAttributesEx this\\file\\dne: The system cannot find the path specified."
+	}
 	tests := []struct {
 		desc    string
 		sources map[string]string
@@ -39,7 +44,7 @@ func TestUploadSources(t *testing.T) {
 		{"normal local folder to GCS", map[string]string{"local": dir}, "", []string{w.sourcesPath + "/local/test"}},
 		{"normal GCS obj to GCS", map[string]string{"gcs": "gs://gcs/file"}, "", []string{w.sourcesPath + "/gcs"}},
 		{"normal GCS bkt to GCS", map[string]string{"gcs": "gs://gcs/folder/"}, "", []string{w.sourcesPath + "/gcs/object", w.sourcesPath + "/gcs/folder/object"}},
-		{"dne local path", map[string]string{"local": "./this/file/dne"}, "stat this/file/dne: no such file or directory", nil},
+		{"dne local path", map[string]string{"local": "./this/file/dne"}, localDNEErr, nil},
 		{"dne GCS path", map[string]string{"gcs": "gs://gcs/path/dne"}, `error copying from file gs://gcs/path/dne: googleapi: got HTTP response code 404 with body: storage: object doesn't exist`, nil},
 		{"GCS path, no object", map[string]string{"gcs": "gs://folder"}, "", []string{w.sourcesPath + "/gcs/object", w.sourcesPath + "/gcs/folder/object"}},
 	}
