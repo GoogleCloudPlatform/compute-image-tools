bd0dbacf796d3cde75132ab10dbffcc33da36ddf
Andrew Jackura <ajackura@google.com>
2017-05-11 17:42:51 -0700

    Add more tests to compute.go (#52)

--- daisy/compute/compute_test.go
+++ daisy/compute/compute_test.go
@@ -22,9 +22,9 @@ import (
 	"net/http/httptest"
 	"reflect"
 	"testing"
-
 	"time"
 
+	"github.com/kylelemons/godebug/pretty"
 	compute "google.golang.org/api/compute/v1"
 	"google.golang.org/api/option"
 )
@@ -264,5 +264,159 @@ func TestWaitForInstanceStopped(t *testing.T) {
 
 	if err := c.WaitForInstanceStopped(testProject, testZone, testInstance, 5*time.Second); err != nil {
 		t.Fatalf("error running WaitForInstanceStopped: %v", err)
+	}
+}
+
+func testCreateInstance(got, want *compute.Instance) error {
+	if got.Name != want.Name {
+		return fmt.Errorf("unexpected Name, got: %s, want: %s", got.Name, want.Name)
+	}
+	if got.MachineType != want.MachineType {
+		return fmt.Errorf("unexpected MachineType, got: %s, want: %s", got.MachineType, want.MachineType)
+	}
+	if diff := pretty.Compare(got.Disks, want.Disks); diff != "" {
+		return fmt.Errorf("Disks do not match expectation: (-got +want)\n%s", diff)
+	}
+	if diff := pretty.Compare(got.Metadata, want.Metadata); diff != "" {
+		return fmt.Errorf("Metadata does not match expectation: (-got +want)\n%s", diff)
+	}
+	if diff := pretty.Compare(got.NetworkInterfaces, want.NetworkInterfaces); diff != "" {
+		return fmt.Errorf("NetworkInterfaces do not match expectation: (-got +want)\n%s", diff)
+	}
+	if diff := pretty.Compare(got.ServiceAccounts, want.ServiceAccounts); diff != "" {
+		return fmt.Errorf("ServiceAccounts do not match expectation: (-got +want)\n%s", diff)
+	}
+	return nil
+}
+
+func TestCreateInstance(t *testing.T) {
+	var body string
+	svr, c, err := newTestClient(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "POST" && r.URL.String() == fmt.Sprintf("/%s/zones/%s/instances?alt=json", testProject, testZone) {
+			buf := new(bytes.Buffer)
+			if _, err := buf.ReadFrom(r.Body); err != nil {
+				t.Fatal(err)
+			}
+			body = buf.String()
+			fmt.Fprint(w, `{}`)
+		} else if r.Method == "GET" && r.URL.String() == fmt.Sprintf("/%s/zones/%s/operations/?alt=json", testProject, testZone) {
+			fmt.Fprint(w, `{"Status":"DONE"}`)
+		} else if r.Method == "GET" && r.URL.String() == fmt.Sprintf("/%s/zones/%s/machineTypes?alt=json", testProject, testZone) {
+			fmt.Fprint(w, `{"Items":[{"Name":"machine-type"}]}`)
+		} else if r.Method == "GET" && r.URL.String() == fmt.Sprintf("/%s/zones/%s/instances/%s?alt=json", testProject, testZone, testInstance) {
+			fmt.Fprint(w, body)
+		} else {
+			w.WriteHeader(500)
+			fmt.Fprintln(w, "URL and Method not recognized:", r.Method, r.URL)
+		}
+	}))
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer svr.Close()
+
+	instance, err := c.NewInstance(testInstance, testProject, testZone, "machine-type")
+	if err != nil {
+		t.Fatalf("error running NewInstance: %v", err)
+	}
+
+	instance.AddPD(testDisk, "source", false, true)
+	instance.AddNetworkInterface("default")
+	instance.AddNetworkInterfaceWithSubnetwork("default", "default")
+	value := "value"
+	instance.AddMetadata(map[string]string{"key": value})
+	instance.AddMetadata(map[string]string{"key": value})
+
+	got, err := instance.Insert()
+	if err != nil {
+		t.Errorf("error running instance.Insert(): %v", err)
+	}
+	want := &compute.Instance{
+		Name: testInstance,
+		Disks: []*compute.AttachedDisk{
+			{
+				AutoDelete: false,
+				Boot:       true,
+				DeviceName: testDisk,
+				Source:     "source",
+				Mode:       "READ_WRITE",
+				Type:       "PERSISTENT",
+			},
+		},
+		NetworkInterfaces: []*compute.NetworkInterface{
+			{
+				AccessConfigs: []*compute.AccessConfig{
+					{
+						Type: "ONE_TO_ONE_NAT",
+					},
+				},
+				Network: "default",
+			},
+			{
+				AccessConfigs: []*compute.AccessConfig{
+					{
+						Type: "ONE_TO_ONE_NAT",
+					},
+				},
+				Network:    "default",
+				Subnetwork: "default",
+			},
+		},
+		ServiceAccounts: []*compute.ServiceAccount{{
+			Email:  "default",
+			Scopes: []string{"https://www.googleapis.com/auth/devstorage.read_only"},
+		}},
+		Metadata:    &compute.Metadata{Items: []*compute.MetadataItems{{Key: "key", Value: &value}, {Key: "key", Value: &value}}},
+		MachineType: fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/zones/%s/machineTypes/machine-type", testProject, testZone),
+	}
+	if err := testCreateInstance(got, want); err != nil {
+		t.Error(err)
+	}
+
+	instance, err = c.NewInstance(testInstance, testProject, testZone, "")
+	if err != nil {
+		t.Errorf("error running NewInstance: %v", err)
+	}
+	got, err = instance.Insert()
+	if err != nil {
+		t.Errorf("error running instance.Insert(): %v", err)
+	}
+	want = &compute.Instance{
+		Name: testInstance,
+		ServiceAccounts: []*compute.ServiceAccount{{
+			Email:  "default",
+			Scopes: []string{"https://www.googleapis.com/auth/devstorage.read_only"},
+		}},
+		MachineType: fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/zones/%s/machineTypes/n1-standard-1", testProject, testZone),
+	}
+	if err := testCreateInstance(got, want); err != nil {
+		t.Error(err)
+	}
+
+	instance, err = c.NewInstance(testInstance, testProject, testZone, "")
+	if err != nil {
+		t.Errorf("error running NewInstance: %v", err)
+	}
+	instance.Scopes = []string{"https://www.googleapis.com/auth/devstorage.full"}
+	got, err = instance.Insert()
+	if err != nil {
+		t.Errorf("error running instance.Insert(): %v", err)
+	}
+	want = &compute.Instance{
+		Name: testInstance,
+		ServiceAccounts: []*compute.ServiceAccount{{
+			Email:  "default",
+			Scopes: []string{"https://www.googleapis.com/auth/devstorage.full"},
+		}},
+		MachineType: fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/zones/%s/machineTypes/n1-standard-1", testProject, testZone),
+	}
+	if err := testCreateInstance(got, want); err != nil {
+		t.Error(err)
+	}
+
+	// Bad machine type.
+	wantErr := "unknown machine type: dne, project: test-project, zone: test-zone"
+	if _, gotErr := c.NewInstance(testInstance, testProject, testZone, "dne"); gotErr.Error() != wantErr {
+		t.Errorf("did not get expected error, got: %q, want: %q", gotErr, wantErr)
 	}
 }
