02a2f6d20f5e506aad61c3a63aef3d3d88788260
Andrew Jackura <ajackura@google.com>
2017-08-04 15:59:50 -0700

    fix test

--- daisy/workflow_test.go
+++ daisy/workflow_test.go
@@ -861,12 +861,12 @@ func TestWrite(t *testing.T) {
 func TestRunStepTimeout(t *testing.T) {
 	w := testWorkflow()
 	s, _ := w.NewStep("test")
-	s.timeout = 1 * time.Microsecond
+	s.timeout = 1 * time.Nanosecond
 	s.testType = &mockStep{runImpl: func(ctx context.Context, s *Step) error {
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(1 * time.Second)
 		return nil
 	}}
-	want := `step "test" did not stop in specified timeout of 1µs`
+	want := `step "test" did not stop in specified timeout of 1ns`
 	if err := w.runStep(context.Background(), s); err == nil || err.Error() != want {
 		t.Errorf("did not get expected error, got: %q, want: %q", err.Error(), want)
 	}
