d61a8cdf22224d9e4aa416cbb246656d92be3d36
Helen Koike <helen.koike@collabora.com>
2017-09-03 05:08:51 -0300

    translate_el: use python-guestfs
    This tool should mount the disk independently if LVM is being used or if
    /boot is in a separate partition and perform customizations in the VM
    image.
    
    Also, I tested with guestmount tool, but it seems that running
    grub2-mkconfig through chroot makes the roofs to point to /dev/fuse
    instead of the disk, so a full virtualisation should be used. One of the
    options is to use virt-customize, but I find using python-guest to be
    better, since it is not bash, we can handle errors better and have more
    interoperability.

---
+++ daisy_workflows/image_import/enterprise_linux/translate_el.py
@@ -0,0 +1,185 @@
+#!/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import guestfs
+import pycurl
+import sys
+import trace
+from StringIO import StringIO
+
+
+disk = '/dev/sdb'
+
+repo_compute = '''
+[google-cloud-compute]
+name=Google Cloud Compute
+baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-compute-el%s-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+'''
+
+repo_sdk = '''
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el%s-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+'''
+
+ifcfg_eth0 = '''
+BOOTPROTO=dhcp
+DEVICE=eth0
+ONBOOT=yes
+TYPE=Ethernet
+DEFROUTE=yes
+PEERDNS=yes
+PEERROUTES=yes
+DHCP_HOSTNAME=localhost
+IPV4_FAILURE_FATAL=no
+NAME="System eth0"
+MTU=1460
+PERSISTENT_DHCLIENT="y"
+'''
+
+grub_cfg = '''
+GRUB_TIMEOUT=0
+GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
+GRUB_DEFAULT=saved
+GRUB_DISABLE_SUBMENU=true
+GRUB_TERMINAL="serial console"
+GRUB_SERIAL_COMMAND="serial --speed=38400"
+GRUB_CMDLINE_LINUX="crashkernel=auto console=ttyS0,38400n8"
+GRUB_DISABLE_RECOVERY="true"
+'''
+
+def translate():
+  s = StringIO()
+  c = pycurl.Curl()
+  c.setopt(pycurl.HTTPHEADER, ['Metadata-Flavor:Google'])
+  c.setopt(c.WRITEFUNCTION, s.write)
+  url='http://metadata/computeMetadata/v1/instance/attributes'
+
+  c.setopt(pycurl.URL, url + '/el_release')
+  c.perform()
+  el_release = s.getvalue()
+  s.truncate(0)
+
+  c.setopt(pycurl.URL, url + '/install_gce_packages')
+  c.perform()
+  install_gce = s.getvalue()
+  s.truncate(0)
+
+  c.setopt(pycurl.URL, url + '/use_rhel_gce_license')
+  c.perform()
+  rhel_license = s.getvalue()
+
+  c.close()
+
+  # All new Python code should pass python_return_dict=True
+  # to the constructor.  It indicates that your program wants
+  # to receive Python dicts for methods in the API that return
+  # hashtables.
+  g = guestfs.GuestFS(python_return_dict=True)
+  g.set_verbose(1)
+  g.set_trace(1)
+
+  # Enable network
+  g.set_network(True)
+
+  # Attach the disk image to libguestfs.
+  g.add_drive_opts(disk)
+
+  # Run the libguestfs back-end.
+  g.launch()
+
+  # Ask libguestfs to inspect for operating systems.
+  roots = g.inspect_os()
+  if len(roots) == 0:
+    raise(Error('inspect_vm: no operating systems found'))
+
+  # Sort keys by length, shortest first, so that we end up
+  # mounting the filesystems in the correct order.
+  mps = g.inspect_get_mountpoints(roots[0])
+  def compare(a, b):
+    return len(a) - len(b)
+
+  for device in sorted(mps.keys(), compare):
+    try:
+      g.mount(mps[device], device)
+    except RuntimeError as msg:
+      print '%s (ignored)' % msg
+
+  # Setup DNS for chroot.
+  g.rm('/etc/resolv.conf')
+  g.upload('/etc/resolv.conf', '/etc/resolv.conf')
+
+  if rhel_license == 'true':
+    if 'Red Hat' in g.cat('/etc/redhat-release'):
+      g.command([
+          'yum', 'install', '-y', '--downloadonly', '--downloaddir=/tmp',
+          'google-rhui-client-rhel%s' % el_release])
+      g.command(['yum', 'remove', '-y', '*rhui*'])
+      print 'Adding in GCE RHUI package.'
+      g.command([
+          'yum', 'install', '-y',
+          '/tmp/google-rhui-client-rhel%s*.rpm' % el_release])
+
+  if install_gce == 'true':
+    print 'Installing GCE packages.'
+    g.write('/etc/yum.repos.d/google-cloud.repo', repo_compute % el_release)
+    if el_release == 7:
+      g.write_append(
+          '/etc/yum.repos.d/google-cloud.repo', repo_sdk % el_release)
+      g.command(['yum', '-y', 'install', 'google-cloud-sdk'])
+    g.command([
+        'yum', '-y', 'install', 'google-compute-engine',
+        'python-google-compute-engine'])
+
+  print 'Updating initramfs'
+  for kver in g.ls('/lib/modules'):
+    g.command(['dracut', '-v', '-f', '--kver', kver])
+
+  print 'Update grub configuration'
+  g.write('/etc/default/grub', grub_cfg)
+  g.command(['grub2-mkconfig', '-o', '/boot/grub2/grub.cfg'])
+
+  # Reset network for DHCP.
+  print 'Resetting network to DHCP for eth0.'
+  g.write('/etc/sysconfig/network-scripts/ifcfg-eth0', ifcfg_eth0)
+
+  # Remove SSH host keys.
+  print "Removing SSH host keys."
+  g.sh("rm -f /etc/ssh/ssh_host_*")
+
+  g.umount_all()
+
+def main():
+  try:
+    translate()
+    print 'TranslateSuccess: Translation finished.'
+  except Exception as e:
+    print 'TranslateFailed: error: '
+    print str(e)
+
+if __name__=='__main__':
+  tracer = trace.Trace(
+      ignoredirs=[sys.prefix, sys.exec_prefix], trace=1, count=0)
+  tracer.run('main()')
--- daisy_workflows/image_import/enterprise_linux/translate_el.sh
+++ daisy_workflows/image_import/enterprise_linux/translate_el.sh
@@ -15,131 +15,13 @@
 
 set -x
 
+# Download translate_el.py
 URL="http://metadata/computeMetadata/v1/instance/attributes"
-EL_RELEASE="$(curl -f -H Metadata-Flavor:Google ${URL}/el_release)"
-INSTALL_GCE="$(curl -f -H Metadata-Flavor:Google ${URL}/install_gce_packages)"
-RHEL_LICENSE="$(curl -f -H Metadata-Flavor:Google ${URL}/use_rhel_gce_license)"
-MNT="/mnt/imported_disk"
+GCS_SOURCE_PATH="$(curl -f -H Metadata-Flavor:Google ${URL}/daisy-sources-path)"
+gsutil cp "${GCS_SOURCE_PATH}/translate_el.py" /tmp/translate_el.py
 
-# Mount the imported disk.
-mkdir ${MNT}
-if [ -b /dev/sdb1 ]; then
-  echo "Trying to mount /dev/sdb1"
-  mount /dev/sdb1 ${MNT}
-  if [ $? -ne 0 ]; then
-    echo "TranslateFailed: Unable to mount imported disk."
-    exit 1
-  fi
-else
-  echo "Trying to mount /dev/sdb"
-  mount /dev/sdb ${MNT}
-  if [ $? -ne 0 ]; then
-    echo "TranslateFailed: Unable to mount imported disk."
-    exit 1
-  fi
-fi
+# Install dependencies
+DEBIAN_FRONTEND=noninteractive apt-get -y install python-guestfs python-pycurl
 
-# Setup DNS for chroot.
-for f in proc sys dev run; do
-  mount -o bind /$f ${MNT}/$f
-done
-mount -o bind /dev/pts ${MNT}/dev/pts
-cp /etc/resolv.conf ${MNT}/etc/resolv.conf
-chroot ${MNT} restorecon /etc/resolv.conf
-
-if [[ "${RHEL_LICENSE}" == "true" ]]; then
-  if $(grep -q "Red Hat" ${MNT}/etc/redhat-release); then
-    # Remove rhui packages and add the google rhui package.
-    chroot yum install -y --downloadonly --downloaddir=/tmp google-rhui-client-rhel${EL_RELEASE}
-    if [ $? -eq 0 ]; then
-      chroot ${MNT} yum remove -y *rhui*
-      echo "Adding in GCE RHUI package."
-      chroot ${MNT} yum install -y /tmp/google-rhui-client-rhel${EL_RELEASE}*.rpm
-    fi
-  fi
-fi
-
-# Install GCE packages if requested.
-if [[ "${INSTALL_GCE}" == "true" ]]; then
-  echo "Installing GCE packages."
-  cat > ${MNT}/etc/yum.repos.d/google-cloud.repo << EOM
-[google-cloud-compute]
-name=Google Cloud Compute
-baseurl=https://packages.cloud.google.com/yum/repos/google-cloud-compute-el${EL_RELEASE}-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
-
-  if [[ ${EL_RELEASE} == "7" ]]; then
-    cat >> ${MNT}/etc/yum.repos.d/google-cloud.repo << EOM
-[google-cloud-sdk]
-name=Google Cloud SDK
-baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el${EL_RELEASE}-x86_64
-enabled=1
-gpgcheck=1
-repo_gpgcheck=1
-gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
-       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
-EOM
-  chroot ${MNT} yum -y install google-cloud-sdk
-fi
-
-  chroot ${MNT} yum -y install google-compute-engine python-google-compute-engine
-  if [ $? -ne 0 ]; then
-    echo "TranslateFailed: GCE package install failed."
-    exit 1
-  fi
-fi
-
-if [[ ${EL_RELEASE} == "7" ]]; then
-  # Update grub config to log to console and disable menu
-  cat > ${MNT}/etc/default/grub <<- "EOM"
-GRUB_TIMEOUT=0
-GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
-GRUB_DEFAULT=saved
-GRUB_DISABLE_SUBMENU=true
-GRUB_TERMINAL="serial console"
-GRUB_SERIAL_COMMAND="serial --speed=38400"
-GRUB_CMDLINE_LINUX="crashkernel=auto console=ttyS0,38400n8"
-GRUB_DISABLE_RECOVERY="true"
-EOM
-  chroot ${MNT} restorecon /etc/default/grub
-  chroot ${MNT} grub2-mkconfig -o /boot/grub2/grub.cfg
-  if [ $? -ne 0 ]; then
-    echo "TranslateFailed: Grub update failed."
-    exit 1
-  fi
-fi
-
-# Reset network for DHCP.
-echo "Resetting network to DHCP for eth0."
-cat > ${MNT}/etc/sysconfig/network-scripts/ifcfg-eth0 <<EOF
-BOOTPROTO=dhcp
-DEVICE=eth0
-ONBOOT=yes
-TYPE=Ethernet
-DEFROUTE=yes
-PEERDNS=yes
-PEERROUTES=yes
-DHCP_HOSTNAME=localhost
-IPV4_FAILURE_FATAL=no
-NAME="System eth0"
-MTU=1460
-PERSISTENT_DHCLIENT="y"
-EOF
-chroot ${MNT} restorecon /etc/sysconfig/network-scripts/ifcfg-eth0
-
-# Remove SSH host keys.
-echo "Removing SSH host keys."
-rm -f ${MNT}/etc/ssh/ssh_host_*
-
-umount -l ${MNT}/dev/pts
-for f in proc sys pts dev run; do
-  umount -l ${MNT}/$f
-done
-umount -l ${MNT}
-
-echo "TranslateSuccess: Translation finished."
+# Customize image
+python /tmp/translate_el.py
