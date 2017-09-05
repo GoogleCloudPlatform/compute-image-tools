70bc67a79c36fdcf61c9fe8079a989118fcbbc35
Scott Crunkleton <crunk1@gmail.com>
2017-09-05 16:47:28 -0700

    Setting up Prow configuration and prowjob container images. Also, some basic functional tests (not currently being run). (#138)
    * Setting up Prow configuration and prowjob container image.
    
    * Updated logging in test modules.

---
+++ daisy/prowjob/run/e2e_tests/__main__.py
@@ -0,0 +1,41 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+import sys
+
+from run import logging
+
+ARGS = []
+
+
+def main():
+    logging.info('Got --tests=%s', ARGS.tests)
+    return 0
+
+
+def parse_args(arguments=None):
+    """Parse arguments or sys.argv[1:]."""
+    p = argparse.ArgumentParser()
+    p.add_argument('--tests', required=True, help="The tests to run.")
+
+    args = p.parse_args(arguments)
+    return args
+
+if __name__ == '__main__':
+    ARGS = parse_args()
+    return_code = main()
+    logging.info('main() returned with code %s', return_code)
+    logging.shutdown()
+    sys.exit(return_code)
