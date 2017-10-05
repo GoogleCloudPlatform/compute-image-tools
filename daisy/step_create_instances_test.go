2cb6418855dc76edd9bf5e94071e80ed012218a1
Andrew Jackura <ajackura@google.com>
2017-10-05 11:00:58 -0700

    fix

--- daisy/step_create_instances_test.go
+++ daisy/step_create_instances_test.go
@@ -62,12 +62,12 @@ func TestLogSerialOutput(t *testing.T) {
 		test, want, name string
 	}{
 		{
-			"dError but instance stopped",
+			"Error but instance stopped",
 			"CreateInstances: streaming instance \"i1\" serial port 0 output to gs://test-bucket/i1-serial-port0.log\n",
 			"i1",
 		},
 		{
-			"dError but instance running",
+			"Error but instance running",
 			"CreateInstances: streaming instance \"i2\" serial port 0 output to gs://test-bucket/i2-serial-port0.log\nCreateInstances: instance \"i2\": error getting serial port: fail\n",
 			"i2",
 		},
@@ -77,7 +77,7 @@ func TestLogSerialOutput(t *testing.T) {
 			"i3",
 		},
 		{
-			"dError but instance deleted",
+			"Error but instance deleted",
 			"CreateInstances: streaming instance \"i4\" serial port 0 output to gs://test-bucket/i4-serial-port0.log\n",
 			"i4",
 		},
