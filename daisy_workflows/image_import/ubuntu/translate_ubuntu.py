2e2f12671dff4eb73fdd6f7da6026a6649ad5146
Zach Marano <zmarano@google.com>
2017-10-23 14:38:44 -0700

    Allow for unmount to fail. It fails in 14.04 for one case that doesn't matter.

--- daisy_workflows/image_import/ubuntu/translate_ubuntu.py
+++ daisy_workflows/image_import/ubuntu/translate_ubuntu.py
@@ -150,7 +150,11 @@ def translate():
   print 'Removing SSH host keys.'
   g.sh('rm -f /etc/ssh/ssh_host_*')
 
-  g.umount_all()
+  try:
+    g.umount_all()
+  except Exception as e:
+    print str(e)
+    print 'Unmount failed. Continuing anyway.'
 
 def main():
   try:
