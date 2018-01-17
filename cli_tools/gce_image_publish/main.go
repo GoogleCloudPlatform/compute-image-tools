7c99da6764ddef6eaba9d7819caf4734c7e08e32
Andrew Jackura <ajackura@google.com>
2018-01-17 13:37:09 -0800

    address comments

--- cli_tools/gce_image_publish/main.go
+++ cli_tools/gce_image_publish/main.go
@@ -280,6 +280,7 @@ func populateSteps(w *daisy.Workflow, prefix string, createImages *daisy.CreateI
 	if deprecateStep != nil && createStep != nil {
 		w.AddDependency(deprecateStep, createStep)
 	}
+
 	// Create before delete on publish.
 	if deleteStep != nil && createStep != nil {
 		w.AddDependency(deleteStep, createStep)
