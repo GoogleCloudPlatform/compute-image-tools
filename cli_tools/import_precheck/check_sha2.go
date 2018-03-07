ce1f85accfd426ce3e174cc84ad9d0ec4fe95dd7
Andrew Jackura <ajackura@google.com>
2018-03-07 10:34:30 -0800

    gce_inventory_agent: make gce_inventory_agent a service (#356)
    Make service code a common library, rename packages folder to package_library

--- cli_tools/import_precheck/check_sha2.go
+++ cli_tools/import_precheck/check_sha2.go
@@ -17,7 +17,7 @@ import (
 	"runtime"
 	"strings"
 
-	"github.com/GoogleCloudPlatform/compute-image-tools/packages"
+	"github.com/GoogleCloudPlatform/compute-image-tools/package_library"
 )
 
 const sha2Windows2008R2KB = "KB3033929"
