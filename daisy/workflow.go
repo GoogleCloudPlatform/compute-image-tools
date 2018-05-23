GoogleCloudPlatform/compute-image-tools

commit c64f6b4546121c189cf4e3c186c95a186d0ae2c1

Daisy: Log serial output to cloud logging (#403)
Clean up log text
Make GCS links clickable instead of using the gs:// path

daisy/workflow.go
@@ -202,13 +202,15 @@ func (w *Workflow) Run(ctx context.Context) error {
 		return err
 	}
 	defer w.cleanup()
-	w.Logger.WorkflowInfo(w, "Using the GCS path gs://%s", path.Join(w.bucket, w.scratchPath))
+	w.Logger.WorkflowInfo(w, "Daisy scratch path: https://console.cloud.google.com/storage/browser/%s", path.Join(w.bucket, w.scratchPath))
 
-	w.Logger.WorkflowInfo(w, "Uploading sources")
-	if err := w.uploadSources(ctx); err != nil {
-		w.Logger.WorkflowInfo(w, "Error uploading sources: %v", err)
-		close(w.Cancel)
-		return err
+	if len(w.Sources) != 0 {
+		w.Logger.WorkflowInfo(w, "Uploading sources")
+		if err := w.uploadSources(ctx); err != nil {
+			w.Logger.WorkflowInfo(w, "Error uploading sources: %v", err)
+			close(w.Cancel)
+			return err
+		}
 	}
 	w.Logger.WorkflowInfo(w, "Running workflow")
 	if err := w.run(ctx); err != nil {

daisy/logger.go
@@ -75,6 +75,7 @@ func (l *daisyLog) StepInfo(w *Workflow, stepName, stepType, format string, a ..
 		StepName:       stepName,
 		StepType:       stepType,
 		Message:        fmt.Sprintf(format, a...),
+		Type:           "Daisy",
 	}
 	l.writeLogEntry(entry)
 }
@@ -107,11 +108,12 @@ type logEntry struct {
 	StepName       string    `json:"stepName,omitempty"`
 	StepType       string    `json:"stepType,omitempty"`
 	Message        string    `json:"message"`
+	Type           string    `json:"type"`
 }
 
 func (l *daisyLog) writeLogEntry(e *logEntry) {
 	if l.cloudLogger != nil {
-		l.cloudLogger.Log(logging.Entry{Payload: e})
+		l.cloudLogger.Log(logging.Entry{Timestamp: e.LocalTimestamp, Payload: e})
 	}
 
 	if l.gcsLogWriter != nil {

daisy/step_attach_disks.go
@@ -110,7 +110,7 @@ func (a *AttachDisks) run(ctx context.Context, s *Step) dErr {
 				ad.Instance = instRes.RealName
 			}
 
-			w.Logger.StepInfo(s.w, s.name, "AttachDisks", "attaching disk %q to instance %q.", ad.AttachedDisk.Source, inst)
+			w.Logger.StepInfo(s.w, s.name, "AttachDisks", "Attaching disk %q to instance %q.", ad.AttachedDisk.Source, inst)
 			if err := w.ComputeClient.AttachDisk(ad.project, ad.zone, ad.Instance, &ad.AttachedDisk); err != nil {
 				e <- newErr(err)
 				return

daisy/step_create_disks.go
@@ -53,7 +53,7 @@ func (c *CreateDisks) run(ctx context.Context, s *Step) dErr {
 				cd.SourceImage = image.link
 			}
 
-			w.Logger.StepInfo(s.w, s.name, "CreateDisks", "creating disk %q.", cd.Name)
+			w.Logger.StepInfo(s.w, s.name, "CreateDisks", "Creating disk %q.", cd.Name)
 			if err := w.ComputeClient.CreateDisk(cd.Project, cd.Zone, &cd.Disk); err != nil {
 				e <- newErr(err)
 				return

daisy/step_create_images.go
@@ -67,7 +67,7 @@ func (c *CreateImages) run(ctx context.Context, s *Step) dErr {
 				}
 			}
 
-			w.Logger.StepInfo(s.w, s.name, "CreateImages", "creating image %q.", ci.Name)
+			w.Logger.StepInfo(s.w, s.name, "CreateImages", "Creating image %q.", ci.Name)
 			if err := w.ComputeClient.CreateImage(ci.Project, &ci.Image); err != nil {
 				e <- newErr(err)
 				return

daisy/step_create_instances.go
@@ -19,10 +19,11 @@ import (
 	"context"
 	"fmt"
 	"path"
+	"strings"
 	"sync"
 	"time"
 
-	"google.golang.org/api/googleapi"
+	"cloud.google.com/go/logging"
 )
 
 // CreateInstances is a Daisy CreateInstances workflow step.
@@ -31,48 +32,76 @@ type CreateInstances []*Instance
 func logSerialOutput(ctx context.Context, s *Step, name string, port int64, interval time.Duration) {
 	w := s.w
 	logsObj := path.Join(w.logsPath, fmt.Sprintf("%s-serial-port%d.log", name, port))
-	w.Logger.StepInfo(w, s.name, "CreateInstances", "streaming instance %q serial port %d output to gs://%s/%s", name, port, w.bucket, logsObj)
+	w.Logger.StepInfo(w, s.name, "CreateInstances", "Streaming instance %q serial port %d output to https://storage.cloud.google.com/%s/%s", name, port, w.bucket, logsObj)
 	var start int64
 	var buf bytes.Buffer
-	var errs int
+	var gcsErr bool
 	tick := time.Tick(interval)
+
+Loop:
 	for {
 		select {
 		case <-ctx.Done():
-			return
+			break Loop
 		case <-tick:
 			resp, err := w.ComputeClient.GetSerialPortOutput(w.Project, w.Zone, name, port, start)
 			if err != nil {
 				// Instance was deleted by this workflow.
 				if _, ok := w.instances.get(name); !ok {
-					return
+					break Loop
 				}
 				// Instance is stopped.
 				stopped, sErr := w.ComputeClient.InstanceStopped(w.Project, w.Zone, name)
 				if stopped && sErr == nil {
-					return
+					break Loop
 				}
-				w.Logger.StepInfo(w, s.name, "CreateInstances", "instance %q: error getting serial port: %v", name, err)
-				return
+				w.Logger.StepInfo(w, s.name, "CreateInstances", "Instance %q: error getting serial port: %v", name, err)
+				break Loop
 			}
 			start = resp.Next
 			buf.WriteString(resp.Contents)
 			wc := w.StorageClient.Bucket(w.bucket).Object(logsObj).NewWriter(ctx)
 			wc.ContentType = "text/plain"
-			if _, err := wc.Write(buf.Bytes()); err != nil {
-				w.Logger.StepInfo(w, s.name, "CreateInstances", "instance %q: error writing log to GCS: %v", name, err)
-				return
+			if _, err := wc.Write(buf.Bytes()); err != nil && !gcsErr {
+				gcsErr = true
+				w.Logger.StepInfo(w, s.name, "CreateInstances", "Instance %q: error writing log to GCS: %v", name, err)
+				continue
 			}
-			if err := wc.Close(); err != nil {
-				if apiErr, ok := err.(*googleapi.Error); ok && (apiErr.Code >= 500 && apiErr.Code <= 599) {
-					errs++
-					continue
-				}
-				w.Logger.StepInfo(w, s.name, "CreateInstances", "instance %q: error saving log to GCS: %v", name, err)
-				return
+			if err := wc.Close(); err != nil && !gcsErr {
+				gcsErr = true
+				w.Logger.StepInfo(w, s.name, "CreateInstances", "Instance %q: error saving log to GCS: %v", name, err)
+				continue
 			}
-			errs = 0
 		}
+	}
+
+	// Write the output to cloud logging only after instance has stopped.
+	// Type assertion check is needed for tests not to panic.
+	// Split if output is too long for log entry (100K max, we leave a 2K buffer).
+	dl, ok := w.Logger.(*daisyLog)
+	if ok {
+		ss := strings.SplitAfter(buf.String(), "\n")
+		var str string
+		cl := func(str string) {
+			dl.cloudLogger.Log(logging.Entry{
+				Payload: map[string]string{
+					"localTimestamp": time.Now().String(),
+					"workflowName":   getAbsoluteName(w),
+					"message":        fmt.Sprintf("Serial port output for instance %q", name),
+					"serialPort1":    str,
+					"type":           "Daisy",
+				},
+			})
+		}
+		for _, s := range ss {
+			if len(str)+len(s) > 98*1024 {
+				cl(str)
+				str = s
+			} else {
+				str += s
+			}
+		}
+		cl(str)
 	}
 }
 
@@ -116,7 +145,7 @@ func (c *CreateInstances) run(ctx context.Context, s *Step) dErr {
 				}
 			}
 
-			w.Logger.StepInfo(w, s.name, "CreateInstances", "creating instance %q.", i.Name)
+			w.Logger.StepInfo(w, s.name, "CreateInstances", "Creating instance %q.", i.Name)
 			if err := w.ComputeClient.CreateInstance(i.Project, i.Zone, &i.Instance); err != nil {
 				eChan <- newErr(err)
 				return

daisy/step_create_instances_test.go
@@ -53,25 +53,25 @@ func TestLogSerialOutput(t *testing.T) {
 	}{
 		{
 			"Error but instance stopped",
-			"streaming instance \"i1\" serial port 0 output to gs://test-bucket/i1-serial-port0.log",
+			"Streaming instance \"i1\" serial port 0 output to https://storage.cloud.google.com/test-bucket/i1-serial-port0.log",
 			"",
 			"i1",
 		},
 		{
 			"Error but instance running",
-			"streaming instance \"i2\" serial port 0 output to gs://test-bucket/i2-serial-port0.log",
-			"instance \"i2\": error getting serial port: fail",
+			"Streaming instance \"i2\" serial port 0 output to https://storage.cloud.google.com/test-bucket/i2-serial-port0.log",
+			"Instance \"i2\": error getting serial port: fail",
 			"i2",
 		},
 		{
 			"Normal flow",
-			"streaming instance \"i3\" serial port 0 output to gs://test-bucket/i3-serial-port0.log",
+			"Streaming instance \"i3\" serial port 0 output to https://storage.cloud.google.com/test-bucket/i3-serial-port0.log",
 			"",
 			"i3",
 		},
 		{
 			"Error but instance deleted",
-			"streaming instance \"i4\" serial port 0 output to gs://test-bucket/i4-serial-port0.log",
+			"Streaming instance \"i4\" serial port 0 output to https://storage.cloud.google.com/test-bucket/i4-serial-port0.log",
 			"",
 			"i4",
 		},

daisy/step_create_networks.go
@@ -47,7 +47,7 @@ func (c *CreateNetworks) run(ctx context.Context, s *Step) dErr {
 		go func(n *Network) {
 			defer wg.Done()
 
-			w.Logger.StepInfo(w, s.name, "CreateNetworks", "creating network %q.", n.Name)
+			w.Logger.StepInfo(w, s.name, "CreateNetworks", "Creating network %q.", n.Name)
 			if err := w.ComputeClient.CreateNetwork(n.Project, &n.Network); err != nil {
 				e <- newErr(err)
 				return

commit 9c7ef23a4be4e646b9472b9d64b7e6248fe62f4a

daisy: fix uploading sources in subworkflow
Fix regressing introduced by #403
Don't check w.Sources before calling uploadSources, this funcions
already deals with empty sources and also upload sources from
subworkflows

daisy/workflow.go
@@ -205,13 +205,11 @@ func (w *Workflow) Run(ctx context.Context) error {
 	defer w.cleanup()
 	w.Logger.WorkflowInfo(w, "Daisy scratch path: https://console.cloud.google.com/storage/browser/%s", path.Join(w.bucket, w.scratchPath))
 
-	if len(w.Sources) != 0 {
-		w.Logger.WorkflowInfo(w, "Uploading sources")
-		if err := w.uploadSources(ctx); err != nil {
-			w.Logger.WorkflowInfo(w, "Error uploading sources: %v", err)
-			close(w.Cancel)
-			return err
-		}
+	w.Logger.WorkflowInfo(w, "Uploading sources")
+	if err := w.uploadSources(ctx); err != nil {
+		w.Logger.WorkflowInfo(w, "Error uploading sources: %v", err)
+		close(w.Cancel)
+		return err
 	}
 	w.Logger.WorkflowInfo(w, "Running workflow")
 	if err := w.run(ctx); err != nil {