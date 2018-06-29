20d471f5ebab83949e8393157e1ed931f1b8c15d
Gustavo Serra Scalet <gustavo.scalet@collabora.com>
2018-06-29 16:36:19 -0300

    network test: Evaluate IP alias functionality also while running

--- daisy_workflows/image_test/network/network-tester.py
+++ daisy_workflows/image_test/network/network-tester.py
@@ -14,6 +14,7 @@
 # limitations under the License.
 
 import logging
+import time
 import urllib2
 
 
@@ -24,14 +25,17 @@ import utils
 
 
 class RequestError(Exception):
-  def __init__(self, name, instance, ip_range):
+  def __init__(self, name, instance, ip_range, positive_test=True):
     self.name = name
     self.instance = instance
     self.ip_range = ip_range
+    self.positive_test = positive_test
 
   def __str__(self):
-    return "%s didn't redirect ip %s to instance %s" % (
-        self.name, self.ip_range, self.instance)
+    text = "%s didn't redirect ip traffic from %s to instance %s"
+    if not self.positive_test:
+      text = "%s redirected ip traffic from %s to instance %s but it shouldn't"
+    return text % (self.name, self.ip_range, self.instance)
 
 
 def GetHostname(addr, timeout=10):
@@ -40,26 +44,49 @@ def GetHostname(addr, timeout=10):
   return urllib2.urlopen(URL, None, timeout).read()
 
 
-def TestIPAlias(testee, ip_alias, ip_mask):
-  testee_hostname = GetHostname(testee)
+def TestIPAlias(instance, ip_alias, ip_mask):
+  testee_hostname = GetHostname(instance)
+  cidr = "%s/%s" % (ip_alias, ip_mask)
 
   # caching ips out of range for using it on negative tests
   ips = list(genips.GenIPs(ip_alias, ip_mask))
 
   # positive test on expected IPs
   for ip in ips:
-    if testee_hostname != GetHostname(ip):
-      raise Exception("Alias hostname should be the same from host machine.")
+    try:
+      if testee_hostname != GetHostname(ip):
+        raise Exception("Alias hostname should be the same from host machine.")
+    except urllib2.URLError:
+      raise RequestError("ip_alias", instance, cidr)
 
   # negative test
   superset_ips = list(genips.GenIPs(ip_alias, int(ip_mask) - 1))
   invalid_ip = superset_ips[superset_ips.index(ips[0]) - 1]
   try:
     if testee_hostname == GetHostname(invalid_ip):
-      e = "IP alias out of range (%s) should not respond." % invalid_ip
-      raise Exception(e)
-  except (RequestError, urllib2.URLError):
+      raise RequestError("ip_alias", instance, cidr, positive_test=False)
+  except urllib2.URLError:
+    # great, it didn't even respond! So no machine is using that ip.
     pass
+
+
+def SetIPAlias(MD, instance, ip_alias=None, ip_mask=None):
+  # by default, remove the aliasing
+  alias_ip_ranges = []
+  if ip_alias:
+    cidr = "%s/%s" % (ip_alias, ip_mask)
+    alias_ip_ranges = [{'ipCidrRange': cidr}]
+
+  info = {
+      'aliasIpRanges': alias_ip_ranges,
+      # a previous fingerprint is needed in order to change the instance
+      'fingerprint': MD.GetInstanceIfaces(instance)[0][u'fingerprint']
+  }
+  MD.Wait(MD.SetInstanceIface(instance, info))
+
+  # The operation is completed but it might not guarantee that the agent has
+  # already applied the rule. For that reason, wait
+  time.sleep(30)
 
 
 def TestForwardingRule(MD, instance, rule_name):
@@ -71,17 +98,33 @@ def TestForwardingRule(MD, instance, rule_name):
 def main():
   MM = utils.MetadataManager
   testee = MM.FetchMetadataDefault('testee')
+  alias_ip = MM.FetchMetadataDefault('alias_ip')
+  alias_ip_mask = MM.FetchMetadataDefault('alias_ip_mask')
 
-  # Verify IP aliasing is working
-  TestIPAlias(testee, MM.FetchMetadataDefault('alias_ip'),
-      MM.FetchMetadataDefault('alias_ip_mask'))
-
-  # Ensure routes are added when ForwardingRules are created
   credentials, _ = auth.default()
   compute = utils.GetCompute(discovery, credentials)
+  MD = MM(compute, testee)
 
+  # Verify IP aliasing is working
+  #  1) Testing if it works on creation time
+  TestIPAlias(testee, alias_ip, alias_ip_mask)
+
+  #  2) Removing it and guaranteeing that it doesn't work anymore
+  SetIPAlias(MD, testee, [])
+  try:
+    TestIPAlias(testee, alias_ip, alias_ip_mask)
+  except RequestError as e:
+    # the positive test is expected to fail, as no alias ip exists.
+    if not e.positive_test:
+      raise e
+
+  #  3) Re-add it while the machine is running and verify that it works
+  SetIPAlias(MD, testee, alias_ip, alias_ip_mask)
+  TestIPAlias(testee, alias_ip, alias_ip_mask)
+
+  # Ensure routes are added when ForwardingRules are created
   testee_fr = MM.FetchMetadataDefault('testee_forwarding_rule')
-  TestForwardingRule(MM(compute, testee), testee, testee_fr)
+  TestForwardingRule(MD, testee, testee_fr)
 
 
 if __name__ == '__main__':
