087601c6a97943aba0fa0d49dc6f4c60e579105c
Andrew Jackura <ajackura@google.com>
2018-08-17 20:57:26 +0000

    fix

--- daisy_workflows/image_test/configuration/windows/test_config.ps1
+++ daisy_workflows/image_test/configuration/windows/test_config.ps1
@@ -12,76 +12,104 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$errors = @()
+$script:errors = @()
+Import-Module 'C:\Program Files\Google\Compute Engine\sysprep\gce_base.psm1' -ErrorAction Stop
 
 function Test-Activation {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
+  $status = cscript C:\Windows\system32\slmgr.vbs /dli
+  if ($status -notcontains 'License Status: Licensed') {
+    Write-Host 'Image does not appear to be properly licensed:'
+    Write-Host $status
+    $script:errors += $MyInvocation.MyCommand
+  }
 }
 
 function Test-MTU {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
   $want = 1460
   $interface = (Get-CimInstance Win32_NetworkAdapter -filter "ServiceName='netkvm'")[0]
   $result = netsh interface ipv4 show subinterface $interface.NetConnectionID
-  $result[3] -match '^\s*(\d+)'
+  $result[3] -match '^\s*(\d+)' | Out-Null
   $mtu = $Matches[1]
   if ($mtu -ne $want) {
     Write-Host "Improper MTU set: ${mtu}"
     Write-Host $result
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
 function Test-PowershellVersion {
-  Write-Host "Running " + $MyInvocation.MyCommand
-  $want = "5.1"
+  Write-Host 'Running' $MyInvocation.MyCommand
+  $want = '5.1'
   $version = "$($PSVersionTable.PSVersion.Major).$($PSVersionTable.PSVersion.Minor)"
-  if ($version -ge $want) {
+  if ($version -lt $want) {
     Write-Host "Improper Powershell version installed: ${version}, want >= ${want}"
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
 function Test-DotNetVersion {
-  Write-Host "Running " + $MyInvocation.MyCommand
-  $want = "4.7"
-  $version = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full" -Name Version | Select-Object -ExpandProperty Version
-  if ($version -ge $want) {
+  Write-Host 'Running' $MyInvocation.MyCommand
+  $want = '4.7'
+  $version = Get-ItemProperty 'HKLM:\SOFTWARE\Microsoft\NET Framework Setup\NDP\v4\Full' -Name Version | Select-Object -ExpandProperty Version
+  if ($version -lt $want) {
     Write-Host "Improper .Net version installed: ${version}, want >= ${want}"
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
 function Test-NTP {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
+  w32tm /resync | Out-Null
+  $ntp = w32tm /query /peers /verbose
+  @('#Peers: 1', 
+    'Peer: metadata.google.internal,0x1', 
+    'State: Active',
+    'LastSyncErrorMsgId: 0x00000000 (Succeeded)') | Foreach-Object {
+      if ($ntp -notcontains $_) {
+        Write-Host "NTP not setup correctly, $_ not in:"
+        Write-Host $ntp
+        $script:errors += $MyInvocation.MyCommand
+        return
+      }
+    }
+
+    $($ntp -match 'Time Remaining') -match '([0-9\.]+)s' | Out-Null
+    if (15.0 -gt $Matches[1]) {
+      Write-Host 'NTP not setup correctly, Time remaining is longer than the 15 minute poll interval:'
+      Write-Host $ntp
+      $script:errors += $MyInvocation.MyCommand
+    }
 }
 
 function Test-EMSEnabled {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
   $bcd = bcdedit 
   if (-not ($bcd | Select-String -Quiet -Pattern "ems * Yes")) {
-    Write-Host "EMS does not appear to be enabled, result of bcdedit command:"
+    Write-Host 'EMS does not appear to be enabled, result of bcdedit command:'
     Write-Host $bcd
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
 function Test-TimeZone {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
   $timezone = (Get-CimInstance Win32_OperatingSystem).CurrentTimeZone
   if ($timezone -ne 0) {
     Write-Host "Improper timezone ${timezone}, want 0"
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
 function Test-Hostname {
-  Write-Host "Running " + $MyInvocation.MyCommand
+  Write-Host 'Running' $MyInvocation.MyCommand
   $hostname = hostname
-  $want = "foo"
+  $hostname_parts = (Get-Metadata -property 'hostname') -split '\.'
+  $want = $hostname_parts[0]
   if ($hostname -ne $want) {
     Write-Host "Improper hostname ${hostname}, want ${want}"
-    $errors += $MyInvocation.MyCommand
+    $script:errors += $MyInvocation.MyCommand
   }
 }
 
@@ -96,49 +124,15 @@ Test-Hostname
 # background
 Test-Activation
 
-if ($errors.Length -eq 0) {
-  Write-Host TestSuccess
-  exit 0
+if ($script:errors) {
+  Write-Host 'TestFailed, the following tests failed:' $errors
+  exit 1
 }
 
-$msg = "TestFailure, the following tests failed: " + $errors -join ", "
+Write-Host TestSuccess
 
 
 <#
-
-  def testActivationStatus(self):
-    windows_test_utils.GetCommandOutputOrRaiseOnFailure(
-        self.vm, r'cscript C:\Windows\system32\slmgr.vbs /ato')
-    activation_info = windows_test_utils.GetCommandOutputOrRaiseOnFailure(
-        self.vm, r'cscript C:\Windows\system32\slmgr.vbs /dli')
-    expected_info = []
-
-    # Check for KMS activation. Internal images do not get licensed from KMS.
-    if FLAGS.clustermanager == 'prod':
-      if (windows_test_utils.IsWinInternal(self.vm) and
-          windows_test_utils.IsWin2008R2(self.vm)):
-        expected_info.append('License Status: Initial grace period')
-      elif (windows_test_utils.IsWinInternal(self.vm) and
-            windows_test_utils.IsWin2012R2(self.vm)):
-        expected_info.append('License Status: Notification')
-      else:
-        expected_info.append('License Status: Licensed')
-
-    for s in expected_info:
-      self.assertIn(
-          s, activation_info,
-          'activation_info %s does not contain "%s"' % (
-              activation_info, s))
-
-  def testPowershellVersion(self):
-    ps_version = windows_test_utils.GetCommandOutputOrRaiseOnFailure(
-        self.vm, ('"$($PSVersionTable.PSVersion.Major).'
-                  '$($PSVersionTable.PSVersion.Minor)"'))
-    expected = ('4.0' if 'server-2008-r2' in FLAGS.vm_image_path else '5.1')
-    self.assertGreaterEqual(
-        ps_version, expected,
-        'PowerShell version %s is not at least version %s.' %
-        (ps_version, expected))
 
   def testNtp(self):
     windows_test_utils.RunCommandOrRaiseOnFailure(self.vm, 'w32tm /resync')
@@ -157,14 +151,5 @@ $msg = "TestFailure, the following tests failed: " + $errors -join ", "
     # Next NTP sync should be less than 15 minutes away.
     self.assertLess(remaining, 900.0, 'Time remaining is longer '
                     'than the 15 minute poll interval.')
-
-  @skip_annotations.SkipTestInEnvironments(
-      ['dev'], reason_msg='Cannot ping externally in dev on Guitar.')
-  def testNetworkConnection(self):
-    """Do basic network test (connect to www.google.com)."""
-    result = windows_test_utils.GetCommandOutputOrRaiseOnFailure(
-        self.vm,
-        'Test-Connection www.google.com -Count 1 -ErrorAction stop -quiet')
-    self.assertIn('True', result)
 
 #>
