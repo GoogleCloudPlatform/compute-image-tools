74ac9cf5a5d3e4067a88c26915f0f6ba117c5c44
Andrew Jackura <ajackura@google.com>
2018-08-17 20:58:46 +0000

    fix

--- daisy_workflows/image_test/configuration/windows/test_config.ps1
+++ daisy_workflows/image_test/configuration/windows/test_config.ps1
@@ -130,26 +130,3 @@ if ($script:errors) {
 }
 
 Write-Host TestSuccess
-
-
-<#
-
-  def testNtp(self):
-    windows_test_utils.RunCommandOrRaiseOnFailure(self.vm, 'w32tm /resync')
-    peer_info = windows_test_utils.GetCommandOutputOrRaiseOnFailure(
-        self.vm, 'w32tm /query /peers /verbose')
-    for expected_info in ['#Peers: 1', 'Peer: metadata.google.internal,0x1',
-                          'State: Active',
-                          'LastSyncErrorMsgId: 0x00000000 (Succeeded)']:
-      self.assertIn(expected_info, peer_info,
-                    'expected_info (%s) is NOT in peer_info (%s)' % (
-                        expected_info, peer_info))
-    match = re.search(r'Time Remaining: ([0-9\.]+)s', peer_info)
-    self.assertIsNotNone(match, 'Couldn\'t find time remaining in output.')
-    remaining = float(match.group(1))
-    self.assertGreater(remaining, 0.0, 'Invalid time remaining.')
-    # Next NTP sync should be less than 15 minutes away.
-    self.assertLess(remaining, 900.0, 'Time remaining is longer '
-                    'than the 15 minute poll interval.')
-
-#>
