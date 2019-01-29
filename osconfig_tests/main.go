e227e48fdeaecfbf8424e1c38c3937ea17ee72fa
Subrat Prasad <subratp@google.com>
2019-01-29 13:41:15 -0800

    Remove unwanted changes

--- osconfig_tests/main.go
+++ osconfig_tests/main.go
@@ -28,6 +28,7 @@ import (
 	"sync"
 
 	"github.com/GoogleCloudPlatform/compute-image-tools/osconfig_tests/junitxml"
+	"github.com/GoogleCloudPlatform/compute-image-tools/osconfig_tests/test_suites/inventory"
 	packagemanagement "github.com/GoogleCloudPlatform/compute-image-tools/osconfig_tests/test_suites/package_management"
 )
 
@@ -39,17 +40,12 @@ var (
 
 var testFunctions = []func(context.Context, *sync.WaitGroup, chan *junitxml.TestSuite, *log.Logger, *regexp.Regexp, *regexp.Regexp){
 	packagemanagement.TestSuite,
-	// inventory.TestSuite,
+	inventory.TestSuite,
 }
 
 func main() {
 	flag.Parse()
 	ctx := context.Background()
-	val, isok := os.LookupEnv("PATH")
-	if isok != true {
-		fmt.Println("path not set")
-	}
-	fmt.Println("PATH variable: ", val)
 
 	var testSuiteRegex *regexp.Regexp
 	if *testSuiteFilter != "" {
