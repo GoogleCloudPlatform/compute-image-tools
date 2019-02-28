2f443f5fd2250ddd3c3db8762e3dfe377001fcbf
Tony Zhang <tzz@google.com>
2019-02-27 16:06:25 -0800

    refine some details

--- daisy_workflows/export/export_disk_ext.sh
+++ daisy_workflows/export/export_disk_ext.sh
@@ -15,6 +15,7 @@
 set -x
 
 DISK_RESIZING_MON=disk_resizing_mon.sh
+
 URL="http://metadata/computeMetadata/v1/instance/attributes"
 GS_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/gcs-path)
 FORMAT=$(curl -f -H Metadata-Flavor:Google ${URL}/format)
@@ -25,15 +26,24 @@ LOCAL_PATH=${GS_PATH##*//}
 OUTS_PATH=${LOCAL_PATH%/*}
 mkdir -p "/gs/${OUTS_PATH}"
 
-# Disk image size info.
+# Prepare disk size info.
+# 1. Disk image size info.
 SIZE_BYTES=$(qemu-img info --output "json" /dev/sdb | grep -m1 "virtual-size" | grep -o '[0-9]\+')
-# Round up to the next GB.
+# 2. Round up to the next GB.
 SIZE_OUTPUT_GB=$(awk "BEGIN {print int((${SIZE_BYTES}/1000000000) + 1)}")
-# Add 10GB of additional space to total disk size to hold OS
-SIZE_DISK_GB=$(awk "BEGIN {print int(${SIZE_OUTPUT_GB} + 10)}")
+# 3. Add 5GB of additional space to max size to prevent the corner case that output
+# file is slightly larger than source disk.
+MAX_BUFFER_DISK_SIZE_GB=$(awk "BEGIN {print int(${SIZE_OUTPUT_GB} + 5)}")
 
-# Get gcs path for disk size monitor script
-DISK_RESIZING_MON_PATH=gs://${OUTS%/*}/sources/${DISK_RESIZING_MON}
+# Prepare buffer disk
+echo "GCEExport: Initializing buffer disk for qemu-img output..."
+sudo arted -a optimal /dev/sdc mklabel gpt
+sudo mkfs.ext4 /dev/sdc
+sudo mount /dev/sdc "/gs/${OUTS_PATH}"
+
+# Fetch disk size monitor script from GCS1
+DISK_RESIZING_MON_GCS_PATH=gs://${OUTS_PATH%/*}/sources/${DISK_RESIZING_MON}
+DISK_RESIZING_MON_LOCAL_PATH=/gs/${DISK_RESIZING_MON}
 echo "GCEExport: Copying disk size monitor script..."
 if ! out=$(gsutil cp "${DISK_RESIZING_MON_PATH}" "/gs/${OUTS}/${DISK_RESIZING_MON}"); then
   echo "ExportFailed: Failed to copy disk size monitor script. Error: ${out}"
@@ -45,15 +55,15 @@ echo "GCEExport: Running disk size monitor, which can resize disk to at most ${S
 sh /gs/${OUTS}/${DISK_RESIZING_MON} &
 
 echo "GCEExport: Exporting disk of size ${SIZE_OUTPUT_GB}GB and format ${FORMAT}."
-
-if ! out=$(qemu-img convert /dev/sdb "/gs/${LOCAL_PATH}" -p -O $FORMAT 2>&1); then
+if ! out=$(qemu-img convert /dev/sdb "/gs/${IMAGE_OUTPUT_PATH}" -p -O $FORMAT 2>&1); then
   echo "ExportFailed: Failed to export disk source to ${GS_PATH} due to qemu-img error: ${out}"
   exit
 fi
 echo ${out}
 
+echo "GCEExport: Copying output image to target GCS path..."
 if ! out=$(gsutil cp "/gs/${LOCAL_PATH}" "${GS_PATH}" 2>&1); then
-  echo "ExportFiled: Failed to copy output image to ${GS_PATH}, error: ${out}"
+  echo "ExportFailed: Failed to copy output image to ${GS_PATH}, error: ${out}"
   exit
 fi
 echo ${out}
