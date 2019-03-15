de00ee6cf4aa661f3ac67682d551af27a7f423e3
Andrew Jackura <ajackura@google.com>
2019-03-15 14:28:47 -0700

    update

--- cli_tools/google-osconfig-agent/main.go
+++ cli_tools/google-osconfig-agent/main.go
@@ -40,12 +40,6 @@ func init() {
 	config.SetVersion(version)
 
 	obtainLock()
-	logger.DeferredFatalFuncs = append(logger.DeferredFatalFuncs, deferredFuncs...)
-	defer func() {
-		for _, f := range deferredFuncs {
-			f()
-		}
-	}()
 }
 
 type logWritter struct{}
@@ -94,6 +88,13 @@ var deferredFuncs []func()
 func main() {
 	flag.Parse()
 	ctx := context.Background()
+
+	logger.DeferredFatalFuncs = append(logger.DeferredFatalFuncs, deferredFuncs...)
+	defer func() {
+		for _, f := range deferredFuncs {
+			f()
+		}
+	}()
 
 	if err := config.SetConfig(); err != nil {
 		logger.Errorf(err.Error())
