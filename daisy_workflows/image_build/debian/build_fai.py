31d90fe5983be4751e4a44fa294e9ee0095e4ddf
Zach Marano <zmarano@google.com>
2019-06-05 10:34:32 -0700

    Update Debian builds.
    - Latest bootstrap-vz build.
    - Debian fai based builds with GCE repo support.

--- daisy_workflows/image_build/debian/build_fai.py
+++ daisy_workflows/image_build/debian/build_fai.py
@@ -21,6 +21,7 @@ debian_cloud_images_version: The debian-cloud-images scripts git commit ID
 to use.
 debian_version: The FAI tool debian version to be requested.
 image_dest: The Cloud Storage destination for the resultant image.
+google_cloud_repo: The repository branch to use for packages.cloud.google.com.
 """
 
 import collections
@@ -32,9 +33,6 @@ import urllib.request
 
 import utils
 
-# The following package is necessary to retrieve from https repositories.
-utils.AptGetInstall(['apt-transport-https', 'qemu-utils', 'dosfstools'])
-
 
 def main():
   # Get Parameters.
@@ -44,6 +42,8 @@ def main():
       'debian_cloud_images_version', raise_on_not_found=True)
   debian_version = utils.GetMetadataAttribute(
       'debian_version', raise_on_not_found=True)
+  google_cloud_repo = utils.GetMetadataAttribute(
+      'google_cloud_repo', raise_on_not_found=True)
   image_dest = utils.GetMetadataAttribute('image_dest',
       raise_on_not_found=True)
   outs_path = utils.GetMetadataAttribute('daisy-outs-path',
@@ -54,6 +54,7 @@ def main():
 
   # force an apt-get update before next install
   utils.AptGetInstall.first_run = True
+  utils.AptGetInstall(['apt-transport-https', 'qemu-utils', 'dosfstools'])
 
   debian_host_version = utils.Execute(['cat', '/etc/debian_version'],
           capture_output=True)
@@ -94,10 +95,47 @@ def main():
   disk_name = 'disk.raw'
   config_space = os.getcwd() + work_dir + '/config_space/'
 
-  # Copy GCE_SPECIFIC fai class
+  # Copy GCE_SPECIFIC fai classes.
   utils.Execute(['cp', '/files/fai_config/packages/GCE_SPECIFIC',
       config_space + 'package_config/GCE_SPECIFIC'])
+  os.mkdir(config_space + 'files/etc/apt/sources.list.d/gce.list')
+  utils.Execute(['cp', '/files/fai_config/sources/GCE_SPECIFIC',
+      config_space + 'files/etc/apt/sources.list.d/gce.list/GCE_SPECIFIC'])
+  utils.Execute(['cp', '/files/fai_config/sources/file_modes',
+      config_space + 'files/etc/apt/sources.list.d/gce.list/file_modes'])
+  utils.Execute(['cp', '/files/fai_config/sources/repository.GCE_SPECIFIC',
+      config_space + 'hooks/repository.GCE_SPECIFIC'])
   fai_classes += ['GCE_SPECIFIC']
+
+  # GCE staging package repo.
+  if google_cloud_repo == "staging" or google_cloud_repo == "unstable":
+    os.mkdir(config_space + 'files/etc/apt/sources.list.d/gce_staging.list')
+    utils.Execute(
+        ['cp', '/files/fai_config/sources/GCE_STAGING',
+         (config_space +
+          'files/etc/apt/sources.list.d/gce_staging.list/GCE_STAGING')])
+    utils.Execute(
+        ['cp', '/files/fai_config/sources/file_modes',
+         (config_space +
+          'files/etc/apt/sources.list.d/gce_staging.list/file_modes')])
+    utils.Execute(['cp', '/files/fai_config/sources/repository.GCE_STAGING',
+        config_space + 'hooks/repository.GCE_STAGING'])
+    fai_classes += ['GCE_STAGING']
+
+  # GCE unstable package repo.
+  if google_cloud_repo == "unstable":
+    os.mkdir(config_space + 'files/etc/apt/sources.list.d/gce_unstable.list')
+    utils.Execute(
+        ['cp', '/files/fai_config/sources/GCE_UNSTABLE',
+         (config_space +
+          'files/etc/apt/sources.list.d/gce_unstable.list/GCE_UNSTABLE')])
+    utils.Execute(
+        ['cp', '/files/fai_config/sources/file_modes',
+         (config_space +
+          'files/etc/apt/sources.list.d/gce_unstable.list/file_modes')])
+    utils.Execute(['cp', '/files/fai_config/sources/repository.GCE_UNSTABLE',
+        config_space + 'hooks/repository.GCE_UNSTABLE'])
+    fai_classes += ['GCE_UNSTABLE']
 
   # Run fai-tool.
   cmd = ['fai-diskimage', '--verbose', '--hostname', 'debian', '--class',
