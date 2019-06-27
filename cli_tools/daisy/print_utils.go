e3412aca5501dd0ab673160797fac2823e539e90
Tony Zhang <tzz@google.com>
2019-06-27 10:40:24 -0700

    fix format

--- cli_tools/daisy/print_utils.go
+++ cli_tools/daisy/print_utils.go
@@ -8,28 +8,27 @@ import (
 )
 
 func printPerfProfile(workflow *daisy.Workflow) {
-        timeRecords := workflow.GetStepTimeRecords()
-        if len(timeRecords) == 0 {
-                return
-        }
+	timeRecords := workflow.GetStepTimeRecords()
+	if len(timeRecords) == 0 {
+		return
+	}
 
-        wfStartTime := time.Now()
-        wfEndTime := time.Time{}
-        fmt.Println("\nPerf Profile:")
-        for _, r := range timeRecords {
-                if wfStartTime.After(r.StartTime) {
-                        wfStartTime = r.StartTime
-                }
-                if wfEndTime.Before(r.EndTime) {
-                        wfEndTime = r.EndTime
-                }
-                fmt.Printf("- %v: %v\n", r.Name, formatDuration(r.EndTime.Sub(r.StartTime)))
-        }
-        fmt.Printf("Total time: %v\n\n", formatDuration(wfEndTime.Sub(wfStartTime)))
+	wfStartTime := time.Now()
+	wfEndTime := time.Time{}
+	fmt.Println("\nPerf Profile:")
+	for _, r := range timeRecords {
+		if wfStartTime.After(r.StartTime) {
+			wfStartTime = r.StartTime
+		}
+		if wfEndTime.Before(r.EndTime) {
+			wfEndTime = r.EndTime
+		}
+		fmt.Printf("- %v: %v\n", r.Name, formatDuration(r.EndTime.Sub(r.StartTime)))
+	}
+	fmt.Printf("Total time: %v\n\n", formatDuration(wfEndTime.Sub(wfStartTime)))
 }
 
 func formatDuration(d time.Duration) string {
-        s := int(d.Seconds())
-        return fmt.Sprintf("%v:%v:%v", s/3600, s/60%60, s%60)
+	s := int(d.Seconds())
+	return fmt.Sprintf("%v:%v:%v", s/3600, s/60%60, s%60)
 }
-
