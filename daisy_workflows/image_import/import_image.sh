5ad5e899c65aed0b06123178cf8fc5163d771a6c
Eric Edens <ericedens@google.com>
2019-08-20 12:18:00 -0700

    Remove gcsfuse as a dependency.
    We've found that qemu-img can fail when converting images that were
    mounted using gcsfuse.

--- daisy_workflows/image_import/import_image.sh
+++ daisy_workflows/image_import/import_image.sh
@@ -15,23 +15,27 @@
 
 set -x
 
+BYTES_1GB=1073741824
 URL="http://metadata/computeMetadata/v1/instance"
 DAISY_SOURCE_URL="$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/daisy-sources-path)"
-SOURCE_DISK_FILE="$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/source_disk_file)"
-SOURCEURL=${SOURCE_DISK_FILE}
-SOURCEBUCKET="$(echo ${SOURCEURL} | awk -F/ '{print $3}')"
-SOURCEPATH="${SOURCEURL#"gs://"}"
+SOURCE_URL="$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/source_disk_file)"
 DISKNAME="$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/disk_name)"
+SCRATCH_DISK_NAME="$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/scratch_disk_name)"
 ME="$(curl -f -H Metadata-Flavor:Google ${URL}/name)"
 ZONE=$(curl -f -H Metadata-Flavor:Google ${URL}/zone)
+
+SOURCE_FILE_EXT="${SOURCE_URL##*.}"
+SOURCE_SIZE_BYTES="$(gsutil du ${SOURCE_URL} | grep -o '^[0-9]\+')"
+IMAGE_PATH="/daisy-scratch/image.${SOURCE_FILE_EXT}"
 
 # Print info.
 echo "#################" 2> /dev/null
 echo "# Configuration #" 2> /dev/null
 echo "#################" 2> /dev/null
-echo "SOURCEURL: ${SOURCEURL}" 2> /dev/null
-echo "SOURCEBUCKET: ${SOURCEBUCKET}" 2> /dev/null
-echo "SOURCEPATH: ${SOURCEPATH}" 2> /dev/null
+echo "IMAGE_PATH: ${IMAGE_PATH}" 2> /dev/null
+echo "SOURCE_URL: ${SOURCE_URL}" 2> /dev/null
+echo "SOURCE_SIZE_BYTES: ${SOURCE_SIZE_BYTES}" 2> /dev/null
+echo "SOURCE_FILE_EXT: ${SOURCE_FILE_EXT}" 2> /dev/null
 echo "DISKNAME: ${DISKNAME}" 2> /dev/null
 echo "ME: ${ME}" 2> /dev/null
 echo "ZONE: ${ZONE}" 2> /dev/null
@@ -48,26 +52,49 @@ function resizeDisk() {
   fi
 }
 
-# Mount GCS bucket containing the disk image.
-mkdir -p /gcs/${SOURCEBUCKET}
-gcsfuse --implicit-dirs ${SOURCEBUCKET} /gcs/${SOURCEBUCKET}
+function copyImageToScratchDisk() {
+  local scratchDiskSizeGigabytes=$(awk "BEGIN {print int(((${SOURCE_SIZE_BYTES} - 1)/${BYTES_1GB}) + 1)}")
+  # We allocate double capacity for OVA, which would
+  # require making an additional copy of its enclosed VMDK.
+  if [[ "${SOURCE_FILE_EXT}" == "ova" ]]; then
+     scratchDiskSizeGigabytes=$((scratchDiskSizeGigabytes * 2))
+  fi
 
-# Atrocious OVA hack.
-SOURCEFILE_TYPE="${$SOURCE_DISK_FILE##*.}"
-if [[ "${SOURCEFILE_TYPE}" == "ova" ]]; then
+  # This disk is initially created with 10GB of space.
+  # Enlarge it if that's insufficient to hold the input image.
+  if [[ ${scratchDiskSizeGigabytes} -gt 10 ]]; then
+    resizeDisk "${SCRATCH_DISK_NAME}" "${scratchDiskSizeGigabytes}" "${ZONE}"
+  fi
+
+  mkdir -p /daisy-scratch
+  # /dev/sdb is used since the scratch disk is the second
+  # disk that's attached in import_disk.wf.json.
+  mkfs.ext4 /dev/sdb
+  mount /dev/sdb /daisy-scratch
+  if [[ $? -ne 0 ]]; then
+    echo "ImportFailed: Failed to prepare scratch disk."
+  fi
+  gsutil cp "${SOURCE_URL}" "${IMAGE_PATH}"
+  echo "Import: Copied image from ${SOURCE_URL} to ${IMAGE_PATH}"
+}
+
+copyImageToScratchDisk
+
+# If the image is an OVA, then copy out its VMDK.
+if [[ "${SOURCE_FILE_EXT}" == "ova" ]]; then
   echo "Import: Unpacking VMDK files from ova."
-  VMDK="$(tar --list -f /gcs/${SOURCEPATH} | grep -m1 vmdk)"
-  tar -C /gcs/${DAISY_SOURCE_URL#"gs://"} -xf /gcs/${SOURCEPATH} ${VMDK}
-  SOURCEPATH="${DAISY_SOURCE_URL#"gs://"}/${VMDK}"
+  VMDK="$(tar --list -f ${IMAGE_PATH} | grep -m1 vmdk)"
+  tar -C /daisy-scratch -xf ${IMAGE_PATH} ${VMDK}
+  IMAGE_PATH="/daisy-scratch/${VMDK}"
   echo "Import: New source file is ${VMDK}"
 fi
 
+# Ensure the output disk has sufficient space to accept the disk image.
 # Disk image size info.
-SIZE_BYTES=$(qemu-img info --output "json" /gcs/${SOURCEPATH} | grep -m1 "virtual-size" | grep -o '[0-9]\+')
+SIZE_BYTES=$(qemu-img info --output "json" ${IMAGE_PATH} | grep -m1 "virtual-size" | grep -o '[0-9]\+')
  # Round up to the next GB.
-SIZE_GB=$(awk "BEGIN {print int((${SIZE_BYTES}/1000000000)+ 1)}")
-
-echo "Import: Importing ${SOURCEPATH} of size ${SIZE_GB}GB to ${DISKNAME} in ${ZONE}." 2> /dev/null
+SIZE_GB=$(awk "BEGIN {print int(((${SIZE_BYTES} - 1)/${BYTES_1GB}) + 1)}")
+echo "Import: Importing ${IMAGE_PATH} of size ${SIZE_GB}GB to ${DISKNAME} in ${ZONE}." 2> /dev/null
 
 # Ensure the disk referenced by $DISKNAME is large enough to
 # hold the inflated disk. For the common case, we initialize
@@ -83,10 +110,10 @@ if ! out=$(gcloud -q compute instances attach-disk ${ME} --disk=${DISKNAME} --zo
 fi
 echo ${out}
 
-# Decompress the image and write it to the disk referenced by $DISKNAME.
-# /dev/sdb is used since it's the second disk that is attached
-# in import_disk.wf.json.
-if ! out=$(qemu-img convert /gcs/${SOURCEPATH} -p -O raw -S 512b /dev/sdb 2>&1); then
+# Convert the image and write it to the disk referenced by $DISKNAME.
+# /dev/sdc is used since we're manually attaching this disk, and sdb was already
+# used by the scratch disk.
+if ! out=$(qemu-img convert ${IMAGE_PATH} -p -O raw -S 512b /dev/sdc 2>&1); then
   echo "ImportFailed: Failed to convert source to raw. [Privacy-> error: ${out} <-Privacy]"
   exit
 fi
