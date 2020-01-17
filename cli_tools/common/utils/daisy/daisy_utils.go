ad5b76da678273962210e9a42bab8f2962aca87f
Tony Zhang <tzz@google.com>
2020-01-17 10:21:32 -0800

    fix format

--- cli_tools/common/utils/daisy/daisy_utils.go
+++ cli_tools/common/utils/daisy/daisy_utils.go
@@ -172,10 +172,10 @@ func RemovePrivacyLogTag(message string) string {
 func PostProcessDErrorForNetworkFlag(action string, err error, network string, w *daisy.Workflow) {
 	if derr, ok := err.(daisy.DError); ok {
 		if derr.CausedByErrType("networkResourceDoesNotExist") && network == "" {
-			w.LogWorkflowInfo("A VPC network is required for running %v," +
-																" and the default VPC network does not exist in your project. You will need to" +
-																" specify a VPC network with the --network flag. For more information about" +
-																" VPC networks, see https://cloud.google.com/vpc.", action)
+			w.LogWorkflowInfo("A VPC network is required for running %v,"+
+				" and the default VPC network does not exist in your project. You will need to"+
+				" specify a VPC network with the --network flag. For more information about"+
+				" VPC networks, see https://cloud.google.com/vpc.", action)
 		}
 	}
 }
