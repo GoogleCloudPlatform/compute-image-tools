06b4b1440b5bfe73a9682a7afb9b274dcdcebb26
Tony Zhang <tzz@google.com>
2020-04-22 22:48:34 -0700

    snapshot: extract project name from source disk's URI

--- daisy/step_create_snapshots.go
+++ daisy/step_create_snapshots.go
@@ -47,12 +47,15 @@ func (c *CreateSnapshots) run(ctx context.Context, s *Step) DError {
 	createSnapshot := func(ss *Snapshot) {
 		defer wg.Done()
 		// Get source disk link if SourceDisk is a daisy reference to a disk.
-		if d, ok := w.disks.get(ss.SourceDisk); ok {
-			ss.SourceDisk = d.link
+		if diskURLRgx.MatchString(ss.SourceDisk) {
+			if d, ok := w.disks.get(ss.SourceDisk); ok {
+				ss.SourceDisk = d.link
 
-			// Override snapshot link due that disk may be from a different project
-			if ss.Project != d.Project {
-				ss.link = fmt.Sprintf("projects/%s/global/snapshots/%s", d.Project, ss.Name)
+				// Override snapshot link due that disk may be from a different project
+				m := NamedSubexp(diskURLRgx, d.link)
+				if ss.Project != m["project"] {
+					ss.link = fmt.Sprintf("projects/%s/global/snapshots/%s", m["project"], ss.Name)
+				}
 			}
 		}
 
