80adf71e7757a07e453f75831d3d2e7a3afee246
Zach Marano <zmarano@google.com>
2020-11-16 10:51:41 -0800

    Move daisy integration tests to Debian 10 and debian-10-worker. (#1430)

--- daisy_integration_tests/scripts/run_daisy_and_check_logs.sh
+++ daisy_integration_tests/scripts/run_daisy_and_check_logs.sh
@@ -34,7 +34,7 @@ run_daisy() {
        "CreateDisks": [
          {
            "name": "disk-from-image-family-url",
-           "sourceImage": "projects/debian-cloud/global/images/family/debian-9",
+           "sourceImage": "projects/debian-cloud/global/images/family/debian-10",
            "type": "pd-ssd"
          }
        ]
