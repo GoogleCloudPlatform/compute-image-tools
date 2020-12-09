cc19c2fec5c24b7a60a5e10e165938a294991bee
Brian Katyl <bxk@google.com>
2020-12-09 13:03:50 -0800

    Updating the GCE Windows Build
    Updating the GCE Windows Build so that we can start building images using the open sourced resources.
    - Updated scripts
    - Updated README.md to be more descriptive of how to build works, what resources are required, and multiple example of how to call the build workflow.
    - Updated workflow files and provided 2016 and 2019 build workflows (more to come later)

--- daisy_workflows/image_build/windows/README.md
+++ daisy_workflows/image_build/windows/README.md
@@ -1,41 +1,93 @@
-### Windows ISO to Google Compute Image Build
+# Microsoft Windows Build
 
-The currently supported versions of Windows Server are listed below. Additional
-versions will be added in the future.
+To build Microsoft Windows images for use in Google Compute Engine, the following resources are needed:
 
-| Windows Server | Servicing Stack Update (SSU) | Cumulative Update (CU) | Additional Recommended Update |
-| ---| ---| --- | --- |
-| 2016 |[KB45090](http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/07/windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu) |[KB4507460](http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/07/windows10.0-kb4507460-x64_6d645b0420de6481c8cbad52885f70f7369cb662.msu) |
+* Microsoft OS Volume license media in ISO format
+* [PowerShell 7.0 or greater MSI installer](https://github.com/PowerShell/PowerShell#get-powershell)
 
-`Table last updated 2019-07-16`
+The following resources are optional:
 
-- Download Servicing Stack Update and Cumulative Update files
-- Rename files so Service Stack Update installs first. **NOTE:** SSU is
-  typically a higher KB number than the CU.
-```shell
-$ curl -O http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/07/windows10.0-kb4507460-x64_6d645b0420de6481c8cbad52885f70f7369cb662.msu
-$ curl -O http://download.windowsupdate.com/c/msdownload/update/software/secu/2019/07/windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu
-$ mv windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu first-windows10.0-kb4509091-x64_1e1039047044a1f632562cd65df3ce6b77804018.msu
-```
-- Move these files into a subdirectory, optionally named `updates`
+* Windows Updates to sideload in the installation process
+* A Windows Server Update Services server.
 
-- Run build workflow
-```shell
-$ daisy -project my_project -zone us-west1-c \
--var:media="SW_DVD9_Win_Server_STD_CORE_2016_64Bit_English_-4_DC_STD_MLF_X21-70526.ISO" \
--var:updates=updates/ windows-server-2016-dc.wf.json
-```
+## Sideloading Windows Update (Optional)
+
+To reduce the time needed to build a Microsoft Windows image we can apply .msu
+based updated to the OS. This process is more beneficial for older operating systems.
+To do this the build process will install all of the .msu files from the specified updates directory or GCS location in numerical and alphanumeric order.
+
+For Windows Server 2016 and newer operating systems, it is beneficial to install
+at least the most recent Servicing Stack Update (SSU), Cumulative Update (CU),
+and .NET framework updates.
+
+It is best to install updates in the following order:
+1. Microsoft .NET Framework Installation
+1. Cumulative Update for .NET Framework
+1. The OS's Servicing Stack Update
+1. The OS's Cumulative Update
+
+## How the workflow builds the image
+
+1. All of the provided files are uploaded to a GSC bucket that will be used with the workflow.
+1. A new installation disk of the specified size is created and attached to a Windows Server 2019 "bootstrap instance".
+1. The WIM image from the media, the provided Windows updates, GCE drivers, and installation files are applied to the installation disk. This is all done in the [bootstrap_install.ps1](https://github.com/GoogleCloudPlatform/compute-image-tools/blob/master/daisy_workflows/image_build/windows/bootstrap_install.ps1).
+1. The "bootstrap instance" is stopped and a new "install instance" is created that boots the installation disk.
+1. The "install instance" is started and will run the [SetupComplete.cmd](https://github.com/GoogleCloudPlatform/compute-image-tools/blob/master/daisy_workflows/image_build/windows/components/SetupComplete.cmd) and then the [post_install.ps1](https://github.com/GoogleCloudPlatform/compute-image-tools/blob/master/daisy_workflows/image_build/windows/post_install.ps1).
+1. The "install instance" is stopped and a GCE Image is created from the installation disk with the appropriate [on-demand licenses for Windows Server](https://cloud.google.com/compute/docs/instances/windows/ms-licensing) and features.
+
+## Workflow Variables
+
+The workflow files provide default values for many of the variables. When calling a workflow,
+any required variable will need to be provided when calling daisy using the -var: flag. The OS specific workflow files simplify the image creation process by populating the OS specific variables to windows-build.wf.json and also creates a GCE image with the appropriate features and license.
+
+
 | Variable Name | Description |
 | --- | --- |
 | project | Project to allocate resources from during build [Project docs](https://cloud.google.com/resource-manager/docs/creating-managing-projects) |
 | zone | Zone to use for GCE build instance [Zone docs](https://cloud.google.com/compute/docs/regions-zones/) |
 | media | Absolute path to or GCS resource name of ISO file |
-| updates | (Optional) Directory containing updates to be included in install |
+| pwsh | Absolute path to or GCS resource name of PowerShell 7+ installer |
+| updates | (Optional) Directory or GCS location containing updates to be included in install |
+| product_key | (Optional) Windows product key to use. Volume license media by default include the Generic Volume License Key. |
 
-- Create a test instance (Optional)
+### Starting a build workflow
+
+Below are some example of how to call daisy using the provided workflows and required variables.
+
+#### Build a Windows Server 2016 Data Center edition with UEFI Support using local files
+* The windows media will be in the same directory and is named WindowServer2016_DataCenter.ISO.
+* The PowerShell 7+ installer will be in the same directory and is named PowerShell-7.0.3-win-x64.msi.
+* The windows updates for Windows Server 2016 are located in a subfolder called Updates.
+
+```shell
+$ daisy -project my_project -zone us-west1-c \
+-var:media="WindowServer2016_DataCenter.ISO" \
+-var:updates=updates/ \
+-var:pwsh=PowerShell-7.0.3-win-x64.msi \
+windows-server-2016-dc-uefi.wf.json
+```
+
+#### Build a Windows Server 2019 Data Center edition with UEFI Support using files in a GCS bucket
+* The windows media is stores in a GCS.
+* The PowerShell 7+ installer is stores in a GCS.
+* The windows updates for Windows Server 2019 are stores in a GCS.
+
+```shell
+$ daisy -project my_project -zone us-west1-c \
+-var:media="gs://example-build-resources/2019/WindowServer2019_DataCenter.ISO" \
+-var:updates="gs://example-build-resources/2019/updates" \
+-var:pwsh="gs://example-build-resources/PowerShell-7.0.3-win-x64.msi" \
+windows-server-2019-dc-uefi.wf.json
+```
+
+### Creating a instance from the newly created image (Optional)
+
+In this example we'll be using an image Server 2019 image created on 12/09/2020
+and it is named windows-server-2016-dc-v12092020 and stored in the the my_project project.
+
 ```shell
 $ gcloud compute instances create instance_name --async --machine-type n1-standard-8 \
---project my_project --zone us-east1-d --image windows-server-2016-dc-v1
+--project my_project --zone us-west1-c --image windows-server-2019-dc-v12092020
 ```
 [Cloud
 SDK Reference for compute instances create](https://cloud.google.com/sdk/gcloud/reference/compute/instances/create)
--- daisy_workflows/image_build/windows/bootstrap_install.ps1
+++ daisy_workflows/image_build/windows/bootstrap_install.ps1
@@ -31,6 +31,7 @@ function Get-MetadataValue {
   }
   catch [System.Net.WebException] {
     if ($default) {
+      Write-Host "Failed to retrieve value for $key, returning default of $default."
       return $default
     }
     else {
@@ -43,95 +44,134 @@ function Get-MetadataValue {
 function Download-Drivers {
   <#
     .SYNOPSIS
-      Downloads a drivers from GCE to the local builder directory.
+      Downloads the drivers from GCE to the local drivers directory.
   #>
   $gs_path = Get-MetadataValue -key 'drivers-path'
   if ($gs_path) {
-    Write-Output "Downloading components from $gs_path."
+    Write-Output "Downloading drivers from $gs_path."
     & 'gsutil' -m cp -r "${gs_path}/*" $script:driver_dir
-    Write-Output 'Download complete.'
+    Write-Output 'Driver download complete.'
   }
   else {
-    throw "Failed to find key 'drivers-path'. Builder cannot continue."
+    throw "Download-Drivers() Failed to find metadata key 'drivers-path'. Build cannot continue."
   }
 }
 
 function Download-Components {
   <#
     .SYNOPSIS
-      Downloads a builder component from GCE to the local builder directory.
+      Downloads the components from GCE to the local components directory.
   #>
   $gs_path = Get-MetadataValue -key 'components-path'
   if ($gs_path) {
     Write-Output "Downloading components from $gs_path."
     & 'gsutil' -m cp -r "${gs_path}/*" $script:components_dir
-    Write-Output 'Download complete.'
+    Write-Output 'Components download complete.'
   }
   else {
-    throw "Failed to find key 'components-path'. Builder cannot continue."
+    throw "Download-Components() Failed to find metadata key 'components-path'. Build cannot continue."
   }
 }
 
-function Format-InstallDisk {
+function Format-InstallDiskUEFI {
   <#
     .SYNOPSIS
-      Clears then formats the install disk and assigns as D:
+      Clears and initializes disk 1 as GPT. Formats the install disk as D: and system partition as S: for UEFI boot.
   #>
-  Write-Output 'Formatting install disk.'
+  Write-Output 'Formatting install disk for UEFI.'
+  Set-Disk -Number 1 -IsOffline $false
+  Clear-Disk -Number 1 -RemoveData -Confirm:$false -ErrorAction SilentlyContinue
+  Initialize-Disk -Number 1 -PartitionStyle GPT
+
+  Write-Output 'Creating FAT32 system partition of 100MB and assigning volume drive S.'
+  New-Partition -DiskNumber 1 -Size 100MB -DriveLetter S | Format-Volume -FileSystem 'FAT32' -Confirm:$false
+  Write-Output 'Creating NTFS Windows partition and assigning volume drive D.'
+  New-Partition -DiskNumber 1 -UseMaximumSize -DriveLetter D | Format-Volume -FileSystem 'NTFS' -Confirm:$false
+  Write-Output 'Formatting UEFI install disk complete.'
+}
+
+function Format-InstallDiskMBR {
+  <#
+    .SYNOPSIS
+      Clears and initializes disk 1 as MBR. Formats disk as NFTS and assigns as D: for MBR boot.
+  #>
+  Write-Output 'Formatting install disk for MBR.'
   Set-Disk -Number 1 -IsOffline $false
   Clear-Disk -Number 1 -RemoveData -Confirm:$false -ErrorAction SilentlyContinue
   Initialize-Disk -Number 1 -PartitionStyle MBR
-  New-Partition -DiskNumber 1 -UseMaximumSize -DriveLetter D -IsActive |
-    Format-Volume -FileSystem 'NTFS' -Confirm:$false
-  Write-Output 'Formatting install disk complete.'
+  Write-Output 'Creating NTFS Windows partition and assigning volume drive D.'
+  New-Partition -DiskNumber 1 -UseMaximumSize -DriveLetter D -IsActive | Format-Volume -FileSystem 'NTFS' -Confirm:$false
+  Write-Output 'Formatting MBR install disk complete.'
 }
 
 function Get-Wim {
+  <#
+    .SYNOPSIS
+      Mount the ISO and return the absolute path of the install.wim.
+  #>
   $iso = "${script:components_dir}\windows.iso"
   if (Test-Path $iso) {
     $mount_result = Mount-DiskImage -ImagePath $iso -PassThru
     $iso_drive = ($mount_result | Get-Volume).DriveLetter
   }
   else {
-    throw "Failed to find iso:'${iso}'. Builder cannot continue."
+    throw "Get-Wim: Failed to find iso:'$iso'. Boostrapping cannot continue."
   }
+  Write-Host "ISO mounted as $iso_drive"
+  $wim = "${iso_drive}:\sources\install.wim"
 
-  return "${iso_drive}:\sources\install.wim"
+  if (-not (Test-Path $wim)) {
+    throw "Get-Wim: Failed to find wim:'$wim'. Boostrapping cannot continue."
+  }
+  return $wim
 }
 
 function Bootstrap-InstallDisk {
   <#
     .SYNOPSIS
-      Mount the ISO and image the disk.
-  #>
+      Apply the image and do all the offline modifications to prepare the system to boot.
 
+    .DESCRIPTION
+      The following steps are completed in Bootstrap-InstallDisk:
+      1. Disable Defender for increased performance.
+      2. Apply the specified OS edition WIM to the install disk.
+      3. Apply the offline updates to the image applied to the install disk.
+      4. Update the autounattend.xml file and apply it to the image.
+      5. Copy the setupcomplete.cmd.
+      6. Copy netkvmco.dll and add the drivers to the image.
+      7. Setup bootloader.
+      8. Disable login animation.
+  #>
+  $slipstream_max_attemps = 2
   $edition = Get-MetadataValue -key 'edition'
 
   # Disable Defender real time monitoring to greatly increase image
   # expansion and patch deployment speed.
   Set-MpPreference -DisableRealtimeMonitoring $true
 
-  Write-Output 'Applying wim image to install disk.'
+  Write-Output "Applying $edition wim image to install disk."
   Expand-WindowsImage -ImagePath $(Get-Wim) -Name $edition -ApplyPath D:\
 
   if (Test-Path $script:updates_dir) {
-    # Add-WindowsPackage will install in alphabetical order.
-    Write-Output 'Slipstreaming updates into image.'
+    Write-Output 'Slipstreaming updates into image in alphabetical order.'
     Get-ChildItem $script:updates_dir | ForEach-Object {
-      Write-Output "Adding '$($_.Name)' to image."
+      Write-Output "Slipstreaming '$($_.Name)' into image."
       $path = $_.FullName
-      try {
-        Add-WindowsPackage -Path D:\ -PackagePath $path -ErrorAction Stop
-        Write-Output "Successfully added $($_.Name)."
-      }
-      catch {
-        Write-Output 'Slipstreaming update failed, trying once more...'
-        Add-WindowsPackage -Path D:\ -PackagePath $path
+      for ($i=1; $i -le $slipstream_max_attemps; $i++) {
+        try {
+          Add-WindowsPackage -Path D:\ -PackagePath $path -ErrorAction Stop
+          Write-Output "Successfully slipstreamed update $($_.Name) on attempt $i."
+          break
+        }
+        catch {
+          Write-Output "Failed to slipstream update '$($_.Name)' on attempt $i of $slipstream_max_attemps attempts."
+        }
       }
     }
+    Write-Output 'Slipstreaming updates completed.'
   }
 
-  Write-Output 'Populating Unattend file.'
+  Write-Output 'Populating Autounattend.xml file.'
   $autounattend_template = "${script:components_dir}\Autounattend-template.xml"
   $autounattend_file = "${script:components_dir}\Autounattend.xml"
   if (-not (Test-Path $autounattend_template)) {
@@ -139,13 +179,16 @@ function Bootstrap-InstallDisk {
   }
 
   $xml = [xml](Get-Content $autounattend_template)
-  if ($product_key_string.length -gt 0) {
-    Write-Output "Product key string $product_key_string."
+
+  $product_key = Get-MetadataValue -key 'product-key'
+  if ($product_key.length -gt 20) {
+    Write-Output "Adding product key $product_key to Autounattend.xml."
     $new_element = $xml.CreateElement('ProductKey')
-    $new_element.set_InnerText($product_key_string)
+    $new_element.set_InnerText($product_key)
     $xml.unattend.settings[1].component[0].AppendChild($new_element)
   }
   $xml.Save($autounattend_file)
+  Write-Output "autounattend-template.xml has been updated and saved to $autounattend_file."
 
   $panther = 'D:\Windows\Panther'
   if (Test-Path $panther) {
@@ -160,13 +203,21 @@ function Bootstrap-InstallDisk {
   Write-Output 'Copying SetupComplete.cmd.'
   $scripts = 'D:\Windows\Setup\Scripts'
   New-Item  $scripts -Type Directory | Out-Null
-  Copy-Item "${script:components_dir}\SetupComplete.cmd" $scripts
+  Copy-Item "${script:components_dir}\SetupComplete.cmd" -Destination "${scripts}\SetupComplete.cmd"
 
   Write-Output 'Copying netkvmco.dll.'
   Copy-Item $script:driver_dir\netkvmco.dll D:\Windows\System32\netkvmco.dll
 
-  Write-Output 'Slipstreaming drivers.'
-  Add-WindowsDriver -Path D:\ -Driver $script:driver_dir -Recurse -Verbose
+
+  # Add-WindowsDriver only works for Windows 10 x86
+  if ($script:x86) {
+    Write-Output 'Slipstreaming drivers using DISM'
+    DISM /Image:D: /Add-Driver /Driver:$script:driver_dir
+  }
+  else {
+    Write-Output 'Slipstreaming drivers using Add-WindowsDriver'
+    Add-WindowsDriver -Path D:\ -Driver $script:driver_dir -Recurse -Verbose
+  }
 
   Write-Output 'Done applying image.'
 
@@ -174,8 +225,15 @@ function Bootstrap-InstallDisk {
     throw 'Windows not installed!'
   }
 
-  Write-Output 'Setting up bootloader.'
-  & bcdboot D:\Windows /s D:
+  if ($script:uefi) {
+    Write-Output 'Setting up UEFI bootloader.'
+    & bcdboot D:\Windows /s S: /f UEFI
+    Set-Partition -DriveLetter S -GptType '{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}'
+  }
+  else {
+    Write-Output 'Setting up MBR bootloader.'
+    & bcdboot D:\Windows /s D: /f BIOS
+  }
 
   Write-Output 'Disabling startup animation.'
   # See http://support.microsoft.com/kb/2955372/en-us
@@ -191,16 +249,25 @@ try {
   $script:updates_dir = "$script:components_dir\updates"
   $script:driver_dir = "$script:components_dir\drivers"
   New-Item $script:builder_dir -Type directory
+  New-Item $script:updates_dir -Type directory
   New-Item $script:driver_dir -Type directory
+
+  $script:uefi = (Get-MetadataValue -key 'uefi-build').ToLower() -eq 'true'
+  $script:x86 = (Get-MetadataValue -key 'x86-build').ToLower() -eq 'true'
 
   Write-Output 'Boostrapping Windows install disk.'
   Download-Components
   Download-Drivers
-  Format-InstallDisk
+  if ($script:uefi) {
+    Format-InstallDiskUEFI
+  }
+  else {
+    Format-InstallDiskMBR
+  }
   Bootstrap-InstallDisk
 
-  Write-Output 'Setting up repo.'
   $repo = Get-MetadataValue -key 'google-cloud-repo'
+  Write-Output "Setting up GooGet repo $repo."
   & 'C:\ProgramData\GooGet\googet.exe' -root 'D:\ProgramData\GooGet' addrepo "google-compute-engine-${repo}" "https://packages.cloud.google.com/yuck/repos/google-compute-engine-${repo}"
 
   Write-Output 'Bootstrapping Complete, shutting down...'
