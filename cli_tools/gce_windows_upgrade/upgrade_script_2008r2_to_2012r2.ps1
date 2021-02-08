c08a161285d4057ee4348e1e9f1f56a24b64905d
camedk <77289637+camedk@users.noreply.github.com>
2021-02-08 22:06:57 +0100

    Update upgrade_script_2008r2_to_2012r2.ps1

--- cli_tools/gce_windows_upgrade/upgrade_script_2008r2_to_2012r2.ps1
+++ cli_tools/gce_windows_upgrade/upgrade_script_2008r2_to_2012r2.ps1
@@ -43,16 +43,16 @@ online disk noerr
   }
 
   # Find the drive which contains install media.
-  For ($i=0; $i -le 5; $i++) {
+  For ($i=0; $i -lt 5; $i++) {
     $Drives = Get-WmiObject Win32_LogicalDisk
-  ForEach ($Drive in $Drives) {
-    if (Test-Path "$($Drive.DeviceID)\Windows_Svr_Std_and_DataCtr_2012_R2_64Bit_English") {
-      $script:install_media_drive = "$($Drive.DeviceID)"
+    ForEach ($Drive in $Drives) {
+      if (Test-Path "$($Drive.DeviceID)\Windows_Svr_Std_and_DataCtr_2012_R2_64Bit_English") {
+        $script:install_media_drive = "$($Drive.DeviceID)"
+      }
     }
-  }
-  if ($script:install_media_drive){break}
-  Write-Host "Disk not ready yet - waiting"
-  Start-Sleep -Seconds 5
+    if ($script:install_media_drive){break}
+    Write-Host "Disk not ready yet - waiting"
+    Start-Sleep -Seconds 5
   }
 
 
