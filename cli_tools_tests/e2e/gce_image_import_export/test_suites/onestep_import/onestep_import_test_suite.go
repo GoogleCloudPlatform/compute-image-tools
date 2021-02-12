730cccf37629ddb183694ad451d9fc6eaa76626f
Tony Zhang <33922283+dntczdx@users.noreply.github.com>
2021-02-12 11:06:42 -0800

    Enable onestep-import gcloud e2e (#1524)
    * Enable onestep-import gcloud e2e
    
    * limit on beta

--- cli_tools_tests/e2e/gce_image_import_export/test_suites/onestep_import/onestep_import_test_suite.go
+++ cli_tools_tests/e2e/gce_image_import_export/test_suites/onestep_import/onestep_import_test_suite.go
@@ -50,6 +50,8 @@ func OnestepImageImportSuite(
 
 	testTypes := []e2e.CLITestType{
 		e2e.Wrapper,
+		e2e.GcloudBetaProdWrapperLatest,
+		e2e.GcloudBetaLatestWrapperLatest,
 	}
 
 	testsMap := map[e2e.CLITestType]map[*junitxml.TestCase]func(
