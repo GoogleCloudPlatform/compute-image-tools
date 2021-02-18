GoogleCloudPlatform/compute-image-tools

commit f9b9edd83ba1ca9b2281a50a5be71369b47d205a

Disk inspection: Honor no-external-ip (#1478)
Prior to this, when VMs are disallowed to use external IPs, inspection fails with an error Constraint constraints/compute.vmExternalIpAccess violated. If --os is supplied, the error is logged to the console. If --os is omitted, import will fail.

cli_tools/gce_ovf_export/domain/args.go
@@ -150,21 +150,24 @@ func toWorkingDir(currentDir, workflowDir string) string {
 	return workflowDir
 }
 
-// DaisyAttrs returns the subset of DaisyAttrs that are required to instantiate
-// a daisy workflow.
-func (args *OVFExportArgs) DaisyAttrs() daisycommon.WorkflowAttributes {
-	return daisycommon.WorkflowAttributes{
-		Project:           args.Project,
-		Zone:              args.Zone,
-		GCSPath:           args.ScratchBucketGcsPath,
-		OAuth:             args.Oauth,
-		Timeout:           args.Timeout.String(),
-		ComputeEndpoint:   args.Ce,
-		DisableGCSLogs:    args.GcsLogsDisabled,
-		DisableCloudLogs:  args.CloudLogsDisabled,
-		DisableStdoutLogs: args.StdoutLogsDisabled,
-		NoExternalIP:      args.NoExternalIP,
-		WorkflowDirectory: args.WorkflowDir,
+// EnvironmentSettings creates an EnvironmentSettings instance from the fields
+// in this struct.
+func (args *OVFExportArgs) EnvironmentSettings() daisycommon.EnvironmentSettings {
+	return daisycommon.EnvironmentSettings{
+		Project:               args.Project,
+		Zone:                  args.Zone,
+		GCSPath:               args.ScratchBucketGcsPath,
+		OAuth:                 args.Oauth,
+		Timeout:               args.Timeout.String(),
+		ComputeEndpoint:       args.Ce,
+		DisableGCSLogs:        args.GcsLogsDisabled,
+		DisableCloudLogs:      args.CloudLogsDisabled,
+		DisableStdoutLogs:     args.StdoutLogsDisabled,
+		NoExternalIP:          args.NoExternalIP,
+		WorkflowDirectory:     args.WorkflowDir,
+		Network:               args.Network,
+		Subnet:                args.Subnet,
+		ComputeServiceAccount: args.ComputeServiceAccount,
 	}
 }
 

cli_tools/gce_vm_image_import/importer/api_inflater.go
@@ -241,7 +241,7 @@ func (inflater *apiInflater) getCalculateChecksumWorkflow(diskURI string) *daisy
 	}
 
 	// Calculate checksum within 20min.
-	daisycommon.SetWorkflowAttributes(w, daisycommon.WorkflowAttributes{
+	daisycommon.EnvironmentSettings{
 		Project:           inflater.args.Project,
 		Zone:              inflater.args.Zone,
 		GCSPath:           inflater.args.ScratchBucketGcsPath,
@@ -251,7 +251,7 @@ func (inflater *apiInflater) getCalculateChecksumWorkflow(diskURI string) *daisy
 		DisableGCSLogs:    inflater.args.GcsLogsDisabled,
 		DisableCloudLogs:  inflater.args.CloudLogsDisabled,
 		DisableStdoutLogs: inflater.args.StdoutLogsDisabled,
-	})
+	}.ApplyToWorkflow(w)
 	if inflater.args.ComputeServiceAccount != "" {
 		w.AddVar("compute_service_account", inflater.args.ComputeServiceAccount)
 	}

cli_tools/gce_vm_image_import/importer/args.go
@@ -299,20 +299,23 @@ func (args *ImportArguments) registerFlags(flagSet *flag.FlagSet) {
 		"Whether to generalize image using Windows Sysprep. Only applicable to Windows.")
 }
 
-// DaisyAttrs returns the subset of DaisyAttrs that are required to instantiate
-// a daisy workflow.
-func (args ImportArguments) DaisyAttrs() daisycommon.WorkflowAttributes {
-	return daisycommon.WorkflowAttributes{
-		Project:           args.Project,
-		Zone:              args.Zone,
-		GCSPath:           args.ScratchBucketGcsPath,
-		OAuth:             args.Oauth,
-		Timeout:           args.Timeout.String(),
-		ComputeEndpoint:   args.ComputeEndpoint,
-		DisableGCSLogs:    args.GcsLogsDisabled,
-		DisableCloudLogs:  args.CloudLogsDisabled,
-		DisableStdoutLogs: args.StdoutLogsDisabled,
-		NoExternalIP:      args.NoExternalIP,
-		WorkflowDirectory: args.WorkflowDir,
+// EnvironmentSettings creates an EnvironmentSettings instance from the fields
+// in this struct.
+func (args ImportArguments) EnvironmentSettings() daisycommon.EnvironmentSettings {
+	return daisycommon.EnvironmentSettings{
+		Project:               args.Project,
+		Zone:                  args.Zone,
+		GCSPath:               args.ScratchBucketGcsPath,
+		OAuth:                 args.Oauth,
+		Timeout:               args.Timeout.String(),
+		ComputeEndpoint:       args.ComputeEndpoint,
+		DisableGCSLogs:        args.GcsLogsDisabled,
+		DisableCloudLogs:      args.CloudLogsDisabled,
+		DisableStdoutLogs:     args.StdoutLogsDisabled,
+		NoExternalIP:          args.NoExternalIP,
+		WorkflowDirectory:     args.WorkflowDir,
+		Network:               args.Network,
+		Subnet:                args.Subnet,
+		ComputeServiceAccount: args.ComputeServiceAccount,
 	}
 }

cli_tools/common/disk/inspect.go
@@ -49,19 +49,12 @@ type Inspector interface {
 // NewInspector creates an Inspector that can inspect GCP disks.
 // A GCE instance runs the inspection; network and subnet are used
 // for its network interface.
-func NewInspector(wfAttributes daisycommon.WorkflowAttributes, network string, subnet string,
-	computeServiceAccount string, logger logging.Logger) (Inspector, error) {
-	wf, err := daisy.NewFromFile(path.Join(wfAttributes.WorkflowDirectory, workflowFile))
+func NewInspector(env daisycommon.EnvironmentSettings, logger logging.Logger) (Inspector, error) {
+	wf, err := daisy.NewFromFile(path.Join(env.WorkflowDirectory, workflowFile))
 	if err != nil {
 		return nil, err
 	}
-	daisycommon.SetWorkflowAttributes(wf, wfAttributes)
-	wf.AddVar("network", network)
-	wf.AddVar("subnet", subnet)
-	if computeServiceAccount != "" {
-		wf.AddVar("compute_service_account", computeServiceAccount)
-	}
-	return &bootInspector{daisycommon.NewDaisyWorker(wf, logger), logger}, nil
+	return &bootInspector{daisycommon.NewDaisyWorker(wf, env, logger), logger}, nil
 }
 
 // bootInspector implements disk.Inspector using the Python boot-inspect package,

cli_tools/daisycommon/daisy_utils.go
@@ -35,7 +35,7 @@ Loop:
 		return nil, daisy.Errf("unknown workflow Var %q passed to Workflow %q", k, w.Name)
 	}
 
-	SetWorkflowAttributes(w, WorkflowAttributes{
+	EnvironmentSettings{
 		Project:           project,
 		Zone:              zone,
 		GCSPath:           gcsPath,
@@ -45,42 +45,59 @@ Loop:
 		DisableGCSLogs:    disableGCSLogs,
 		DisableCloudLogs:  disableCloudLogs,
 		DisableStdoutLogs: disableStdoutLogs,
-	})
+	}.ApplyToWorkflow(w)
 
 	return w, nil
 }
 
-// WorkflowAttributes holds common attributes that are required to instantiate a daisy workflow.
-type WorkflowAttributes struct {
-	Project, Zone, GCSPath, OAuth, Timeout, ComputeEndpoint, WorkflowDirectory string
-	DisableGCSLogs, DisableCloudLogs, DisableStdoutLogs, NoExternalIP          bool
+// EnvironmentSettings controls the resources that are used during tool execution.
+type EnvironmentSettings struct {
+	// Location of workflows
+	WorkflowDirectory string
+
+	// Fields from daisy.Workflow
+	Project, Zone, GCSPath, OAuth, Timeout, ComputeEndpoint string
+	DisableGCSLogs, DisableCloudLogs, DisableStdoutLogs     bool
+
+	// Worker instance customizations
+	Network, Subnet       string
+	ComputeServiceAccount string
+	NoExternalIP          bool
 }
 
-// SetWorkflowAttributes sets workflow running attributes.
-func SetWorkflowAttributes(w *daisy.Workflow, attrs WorkflowAttributes) {
-	w.Project = attrs.Project
-	w.Zone = attrs.Zone
-	if attrs.GCSPath != "" {
-		w.GCSPath = attrs.GCSPath
+// ApplyWorkerCustomizations sets variables on daisy.Workflow that
+// are used when creating worker instances.
+func (env EnvironmentSettings) ApplyWorkerCustomizations(wf *daisy.Workflow) {
+	wf.AddVar("network", env.Network)
+	wf.AddVar("subnet", env.Subnet)
+	if env.ComputeServiceAccount != "" {
+		wf.AddVar("compute_service_account", env.ComputeServiceAccount)
 	}
-	if attrs.OAuth != "" {
-		w.OAuthPath = attrs.OAuth
-	}
-	if attrs.Timeout != "" {
-		w.DefaultTimeout = attrs.Timeout
-	}
+}
 
-	if attrs.ComputeEndpoint != "" {
-		w.ComputeEndpoint = attrs.ComputeEndpoint
+// ApplyToWorkflow sets fields on daisy.Workflow from the environment settings.
+func (env EnvironmentSettings) ApplyToWorkflow(w *daisy.Workflow) {
+	w.Project = env.Project
+	w.Zone = env.Zone
+	if env.GCSPath != "" {
+		w.GCSPath = env.GCSPath
 	}
-
-	if attrs.DisableGCSLogs {
+	if env.OAuth != "" {
+		w.OAuthPath = env.OAuth
+	}
+	if env.Timeout != "" {
+		w.DefaultTimeout = env.Timeout
+	}
+	if env.ComputeEndpoint != "" {
+		w.ComputeEndpoint = env.ComputeEndpoint
+	}
+	if env.DisableGCSLogs {
 		w.DisableGCSLogging()
 	}
-	if attrs.DisableCloudLogs {
+	if env.DisableCloudLogs {
 		w.DisableCloudLogging()
 	}
-	if attrs.DisableStdoutLogs {
+	if env.DisableStdoutLogs {
 		w.DisableStdoutLogging()
 	}
 }

cli_tools/daisycommon/daisy_utils_test.go
@@ -18,13 +18,14 @@ import (
 	"reflect"
 	"testing"
 
-	"github.com/GoogleCloudPlatform/compute-image-tools/daisy"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/GoogleCloudPlatform/compute-image-tools/daisy"
 )
 
-func TestParseWorkflows(t *testing.T) {
+func Test_ParseWorkflow_HappyCase(t *testing.T) {
 	path := "../../daisy/test_data/test.wf.json"
-	varMap := map[string]string{"key1": "var1", "key2": "var2"}
+	varMap := map[string]string{"bootstrap_instance_name": "bootstrap-${NAME}", "key1": "var1", "key2": "var2", "machine_type": "n1-standard-1"}
 	project := "project"
 	zone := "zone"
 	gcsPath := "gcspath"
@@ -40,7 +41,7 @@ func TestParseWorkflows(t *testing.T) {
 	assertWorkflow(t, w, project, zone, gcsPath, oauth, dTimeout, endpoint, varMap)
 }
 
-func TestParseWorkflowsInvalidPath(t *testing.T) {
+func Test_ParseWorkflow_RaisesErrorWhenInvalidPath(t *testing.T) {
 	varMap := map[string]string{"key1": "var1", "key2": "var2"}
 	project := "project"
 	zone := "zone"
@@ -48,10 +49,173 @@ func TestParseWorkflowsInvalidPath(t *testing.T) {
 	oauth := "oauthpath"
 	dTimeout := "10m"
 	endpoint := "endpoint"
-	w, err := ParseWorkflow("NOT_VALID_PATH", varMap, project, zone, gcsPath, oauth, dTimeout, endpoint,
+	w, err := ParseWorkflow("/file/not/found", varMap, project, zone, gcsPath, oauth, dTimeout, endpoint,
 		true, true, true)
 	assert.Nil(t, w)
 	assert.NotNil(t, err)
+	assert.Contains(t, err.Error(), "/file/not/found: no such file or directory")
+}
+
+func Test_ApplyWorkerCustomizations(t *testing.T) {
+	for _, tt := range []struct {
+		name                       string
+		env                        EnvironmentSettings
+		originalVars, expectedVars map[string]string
+	}{
+		{
+			name: "create daisy.Var when either network or subnetwork is empty",
+			env: EnvironmentSettings{
+				Network:               "",
+				Subnet:                "",
+				ComputeServiceAccount: "csa",
+			},
+			originalVars: map[string]string{},
+			expectedVars: map[string]string{
+				"network":                 "",
+				"subnet":                  "",
+				"compute_service_account": "csa",
+			},
+		},
+		{
+			name: "overwrite daisy.Var when either network or subnetwork is empty",
+			env: EnvironmentSettings{
+				Network:               "",
+				Subnet:                "",
+				ComputeServiceAccount: "csa",
+			},
+			originalVars: map[string]string{
+				"network": "default",
+				"subnet":  "regional",
+			},
+			expectedVars: map[string]string{
+				"network":                 "",
+				"subnet":                  "",
+				"compute_service_account": "csa",
+			},
+		},
+		{
+			name: "don't create daisy.Var when compute_service_account is empty",
+			env: EnvironmentSettings{
+				Network:               "net",
+				Subnet:                "sub",
+				ComputeServiceAccount: "",
+			},
+			originalVars: map[string]string{},
+			expectedVars: map[string]string{
+				"network": "net",
+				"subnet":  "sub",
+			},
+		},
+		{
+			name: "don't overwrite daisy.Var when compute_service_account is empty",
+			env: EnvironmentSettings{
+				Network:               "net",
+				Subnet:                "sub",
+				ComputeServiceAccount: "",
+			},
+			originalVars: map[string]string{
+				"compute_service_account": "default",
+			},
+			expectedVars: map[string]string{
+				"network":                 "net",
+				"subnet":                  "sub",
+				"compute_service_account": "default",
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			wf := daisy.Workflow{}
+			for k, v := range tt.originalVars {
+				wf.AddVar(k, v)
+			}
+			tt.env.ApplyWorkerCustomizations(&wf)
+			expectedVars := tt.expectedVars
+			assertEqualWorkflowVars(t, &wf, expectedVars)
+		})
+	}
+}
+
+func Test_ApplyToWorkflow(t *testing.T) {
+	for _, tt := range []struct {
+		name               string
+		env                EnvironmentSettings
+		original, expected *daisy.Workflow
+	}{
+		{
+			name: "always overwrite when source fields are non-empty",
+			env: EnvironmentSettings{
+				Project:         "lucky-lemur",
+				Zone:            "us-west1-c",
+				GCSPath:         "new-path",
+				OAuth:           "new-oauth",
+				Timeout:         "new-timeout",
+				ComputeEndpoint: "new-endpoint",
+			},
+			original: &daisy.Workflow{
+				Project:         "original-project",
+				Zone:            "original-zone",
+				GCSPath:         "original-path",
+				OAuthPath:       "original-oauth",
+				DefaultTimeout:  "original-timeout",
+				ComputeEndpoint: "original-endpoint",
+			},
+			expected: &daisy.Workflow{
+				Project:         "lucky-lemur",
+				Zone:            "us-west1-c",
+				GCSPath:         "new-path",
+				OAuthPath:       "new-oauth",
+				DefaultTimeout:  "new-timeout",
+				ComputeEndpoint: "new-endpoint",
+			},
+		},
+		{
+			name: "project and zone overwrite when empty",
+			env:  EnvironmentSettings{},
+			original: &daisy.Workflow{
+				Project:         "original-project",
+				Zone:            "original-zone",
+				GCSPath:         "original-path",
+				OAuthPath:       "original-oauth",
+				DefaultTimeout:  "original-timeout",
+				ComputeEndpoint: "original-endpoint",
+			},
+			expected: &daisy.Workflow{
+				GCSPath:         "original-path",
+				OAuthPath:       "original-oauth",
+				DefaultTimeout:  "original-timeout",
+				ComputeEndpoint: "original-endpoint",
+			},
+		},
+	} {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.env.ApplyToWorkflow(tt.original)
+			assert.Equal(t, tt.original, tt.expected)
+		})
+	}
+}
+
+func Test_ApplyToWorkflow_PropagatesLogging(t *testing.T) {
+	original := &daisy.Workflow{}
+
+	// ApplyToWorkflow calls methods to disable logging, which in turn updates private
+	// fields on daisy.Workflow. This test inspects private fields directly
+	// to validate that logging is disabled.
+	privateLoggingFields := []string{"gcsLoggingDisabled", "stdoutLoggingDisabled", "cloudLoggingDisabled"}
+	for _, fieldName := range privateLoggingFields {
+		realValue := reflect.ValueOf(original).Elem().FieldByName(fieldName)
+		assert.False(t, realValue.Bool(), "field: %s", fieldName)
+	}
+
+	EnvironmentSettings{
+		DisableGCSLogs:    true,
+		DisableCloudLogs:  true,
+		DisableStdoutLogs: true,
+	}.ApplyToWorkflow(original)
+
+	for _, fieldName := range privateLoggingFields {
+		realValue := reflect.ValueOf(original).Elem().FieldByName(fieldName)
+		assert.True(t, realValue.Bool(), "field: %s", fieldName)
+	}
 }
 
 func assertWorkflow(t *testing.T, w *daisy.Workflow, project string, zone string, gcsPath string,
@@ -71,7 +235,13 @@ func assertWorkflow(t *testing.T, w *daisy.Workflow, project string, zone string
 			t.Errorf("%v != %v", tt.want, tt.got)
 		}
 	}
-	if reflect.DeepEqual(w.Vars, varMap) {
-		t.Errorf("unexpected vars, want: %v, got: %v", varMap, w.Vars)
+	assertEqualWorkflowVars(t, w, varMap)
+}
+
+func assertEqualWorkflowVars(t *testing.T, wf *daisy.Workflow, expectedVars map[string]string) {
+	actualVars := map[string]string{}
+	for k, v := range wf.Vars {
+		actualVars[k] = v.Value
 	}
+	assert.Equal(t, actualVars, expectedVars)
 }

cli_tools/daisycommon/daisy_worker.go
@@ -17,6 +17,7 @@ package daisycommon
 import (
 	"context"
 
+	daisy_utils "github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/daisy"
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/logging"
 	"github.com/GoogleCloudPlatform/compute-image-tools/daisy"
 )
@@ -33,22 +34,25 @@ type DaisyWorker interface {
 // NewDaisyWorker returns an implementation of DaisyWorker. The returned value is
 // designed to be run once and discarded. In other words, don't run RunAndReadSerialValue
 // twice on the same instance.
-func NewDaisyWorker(wf *daisy.Workflow, logger logging.Logger) DaisyWorker {
-	return &defaultDaisyWorker{wf, logger}
+func NewDaisyWorker(wf *daisy.Workflow, env EnvironmentSettings, logger logging.Logger) DaisyWorker {
+	return &defaultDaisyWorker{wf, env, logger}
 }
 
 type defaultDaisyWorker struct {
 	wf     *daisy.Workflow
+	env    EnvironmentSettings
 	logger logging.Logger
 }
 
 // runAndReadSerialValue runs the daisy workflow with the supplied vars, and returns the serial
 // output value associated with the supplied key.
 func (w *defaultDaisyWorker) RunAndReadSerialValue(key string, vars map[string]string) (string, error) {
+	w.env.ApplyToWorkflow(w.wf)
+	w.env.ApplyWorkerCustomizations(w.wf)
 	for k, v := range vars {
 		w.wf.AddVar(k, v)
 	}
-	err := w.wf.Run(context.Background())
+	err := w.wf.RunWithModifiers(context.Background(), w.preValidateFunction, w.postValidateFunction)
 	if w.wf.Logger != nil {
 		for _, trace := range w.wf.Logger.ReadSerialPortLogs() {
 			w.logger.Trace(trace)
@@ -68,4 +72,12 @@ func (w *defaultDaisyWorker) Cancel(reason string) bool {
 
 	//indicate cancel was not performed
 	return false
+}
+
+func (w *defaultDaisyWorker) preValidateFunction(wf *daisy.Workflow) {
+	// no-op
+}
+
+func (w *defaultDaisyWorker) postValidateFunction(wf *daisy.Workflow) {
+	daisy_utils.UpdateAllInstanceNoExternalIP(wf, w.env.NoExternalIP)
 }

cli_tools/gce_ovf_export/domain/args_test.go
@@ -17,8 +17,9 @@ package ovfexportdomain
 import (
 	"testing"
 
-	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/daisycommon"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/daisycommon"
 )
 
 func TestIsInstanceExport(t *testing.T) {
@@ -34,12 +35,21 @@ func TestIsMachineImageExport(t *testing.T) {
 func TestDaisyAttrs(t *testing.T) {
 	params := GetAllInstanceExportArgs()
 	assert.Equal(t,
-		daisycommon.WorkflowAttributes{
-			Project: params.Project, Zone: params.Zone, GCSPath: params.ScratchBucketGcsPath,
-			OAuth: params.Oauth, Timeout: params.Timeout.String(), ComputeEndpoint: params.Ce,
-			WorkflowDirectory: params.WorkflowDir, DisableGCSLogs: params.GcsLogsDisabled,
-			DisableCloudLogs: params.CloudLogsDisabled, DisableStdoutLogs: params.StdoutLogsDisabled,
-			NoExternalIP: params.NoExternalIP,
+		daisycommon.EnvironmentSettings{
+			Project:               params.Project,
+			Zone:                  params.Zone,
+			GCSPath:               params.ScratchBucketGcsPath,
+			OAuth:                 params.Oauth,
+			Timeout:               params.Timeout.String(),
+			ComputeEndpoint:       params.Ce,
+			WorkflowDirectory:     params.WorkflowDir,
+			DisableGCSLogs:        params.GcsLogsDisabled,
+			DisableCloudLogs:      params.CloudLogsDisabled,
+			DisableStdoutLogs:     params.StdoutLogsDisabled,
+			NoExternalIP:          params.NoExternalIP,
+			Network:               params.Network,
+			Subnet:                params.Subnet,
+			ComputeServiceAccount: params.ComputeServiceAccount,
 		},
-		params.DaisyAttrs())
+		params.EnvironmentSettings())
 }

commit c02695147465fcc7d226fb4dbcf0f64688997939

Image import: Include network and subnet for inspection (#1530)
This fixes a regression from #1478 that caused the default network to be used for inspection. No affected users were detected

cli_tools/common/image/importer/api_inflater.go
@@ -26,7 +26,6 @@ import (
 	daisyUtils "github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/daisy"
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/logging"
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/storage"
-	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/daisycommon"
 	"github.com/GoogleCloudPlatform/compute-image-tools/daisy"
 	daisyCompute "github.com/GoogleCloudPlatform/compute-image-tools/daisy/compute"
 )
@@ -278,17 +277,9 @@ func (inflater *apiInflater) getCalculateChecksumWorkflow(diskURI string) *daisy
 	}
 
 	// Calculate checksum within 20min.
-	daisycommon.EnvironmentSettings{
-		Project:           inflater.request.Project,
-		Zone:              inflater.request.Zone,
-		GCSPath:           inflater.request.ScratchBucketGcsPath,
-		OAuth:             inflater.request.Oauth,
-		Timeout:           "20m",
-		ComputeEndpoint:   inflater.request.ComputeEndpoint,
-		DisableGCSLogs:    inflater.request.GcsLogsDisabled,
-		DisableCloudLogs:  inflater.request.CloudLogsDisabled,
-		DisableStdoutLogs: inflater.request.StdoutLogsDisabled,
-	}.ApplyToWorkflow(w)
+	env := inflater.request.EnvironmentSettings()
+	env.Timeout = "20m"
+	env.ApplyToWorkflow(w)
 	if inflater.request.ComputeServiceAccount != "" {
 		w.AddVar("compute_service_account", inflater.request.ComputeServiceAccount)
 	}

cli_tools/common/image/importer/request.go
@@ -109,17 +109,20 @@ type ImageImportRequest struct {
 // a daisy workflow.
 func (args ImageImportRequest) EnvironmentSettings() daisycommon.EnvironmentSettings {
 	return daisycommon.EnvironmentSettings{
-		Project:            args.Project,
-		Zone:               args.Zone,
-		GCSPath:            args.ScratchBucketGcsPath,
-		OAuth:              args.Oauth,
-		Timeout:            args.Timeout.String(),
-		ComputeEndpoint:    args.ComputeEndpoint,
-		DaisyLogLinePrefix: args.DaisyLogLinePrefix,
-		DisableGCSLogs:     args.GcsLogsDisabled,
-		DisableCloudLogs:   args.CloudLogsDisabled,
-		DisableStdoutLogs:  args.StdoutLogsDisabled,
-		NoExternalIP:       args.NoExternalIP,
-		WorkflowDirectory:  args.WorkflowDir,
+		Project:               args.Project,
+		Zone:                  args.Zone,
+		GCSPath:               args.ScratchBucketGcsPath,
+		OAuth:                 args.Oauth,
+		Timeout:               args.Timeout.String(),
+		ComputeEndpoint:       args.ComputeEndpoint,
+		DaisyLogLinePrefix:    args.DaisyLogLinePrefix,
+		DisableGCSLogs:        args.GcsLogsDisabled,
+		DisableCloudLogs:      args.CloudLogsDisabled,
+		DisableStdoutLogs:     args.StdoutLogsDisabled,
+		Network:               args.Network,
+		Subnet:                args.Subnet,
+		ComputeServiceAccount: args.ComputeServiceAccount,
+		NoExternalIP:          args.NoExternalIP,
+		WorkflowDirectory:     args.WorkflowDir,
 	}
 }

cli_tools/gce_vm_image_import/cli/args.go
@@ -26,7 +26,6 @@ import (
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/flags"
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/param"
 	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/path"
-	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/daisycommon"
 )
 
 // imageImportArgs receives arguments passed by the user and facilitates creating
@@ -205,25 +204,4 @@ func (args *imageImportArgs) registerFlags(flagSet *flag.FlagSet) {
 
 	flagSet.BoolVar(&args.SysprepWindows, "sysprep_windows", false,
 		"Generalize image using Windows Sysprep. Only applicable to Windows.")
-}
-
-// EnvironmentSettings creates an EnvironmentSettings instance from the fields
-// in this struct.
-func (args imageImportArgs) EnvironmentSettings() daisycommon.EnvironmentSettings {
-	return daisycommon.EnvironmentSettings{
-		Project:               args.Project,
-		Zone:                  args.Zone,
-		GCSPath:               args.ScratchBucketGcsPath,
-		OAuth:                 args.Oauth,
-		Timeout:               args.Timeout.String(),
-		ComputeEndpoint:       args.ComputeEndpoint,
-		DisableGCSLogs:        args.GcsLogsDisabled,
-		DisableCloudLogs:      args.CloudLogsDisabled,
-		DisableStdoutLogs:     args.StdoutLogsDisabled,
-		NoExternalIP:          args.NoExternalIP,
-		WorkflowDirectory:     args.WorkflowDir,
-		Network:               args.Network,
-		Subnet:                args.Subnet,
-		ComputeServiceAccount: args.ComputeServiceAccount,
-	}
 }

cli_tools/common/image/importer/request_test.go
@@ -206,30 +206,38 @@ func Test_validate_ChecksForConflictingArguments(t *testing.T) {
 
 func Test_EnvironmentSettings(t *testing.T) {
 	request := ImageImportRequest{
-		Project:              "panda",
-		Zone:                 "us-west",
-		ScratchBucketGcsPath: "gs://bucket/path",
-		Oauth:                "oauth-info",
-		Timeout:              time.Hour * 3,
-		ComputeEndpoint:      "endpoint-uri",
-		GcsLogsDisabled:      true,
-		CloudLogsDisabled:    true,
-		StdoutLogsDisabled:   true,
-		NoExternalIP:         true,
-		DaisyLogLinePrefix:   "disk-0",
+		Project:               "panda",
+		Zone:                  "us-west",
+		ScratchBucketGcsPath:  "gs://bucket/path",
+		Oauth:                 "oauth-info",
+		Timeout:               time.Hour * 3,
+		ComputeEndpoint:       "endpoint-uri",
+		DaisyLogLinePrefix:    "disk-0",
+		GcsLogsDisabled:       true,
+		CloudLogsDisabled:     true,
+		StdoutLogsDisabled:    true,
+		Network:               "network",
+		Subnet:                "subnet",
+		ComputeServiceAccount: "email@example.com",
+		NoExternalIP:          true,
+		WorkflowDir:           "workflow-dir",
 	}
 	expected := daisycommon.EnvironmentSettings{
-		Project:            "panda",
-		Zone:               "us-west",
-		GCSPath:            "gs://bucket/path",
-		OAuth:              "oauth-info",
-		Timeout:            "3h0m0s",
-		ComputeEndpoint:    "endpoint-uri",
-		DisableGCSLogs:     true,
-		DisableCloudLogs:   true,
-		DisableStdoutLogs:  true,
-		NoExternalIP:       true,
-		DaisyLogLinePrefix: "disk-0",
+		Project:               "panda",
+		Zone:                  "us-west",
+		GCSPath:               "gs://bucket/path",
+		OAuth:                 "oauth-info",
+		Timeout:               "3h0m0s",
+		ComputeEndpoint:       "endpoint-uri",
+		DaisyLogLinePrefix:    "disk-0",
+		DisableGCSLogs:        true,
+		DisableCloudLogs:      true,
+		DisableStdoutLogs:     true,
+		Network:               "network",
+		Subnet:                "subnet",
+		ComputeServiceAccount: "email@example.com",
+		NoExternalIP:          true,
+		WorkflowDirectory:     "workflow-dir",
 	}
 	assert.Equal(t, expected, request.EnvironmentSettings())
 }

cli_tools_tests/module/import/inflater_test.go
@@ -128,7 +128,7 @@ func runDaisyInflate(t *testing.T, fileURI string) string {
 		Timeout:     time.Hour,
 		Zone:        zone,
 	}
-	inflater, err := importer.NewDaisyInflater(request, imagefile.NewGCSInspector(), logging.NewToolLogger("TODO"))
+	inflater, err := importer.NewDaisyInflater(request, imagefile.NewGCSInspector(), logging.NewToolLogger("[test]"))
 	if err != nil {
 		t.Fatal(err)
 	}

cli_tools_tests/module/import/vpc_test.go
@@ -1,0 +1,46 @@
+//  Copyright 2021 Google Inc. All Rights Reserved.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+
+package import_test
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+
+	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/common/utils/logging"
+	"github.com/GoogleCloudPlatform/compute-image-tools/cli_tools/gce_vm_image_import/cli"
+)
+
+func Test_Import_UsesNetworkAndSubnetFromArgs(t *testing.T) {
+	t.Parallel()
+	// This project doesn't have a 'default' network. Import will fail if a worker
+	// isn't configured to use the custom network and subnet.
+	project := "compute-image-test-custom-vpc"
+	network := "projects/compute-image-test-custom-vpc/global/networks/unrestricted-egress"
+	subnet := "regions/us-central1/subnetworks/unrestricted-egress"
+	imageName := "i" + uuid.New().String()
+	err := cli.Main([]string{
+		"-image_name", imageName,
+		"-client_id", "test",
+		"-source_image", "projects/compute-image-tools-test/global/images/debian-9-translate",
+		"-project", project,
+		"-zone", "us-central1-a",
+		"-network", network,
+		"-subnet", subnet,
+	}, logging.NewToolLogger("[test]"), "../../../daisy_workflows")
+
+	assert.NoError(t, err)
+}