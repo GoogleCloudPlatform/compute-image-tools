c1bd25982b3f5010b924183edf1e7f05b72491c8
Eric Edens <ericedens@google.com>
2021-03-25 17:17:01 -0700

    import_precheck: Fail when root device spans multiple physical disks (#1581)
    Prior to this, the precheck tool falsely reported that a system was importable when the root device spanned multiple physical disks (typically due to usage of LVM).

--- cli_tools/import_precheck/check_disks_linux.go
+++ cli_tools/import_precheck/check_disks_linux.go
@@ -14,22 +14,26 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
-	"strings"
-	"unicode"
+	"sort"
 )
 
 // disksCheck performs disk configuration checking:
 // - finding the root filesystem partition
 // - checking if the device is MBR
+// - checking whether the root mount is physically located on a single disk.
+//   The check fails, for example, when the root mount is on an LVM
+//   logical volume that spans multiple disks.
 // - check for GRUB
 // - warning for any mount points from partitions from other devices
 type disksCheck struct {
 	getMBROverride func(devName string) ([]byte, error)
-	lsblkOverride  func() (string, error)
+	lsblkOverride  func() ([]byte, error)
 }
 
 func (c *disksCheck) getMBR(devName string) ([]byte, error) {
@@ -46,61 +50,173 @@ func (c *disksCheck) getMBR(devName string) ([]byte, error) {
 	return data, nil
 }
 
-func (c *disksCheck) lsblk() (string, error) {
-	cmd := exec.Command("lsblk", "-i")
-	out, err := cmd.Output()
-	if err != nil {
-		exitErr := err.(*exec.ExitError)
-		return "", fmt.Errorf("lsblk: %v, stderr: %s", err, exitErr.Stderr)
+func (c *disksCheck) readMounts() (*mountPoints, error) {
+	var lsblkOut []byte
+	var err error
+	if c.lsblkOverride != nil {
+		lsblkOut, err = c.lsblkOverride()
+	} else {
+		cmd := exec.Command("lsblk", "--json", "--output", "name,mountpoint,type")
+		lsblkOut, err = cmd.Output()
+		if err != nil {
+			var exitErr *exec.ExitError
+			if errors.As(err, &exitErr) {
+				err = fmt.Errorf("lsblk: %v, stderr: %s", err, exitErr.Stderr)
+			} else {
+				err = fmt.Errorf("lsblk: %v", err)
+			}
+		}
 	}
-	return string(out), nil
+
+	if err != nil {
+		return nil, err
+	}
+	parsed := &LSBLKOutput{}
+	err = json.Unmarshal(lsblkOut, parsed)
+	if err != nil {
+		return nil, err
+	}
+	mounts := &mountPoints{}
+	mounts.addAll(parsed.Blockdevices, []string{})
+	return mounts, nil
 }
 
 func (c *disksCheck) getName() string {
 	return "Disks Check"
 }
 
-func (c *disksCheck) run() (*report, error) {
-	r := &report{name: c.getName()}
-	var out string
-	var err error
-	if c.lsblkOverride != nil {
-		out, err = c.lsblkOverride()
-	} else {
-		out, err = c.lsblk()
+// mountPoints supports listing the mounts on a system, and determining which block device(s)
+// the mount is physically contained. This is helpful since technologies such as LVM allow
+// logical mounts to span physical devices.
+//
+// The data structure is modeled as a one-to-many inverse index of mountDir and its associated
+// device hierarchy. For example, given the following device tree:
+//
+//  /dev/sda
+//        ∟ sda1
+//            ∟ logical-volume ->  /
+//  /dev/sdb
+//        ∟ sdb2
+//            ∟ logical-volume ->  /
+//
+//  mounts would contain two entries:
+//
+//    {/, [sda, sda1, logical-volume]}
+//    {/, [sdb, sdb2, logical-volume]}
+type mountPoints struct {
+	mounts []mountPoint
+}
+
+// mountPoint describes the device hierarchy for a mounted directory.
+// The hierarchy starts at the physical device, and contains an entry for each
+// partition or logical volume.
+type mountPoint struct {
+	dir       string
+	hierarchy []string
+}
+
+// getPhysicalDevice returns the physical device of this mountPoint.
+func (m *mountPoint) getPhysicalDevice() string {
+	return m.hierarchy[0]
+}
+
+// listPhysicalDevicesForMount returns the the block device(s) that contain the mountDir.
+// An empty return value means the mountDir was not found.
+func (m *mountPoints) listPhysicalDevicesForMount(mountDir string) (devices []string) {
+	set := map[string]struct{}{}
+	for _, mp := range m.mounts {
+		if mp.dir == mountDir {
+			set[mp.getPhysicalDevice()] = struct{}{}
+		}
 	}
+	for k := range set {
+		devices = append(devices, k)
+	}
+	// sorted for stability in testing.
+	sort.Strings(devices)
+	return devices
+}
+
+// listMountedDirectories returns all mounted directories.
+func (m *mountPoints) listMountedDirectories() (mountedDirectories []string) {
+	set := map[string]struct{}{}
+	for _, mount := range m.mounts {
+		set[mount.dir] = struct{}{}
+	}
+	for k := range set {
+		mountedDirectories = append(mountedDirectories, k)
+	}
+	// sorted for stability in testing.
+	sort.Strings(mountedDirectories)
+	return mountedDirectories
+}
+
+// addAll populates the mountPoints data structure with the response from lsblk.
+func (m *mountPoints) addAll(elements []DiskElement, basePath []string) {
+	for _, element := range elements {
+		path := make([]string, len(basePath))
+		copy(path, basePath)
+		path = append(path, element.Name)
+		if element.Mountpoint != "" {
+			m.mounts = append(m.mounts, mountPoint{element.Mountpoint, path})
+		}
+		if len(element.Children) > 0 {
+			m.addAll(element.Children, path)
+		}
+	}
+}
+
+func (c *disksCheck) run() (r *report, err error) {
+	r = &report{name: c.getName()}
+
+	allMounts, err := c.readMounts()
 	if err != nil {
-		return nil, err
-	}
-
-	l := lsblkParse(out)
-
-	r.Info(fmt.Sprintf("`lsblk -i` results:\n%s", l.raw))
-	if len(l.devRows) != 1 {
-		r.Warn("translation only supports single block device. Will attempt to determine if translation is possible...")
-	}
-
-	if l.rootDev == "" {
-		r.Fatal("root filesystem partition not found on any block devices.")
+		r.Warn(fmt.Sprintf("Failed to execute lsblk: %s", err))
 		return r, nil
 	}
-	r.Info(fmt.Sprintf("root filesystem on device %q partition %q", l.rootDev, l.rootPart))
 
-	for dev, rows := range l.devRows {
-		for _, row := range rows {
-			if row["MOUNTPOINT"] != "" && dev != l.rootDev {
-				format := "partition %s (%s) is not on the root device %s and will be OMITTED from translation."
-				r.Warn(fmt.Sprintf(format, row["NAME"], row["MOUNTPOINT"], l.rootDev))
+	rootDevices := allMounts.listPhysicalDevicesForMount("/")
+	switch len(rootDevices) {
+	case 0:
+		r.Fatal("root filesystem partition not found on any block devices.")
+		return r, nil
+	case 1:
+		r.Info(fmt.Sprintf("root filesystem found on device: %s", rootDevices[0]))
+	default:
+		format := "root filesystem spans multiple block devices (%s). Typically this occurs when an LVM logical " +
+			"volume spans multiple block devices. Image import only supports single block device."
+		r.Fatal(fmt.Sprintf(format, rootDevices))
+		return r, nil
+	}
+
+	rootDevice := rootDevices[0]
+	for _, mountDir := range allMounts.listMountedDirectories() {
+		if mountDir == "/" {
+			continue
+		}
+		devices := allMounts.listPhysicalDevicesForMount(mountDir)
+		switch len(devices) {
+		case 0:
+			// This implies a bug in mountPoints.addAll.
+			panic(fmt.Sprintf("Invalid parse of mount %s", mountDir))
+		case 1:
+			// devices[0] is the only physical device that contains this mountDir.
+			if devices[0] != rootDevice {
+				format := "mount %s is not on the root device %s and will be OMITTED from image import."
+				r.Warn(fmt.Sprintf(format, mountDir, rootDevice))
 			}
+		default:
+			format := "mount %s is on multiple physical devices (%s) and will be OMITTED from image import."
+			r.Warn(fmt.Sprintf(format, mountDir, devices))
 		}
 	}
 
 	// MBR checking.
 	var mbrData []byte
 	if c.getMBROverride != nil {
-		mbrData, err = c.getMBROverride(l.rootDev)
+		mbrData, err = c.getMBROverride(rootDevice)
 	} else {
-		mbrData, err = c.getMBR(l.rootDev)
+		mbrData, err = c.getMBR(rootDevice)
 	}
 	if err != nil {
 		return nil, err
@@ -119,89 +235,17 @@ func (c *disksCheck) run() (*report, error) {
 	return r, nil
 }
 
-type lsblk struct {
-	schema   []lsblkSchemaCol
-	devRows  map[string][]lsblkRow
-	rootDev  string
-	rootPart string
-	rows     []lsblkRow
-	raw      string
+// LSBLKOutput is a struct representing the output from running
+//  `lsblk --json`.
+type LSBLKOutput struct {
+	Blockdevices []DiskElement `json:"blockdevices"`
 }
 
-type lsblkRow map[string]string
-
-type lsblkSchemaCol struct {
-	name       string
-	start, end int
-}
-
-func (l *lsblk) parse(content string) {
-	l.raw = content
-	lines := strings.Split(content, "\n")
-
-	// Block device and partition mount checking.
-	// devs is a list of devices/partitions. Map of device name -> device/partitions data
-	// Slice schema: NAME, MAJ:MIN, SIZE, RO, TYPE, MOUNTPOINT.
-	l.schema = lsblkSchema(lines[0])
-	var curDev string
-	for _, line := range lines[1:] {
-		if line == "" {
-			continue
-		}
-		row := lsblkRow{}
-		for i, col := range l.schema {
-			var val string
-			if col.start >= len(line) {
-				val = ""
-			} else if col.end >= len(line) {
-				val = line[col.start:]
-			} else {
-				val = line[col.start:col.end]
-			}
-			val = strings.TrimSpace(val)
-			if i == 0 {
-				val = strings.TrimLeft(val, "|`- ")
-			}
-			row[string(col.name)] = val
-		}
-		if unicode.IsLetter(rune(line[0])) {
-			curDev = row["NAME"]
-		}
-		if row["MOUNTPOINT"] == "/" {
-			l.rootDev = curDev
-			l.rootPart = row["NAME"]
-		}
-
-		if l.devRows == nil {
-			l.devRows = map[string][]lsblkRow{curDev: {row}}
-		} else if _, ok := l.devRows[curDev]; !ok {
-			l.devRows[curDev] = []lsblkRow{row}
-		} else {
-			l.devRows[curDev] = append(l.devRows[curDev], row)
-		}
-		l.rows = append(l.rows, row)
-	}
-}
-
-func lsblkParse(output string) lsblk {
-	l := lsblk{}
-	l.parse(output)
-	return l
-}
-
-func lsblkSchema(header string) []lsblkSchemaCol {
-	var cols []lsblkSchemaCol
-	start := 0
-	for i, c := range header {
-		isSpace := unicode.IsSpace(c)
-		next := i + 1
-		if next == len(header) || isSpace && !unicode.IsSpace(rune(header[next])) {
-			// Start of next word (or end of header). Finish this current column. Set start to next word.
-			name := strings.TrimSpace(header[start:next])
-			col := lsblkSchemaCol{name, start, next}
-			cols = append(cols, col)
-			start = next
-		}
-	}
-	return cols
+// DiskElement is a struct representing the nested fields within
+// the output of `lsblk --json`. See the testdata directory for
+// examples of nesting.
+type DiskElement struct {
+	Name       string        `json:"name"`
+	Mountpoint string        `json:"mountpoint"`
+	Children   []DiskElement `json:"children"`
 }
