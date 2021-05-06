bae4e096afa21326f77a4f8c615017d84641dbe6
Brian Katyl <bxk@google.com>
2021-05-06 13:12:04 -0600

    Update compute.go

--- daisy/compute/compute.go
+++ daisy/compute/compute.go
@@ -219,14 +219,13 @@ func shouldRetryWithWait(tripper http.RoundTripper, err error, multiplier int) b
 		}
 	}
 
-
 	apiErr, ok := err.(*googleapi.Error)
 	var retry bool
 	switch {
 	case !ok && (strings.Contains(err.Error(), "connection reset by peer") || strings.Contains(err.Error(), "unexpected EOF")):
 		retry = true
 	case !ok && (strings.Contains(err.Error(), "server sent GOAWAY") || strings.Contains(err.Error(), "ENHANCE_YOUR_CALM")):
-		// The wait operation can return GOAWAY/ENHANCE_YOUR_CALM messages,
+		// The wait operation can return GOAWAY/ENHANCE_YOUR_CALM messages, so doubling the wait multiplier as it based on the retry count.
 		multiplier = multiplier * 2
 		retry = true
 	case !ok && tkValid:
