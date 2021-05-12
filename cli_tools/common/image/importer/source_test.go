fb5398397f769efc11f37c185875bfabe099050a
Brian Katyl <bxk@google.com>
2021-05-11 21:10:44 -0600

    Revert "Updating CLI Tools dependencies (#1623)" (#1632)
    This reverts commit 1c11180a83b421a75be192f84ab8645e466979d9.

--- cli_tools/common/image/importer/source_test.go
+++ cli_tools/common/image/importer/source_test.go
@@ -35,7 +35,7 @@ func TestEmptyFilesAreRejected(t *testing.T) {
 
 	fileContent := ""
 
-	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent, true))
+	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent))
 	_, err := factory.Init(source.Path(), "")
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "cannot import an image from an empty file")
@@ -50,7 +50,7 @@ func TestGzipCompressedFilesAreRejected(t *testing.T) {
 
 	fileContent := test.CreateCompressedFile()
 
-	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent, true))
+	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent))
 	_, err := factory.Init(source.Path(), "")
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "the input file is a gzip file, which is not supported")
@@ -65,7 +65,7 @@ func TestUncompressedFilesAreAllowed(t *testing.T) {
 
 	fileContent := "fileContent"
 
-	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent, true))
+	factory := NewSourceFactory(createMockStorageClient(t, source, fileContent))
 	result, err := factory.Init(source.Path(), "")
 	assert.NoError(t, err)
 	assert.Equal(t, result, source)
@@ -81,11 +81,10 @@ func TestGcsFilePathMustBeFullyQualified(t *testing.T) {
 
 func TestEnsureEitherFileOrImageIsPresent(t *testing.T) {
 	var cases = []struct {
-		name           string
-		file           fileSource
-		image          string
-		valid          bool
-		verifyFileRead bool
+		name  string
+		file  fileSource
+		image string
+		valid bool
 	}{
 		{
 			name: "both",
@@ -94,14 +93,12 @@ func TestEnsureEitherFileOrImageIsPresent(t *testing.T) {
 				bucket:  "bucket",
 				object:  "global/images/ubuntu-1604",
 			},
-			image:          "global/images/ubuntu-1604",
-			valid:          false,
-			verifyFileRead: false,
+			image: "global/images/ubuntu-1604",
+			valid: false,
 		},
 		{
-			name:           "neither",
-			valid:          false,
-			verifyFileRead: false,
+			name:  "neither",
+			valid: false,
 		},
 		{
 			name: "only file",
@@ -110,20 +107,18 @@ func TestEnsureEitherFileOrImageIsPresent(t *testing.T) {
 				bucket:  "bucket",
 				object:  "global/images/ubuntu-1604",
 			},
-			valid:          true,
-			verifyFileRead: true,
+			valid: true,
 		},
 		{
-			name:           "only image",
-			image:          "global/images/ubuntu-1604",
-			valid:          true,
-			verifyFileRead: false,
+			name:  "only image",
+			image: "global/images/ubuntu-1604",
+			valid: true,
 		},
 	}
 
 	for _, tt := range cases {
 		t.Run(tt.name, func(t *testing.T) {
-			factory := NewSourceFactory(createMockStorageClient(t, tt.file, "file-content", tt.verifyFileRead))
+			factory := NewSourceFactory(createMockStorageClient(t, tt.file, "file-content"))
 			_, err := factory.Init(tt.file.Path(), tt.image)
 			if tt.valid {
 				assert.NoError(t, err)
@@ -176,13 +171,11 @@ func TestImagePathIsValidated(t *testing.T) {
 	}
 }
 
-func createMockStorageClient(t *testing.T, filePath fileSource, fileContent string, testExpectations bool) *mocks.MockStorageClientInterface {
+func createMockStorageClient(t *testing.T, filePath fileSource, fileContent string) *mocks.MockStorageClientInterface {
 	mockCtrl := gomock.NewController(t)
 	mockStorageObject := mocks.NewMockStorageObject(mockCtrl)
+	mockStorageObject.EXPECT().NewReader().Return(ioutil.NopCloser(strings.NewReader(fileContent)), nil)
 	mockStorageClient := mocks.NewMockStorageClientInterface(mockCtrl)
-	if testExpectations {
-		mockStorageObject.EXPECT().NewReader().Return(ioutil.NopCloser(strings.NewReader(fileContent)), nil)
-		mockStorageClient.EXPECT().GetObject(filePath.bucket, filePath.object).Return(mockStorageObject)
-	}
+	mockStorageClient.EXPECT().GetObject(filePath.bucket, filePath.object).Return(mockStorageObject)
 	return mockStorageClient
 }
