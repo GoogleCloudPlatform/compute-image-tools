6f34ad5d37a40a7c45274e35126a1a3c186b5a3d
eeaton <ellioteaton@gmail.com>
2021-07-15 14:57:10 +0100

    Update export_disk_ext.sh
    Added a more robust test condition and error message for both Storage and Compute endpoints.
    
    This test removes the original curl to the Discovery API, which can be accessed without authentication but tests access to an unrelated API.
    Instead, this test now checks HTTP Statuscodes for these endpoints. Without specific parameters, the endpoints will return 4XX; that is acceptable because we're yet not testing permission to reach a specific resource. Returning any status code equal to or greater than 200 indicates that at least the VM can communicate to this endpoint.
    
    I've also changed the error message to be more useful. The previous message over-emphasized Private Google Access, which made troubleshooting difficult.

--- daisy_workflows/export/export_disk_ext.sh
+++ daisy_workflows/export/export_disk_ext.sh
@@ -18,12 +18,17 @@ function serialOutputPrefixedKeyValue() {
   stdbuf -oL echo "$1: <serial-output key:'$2' value:'$3'>"
 }
 
-# Verify VM has network access to Storage API by checking a public bucket
-gsutil ls &> /dev/null;
-if [[ $? -ne 0 ]]; then
-  echo "ExportFailed: Cannot access Google APIs. Ensure that VPC settings allow VMs to access Google APIs either via external IP or Private Google Access. More info at: https://cloud.google.com/vpc/docs/configure-private-google-access"
-  exit
-fi
+# Verify VM has network access to Compute and Storage API endpoints
+for endpoint in "https://compute.googleapis.com" "https://storage.googleapis.com"
+do
+  STATUSCODE=$(curl --head --silent --write-out '%{http_code}'  --output /dev/null $endpoint)
+  if [ $STATUSCODE -ge 200 ] ; then
+    continue
+  else
+    echo "ExportFailed: Cannot access Google APIs at $endpoint. Ensure that VPC settings, including DNS, Firewall Rules, and Private Google Access, allow VMs to access Google APIs."
+    exit
+  fi
+done
 
 BYTES_1GB=1073741824
 URL="http://metadata/computeMetadata/v1/instance/attributes"
