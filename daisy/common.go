GoogleCloudPlatform/compute-image-tools

commit 8d54747d234c6ee14e6b83094733ad35c50d4725

Daisy: Recursively build child workflows in NewFromFile when path doesn't have variable. (#1701)
This adds support for recursively reading child workflows. The feature was first added in #1693, but rolled back in #1700 due to a bug that it introduced.

The issue: Variable instantiate occurs during populate, and callers expect that behavior. Callers also expect to define child workflow names using variables. Those are in conflict if we want to read child workflows before populate.

To address this, I added a fix in eacb97a where child workflows are eagerly read only when they're fully specified. If the filename contains a variable, then the legacy behavior of reading the workflow during the step's populate is retained.

daisy/common.go
@@ -29,6 +29,10 @@ import (
 	"google.golang.org/api/compute/v1"
 )
 
+var (
+	varPattern = regexp.MustCompile(`\$\{.*\}`)
+)
+
 func getUser() string {
 	if cu, err := user.Current(); err == nil {
 		return cu.Username
@@ -124,6 +128,11 @@ func substitute(v reflect.Value, replacer *strings.Replacer) {
 	})
 }
 
+// hasVariableDeclaration determines whether s contains a variable declaration of the style `${varname}`
+func hasVariableDeclaration(s string) bool {
+	return varPattern.MatchString(s)
+}
+
 func getRegionFromZone(z string) string {
 	if z != "" {
 		return z[:len(z)-2]
@@ -159,6 +168,17 @@ func (w *Workflow) substituteSourceVars(ctx context.Context, v reflect.Value) DE
 	})
 }
 
+// traverseAction allows callers of traverseData to customize the function's traversal.
+type traverseAction uint
+
+const (
+	// Continue the traversal; this is the default action of
+	// traverseData, which traverses to all nodes.
+	continueTraversal traverseAction = iota
+	// Do not process this node or any of its children.
+	prune
+)
+
 // traverseData traverses complex data structures and runs
 // a function, f, on its basic data types.
 // Traverses arrays, maps, slices, and public fields of structs.
@@ -166,10 +186,20 @@ func (w *Workflow) substituteSourceVars(ctx context.Context, v reflect.Value) DE
 // Slices, maps, and structs will not have f called on them, but will
 // traverse their subelements.
 // Errors returned from f will be returned by traverseDataStructure.
-func traverseData(v reflect.Value, f func(reflect.Value) DError) DError {
+// actions allows the caller to determine which action to take at a node.
+// The default action is 'continueTraverse'.
+func traverseData(v reflect.Value, f func(reflect.Value) DError, actions ...func(reflect.Value) traverseAction) DError {
+
 	if !v.CanSet() {
 		// Don't run on private fields.
 		return nil
+	}
+
+	for _, action := range actions {
+		switch action(v) {
+		case prune:
+			return nil
+		}
 	}
 
 	switch v.Kind() {
@@ -180,13 +210,13 @@ func traverseData(v reflect.Value, f func(reflect.Value) DError) DError {
 			return nil
 		}
 		// I'm a pointer, dereference me.
-		return traverseData(v.Elem(), f)
+		return traverseData(v.Elem(), f, actions...)
 	}
 
 	switch v.Kind() {
 	case reflect.Array, reflect.Slice:
 		for i := 0; i < v.Len(); i++ {
-			if err := traverseData(v.Index(i), f); err != nil {
+			if err := traverseData(v.Index(i), f, actions...); err != nil {
 				return err
 			}
 		}
@@ -201,10 +231,10 @@ func traverseData(v reflect.Value, f func(reflect.Value) DError) DError {
 			newKv.Set(kv)
 			newVv := reflect.New(vv.Type()).Elem()
 			newVv.Set(vv)
-			if err := traverseData(newKv, f); err != nil {
+			if err := traverseData(newKv, f, actions...); err != nil {
 				return err
 			}
-			if err := traverseData(newVv, f); err != nil {
+			if err := traverseData(newVv, f, actions...); err != nil {
 				return err
 			}
 
@@ -215,7 +245,7 @@ func traverseData(v reflect.Value, f func(reflect.Value) DError) DError {
 		}
 	case reflect.Struct:
 		for i := 0; i < v.NumField(); i++ {
-			if err := traverseData(v.Field(i), f); err != nil {
+			if err := traverseData(v.Field(i), f, actions...); err != nil {
 				return err
 			}
 		}

daisy/workflow_test.go
@@ -32,6 +32,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/storage"
+	"github.com/stretchr/testify/assert"
 	computeAlpha "google.golang.org/api/compute/v0.alpha"
 	computeBeta "google.golang.org/api/compute/v0.beta"
 	"google.golang.org/api/compute/v1"
@@ -236,6 +237,65 @@ func TestNewFromFileError(t *testing.T) {
 			t.Errorf("did not get expected error from NewFromFile():\ngot: %q\nwant: %q", err.Error(), tt.error)
 		}
 	}
+}
+
+func TestNewIncludedWorkflowFromFile_UsesResourcesFromParent(t *testing.T) {
+	parent := New()
+	parent.workflowDir = "./test_data"
+	included, err := parent.NewIncludedWorkflowFromFile("TestNewIncludedWorkflowFromFile_UsesResourcesFromParent.wf.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	assertEqual(t, parent.Cancel, included.Cancel, "Cancel")
+	assertEqual(t, parent, included.parent, "parent")
+	assertEqual(t, parent.disks, included.disks, "disks")
+	assertEqual(t, parent.forwardingRules, included.forwardingRules, "forwardingRules")
+	assertEqual(t, parent.images, included.images, "images")
+	assertEqual(t, parent.machineImages, included.machineImages, "machineImages")
+	assertEqual(t, parent.instances, included.instances, "instances")
+	assertEqual(t, parent.networks, included.networks, "networks")
+	assertEqual(t, parent.subnetworks, included.subnetworks, "subnetworks")
+	assertEqual(t, parent.targetInstances, included.targetInstances, "targetInstances")
+	assertEqual(t, parent.snapshots, included.snapshots, "snapshots")
+	assertEqual(t, parent.objects, included.objects, "objects")
+}
+
+func assertEqual(t *testing.T, expected, actual interface{}, msg string) {
+	t.Helper()
+	if expected != actual {
+		t.Error(msg)
+	}
+}
+
+func TestNewFromFile_ReadsChildWorkflows(t *testing.T) {
+	parent, derr := NewFromFile("./test_data/TestNewFromFile_ReadsChildWorkflows.parent.wf.json")
+	if derr != nil {
+		t.Fatal(derr)
+	}
+
+	// Included Workflow
+	includedStep1 := parent.Steps["include-workflow"].IncludeWorkflow
+	assert.NotNil(t, includedStep1, "NewFromFile should read and parse included workflow")
+	assert.Equal(t, map[string]string{
+		"k1": "v1",
+	}, includedStep1.Vars, "included workflow should have variables that were declared in its step in the parent.")
+	includedStep2 := includedStep1.Workflow.Steps["include-workflow"].IncludeWorkflow
+	assert.NotNil(t, includedStep2, "NewFromFile should read and parse included workflows recursively")
+	assert.Equal(t, map[string]string{
+		"k3": "v3",
+	}, includedStep2.Vars, "included workflow should have variables that were declared in its step in the parent.")
+
+	// Sub Workflow
+	subStep1 := parent.Steps["sub-workflow"].SubWorkflow
+	assert.NotNil(t, subStep1, "NewFromFile should read and parse included workflow")
+	assert.Equal(t, map[string]string{
+		"k2": "v2",
+	}, subStep1.Vars, "sub workflow should have variables that were declared in its step in the parent.")
+	subStep2 := subStep1.Workflow.Steps["sub-workflow"].SubWorkflow
+	assert.NotNil(t, subStep2, "NewFromFile should read and parse sub workflows recursively")
+	assert.Equal(t, map[string]string{
+		"k4": "v4",
+	}, subStep2.Vars, "sub workflow should have variables that were declared in its step in the parent.")
 }
 
 func TestNewFromFile(t *testing.T) {
@@ -470,24 +530,6 @@ func TestNewFromFile(t *testing.T) {
 				}},
 			},
 		},
-		"include-workflow": {
-			name: "include-workflow",
-			IncludeWorkflow: &IncludeWorkflow{
-				Vars: map[string]string{
-					"key": "value",
-				},
-				Path: "./test_sub.wf.json",
-			},
-		},
-		"sub-workflow": {
-			name: "sub-workflow",
-			SubWorkflow: &SubWorkflow{
-				Vars: map[string]string{
-					"key": "value",
-				},
-				Path: "./test_sub.wf.json",
-			},
-		},
 	}
 	want.Dependencies = map[string][]string{
 		"create-disks":          {},
@@ -498,8 +540,6 @@ func TestNewFromFile(t *testing.T) {
 		"create-image-locality": {"postinstall-stopped"},
 		"create-image":          {"create-image-locality"},
 		"create-machine-image":  {"create-image"},
-		"include-workflow":      {"create-image"},
-		"sub-workflow":          {"create-image"},
 	}
 
 	for _, s := range want.Steps {
@@ -529,6 +569,53 @@ func TestNewStep(t *testing.T) {
 	}
 	if err == nil {
 		t.Error("should have erred, but didn't")
+	}
+}
+
+func TestNewFromFile_SupportsNestedVariables(t *testing.T) {
+	cases := []struct {
+		name     string
+		template string
+	}{
+		{"Variable in filename", "./test_data/TestNewFromFile_SupportsNestedVariables_VarInFilename.parent.wf.json"},
+		{"No variable in filename", "./test_data/TestNewFromFile_SupportsNestedVariables.parent.wf.json"}}
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			ctx := context.Background()
+			client, err := newTestGCSClient()
+			if err != nil {
+				t.Fatal(err)
+			}
+			td, err := ioutil.TempDir(os.TempDir(), "")
+			if err != nil {
+				t.Fatalf("error creating temp dir: %v", err)
+			}
+			defer os.RemoveAll(td)
+			tf := filepath.Join(td, "test.cred")
+			if err := ioutil.WriteFile(tf, []byte(`{ "type": "service_account" }`), 0600); err != nil {
+				t.Fatalf("error creating temp file: %v", err)
+			}
+
+			wf, err := NewFromFile(tt.template)
+			if err != nil {
+				t.Fatal(err)
+			}
+			wf.Zone = "wf-zone"
+			wf.Project = "bar-project"
+			wf.OAuthPath = tf
+			wf.Logger = &MockLogger{}
+			wf.StorageClient = client
+			wf.externalLogging = true
+
+			err = wf.populate(ctx)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			child := wf.Steps["include-workflow"].IncludeWorkflow
+			assert.Equal(t, "v1", child.Vars["k1"])
+			assert.Equal(t, "image-v1", (*child.Workflow.Steps["create-disks"].CreateDisks)[0].SourceImage)
+		})
 	}
 }
 

daisy/common_test.go
@@ -74,6 +74,27 @@ func TestMinInt(t *testing.T) {
 	}
 }
 
+func TestHasVariableDeclaration(t *testing.T) {
+	tests := []struct {
+		desc string
+		s    string
+		want bool
+	}{
+		{"no declaration", "content", false},
+		{"no declaration: empty string", "", false},
+		{"no declaration: only dollar", "$var", false},
+		{"no declaration: no closing bracket", "{", false},
+		{"contains declaration", "content ${k}", true},
+		{"contains declaration: source", "${SOURCE: fname}", true},
+	}
+
+	for _, tt := range tests {
+		if got := hasVariableDeclaration(tt.s); got != tt.want {
+			t.Errorf("%s: %v != %v", tt.desc, got, tt.want)
+		}
+	}
+}
+
 func TestRandString(t *testing.T) {
 	for i := 0; i < 10; i++ {
 		l := len(randString(i))

daisy/step_includeworkflow.go
@@ -33,7 +33,9 @@ type IncludeWorkflow struct {
 }
 
 func (i *IncludeWorkflow) populate(ctx context.Context, s *Step) DError {
-	if i.Path != "" {
+	// Typically s.Workflow is instantiated when the parent workflow is read in NewFromFile.
+	// Workflow could be nil when the parent workflow is constructed manually using Go structs.
+	if i.Path != "" && i.Workflow == nil {
 		var err error
 		if i.Workflow, err = s.w.NewIncludedWorkflowFromFile(i.Path); err != nil {
 			return newErr("failed to parse duration for step includeworkflow", err)
@@ -97,6 +99,14 @@ Loop:
 	}
 	substitute(reflect.ValueOf(i.Workflow).Elem(), strings.NewReplacer(replacements...))
 
+	for name, st := range i.Workflow.Steps {
+		st.name = name
+		st.w = i.Workflow
+		if err := st.w.populateStep(ctx, st); err != nil {
+			return err
+		}
+	}
+
 	// We do this here, and not in validate, as embedded startup scripts could
 	// have what we think are daisy variables.
 	if err := i.Workflow.validateVarsSubbed(); err != nil {
@@ -105,14 +115,6 @@ Loop:
 
 	if err := i.Workflow.substituteSourceVars(ctx, reflect.ValueOf(i.Workflow).Elem()); err != nil {
 		return err
-	}
-
-	for name, st := range i.Workflow.Steps {
-		st.name = name
-		st.w = i.Workflow
-		if err := st.w.populateStep(ctx, st); err != nil {
-			return err
-		}
 	}
 
 	// Copy Sources up to parent resolving relative paths as we go.

daisy/step_includeworkflow_test.go
@@ -107,7 +107,21 @@ func TestIncludeWorkflowPopulate(t *testing.T) {
 	}
 }
 
-func TestIncludeWorkflowRun(t *testing.T) {}
+func TestIncludeWorkflowPopulate_SkipsReadingPathWhenWorkflowNil(t *testing.T) {
+	child := testWorkflow()
+	parent := testWorkflow()
+	parent.Steps = map[string]*Step{
+		"child": {
+			IncludeWorkflow: &IncludeWorkflow{
+				Path:     "test-will-fail-if-this-is-read",
+				Workflow: child,
+			},
+		},
+	}
+	if err := parent.populate(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+}
 
 func TestIncludeWorkflowValidate(t *testing.T) {
 	ctx := context.Background()

daisy/step_sub_workflow.go
@@ -27,7 +27,9 @@ type SubWorkflow struct {
 }
 
 func (s *SubWorkflow) populate(ctx context.Context, st *Step) DError {
-	if s.Path != "" {
+	// Typically s.Workflow is instantiated when the parent workflow is read in NewFromFile.
+	// Workflow could be nil when the parent workflow is constructed manually using Go structs.
+	if s.Path != "" && s.Workflow == nil {
 		var err error
 		if s.Workflow, err = st.w.NewSubWorkflowFromFile(s.Path); err != nil {
 			return ToDError(err)

daisy/step_sub_workflow_test.go
@@ -58,6 +58,22 @@ func TestSubWorkflowPopulate(t *testing.T) {
 	}
 }
 
+func TestSubWorkflowPopulate_SkipsReadingPathWhenWorkflowNil(t *testing.T) {
+	child := testWorkflow()
+	parent := testWorkflow()
+	parent.Steps = map[string]*Step{
+		"child": {
+			SubWorkflow: &SubWorkflow{
+				Path:     "test-will-fail-if-this-is-read",
+				Workflow: child,
+			},
+		},
+	}
+	if err := parent.populate(context.Background()); err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestSubWorkflowRun(t *testing.T) {
 	ctx := context.Background()
 	w := testWorkflow()

daisy/validate.go
@@ -114,5 +114,11 @@ func (w *Workflow) validateVarsSubbed() DError {
 			}
 		}
 		return nil
+	}, func(v reflect.Value) traverseAction {
+		_, ok := v.Interface().(*Workflow)
+		if ok {
+			return prune
+		}
+		return continueTraversal
 	})
 }

commit cd9c48d8d47e93e92e898719480c7cda4f2d35d2

Daisy: Don't clobber child variables from parent workflow (#1704)
This fixes a regression from #1701 where child workflows were receiving the autovars from their parents.

daisy/common.go
@@ -125,6 +125,12 @@ func substitute(v reflect.Value, replacer *strings.Replacer) {
 			val.SetString(replacer.Replace(val.String()))
 		}
 		return nil
+	}, func(v reflect.Value) traverseAction {
+		_, ok := v.Interface().(*Workflow)
+		if ok {
+			return prune
+		}
+		return continueTraversal
 	})
 }
 

daisy/workflow_test.go
@@ -614,7 +614,7 @@ func TestNewFromFile_SupportsNestedVariables(t *testing.T) {
 
 			child := wf.Steps["include-workflow"].IncludeWorkflow
 			assert.Equal(t, "v1", child.Vars["k1"])
-			assert.Equal(t, "image-v1", (*child.Workflow.Steps["create-disks"].CreateDisks)[0].SourceImage)
+			assert.Equal(t, "include-workflow-image-v1", (*child.Workflow.Steps["create-disks"].CreateDisks)[0].SourceImage)
 		})
 	}
 }