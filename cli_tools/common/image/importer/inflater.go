773ecd2e865306a0e8b84724a2a6421e22699a64
Tony Zhang <tzz@google.com>
2021-11-17 22:47:14 -0800

    remove debug code

--- cli_tools/common/image/importer/inflater.go
+++ cli_tools/common/image/importer/inflater.go
@@ -303,11 +303,6 @@ func (facade *shadowTestInflaterFacade) compareWithShadowInflater(mainPd, shadow
 		result = fmt.Sprintf(matchFormat, sizeGbMatch, sourceGbMatch, contentMatch, qemuChecksumMatch)
 	}
 
-	fmt.Println("1>>>", mainIi.checksum)
-	fmt.Println("2>>>", facade.qemuChecksum)
-	fmt.Println("3>>>", shadowIi.checksum)
-	fmt.Println("r>>>", result)
-
 	return result
 }
 
