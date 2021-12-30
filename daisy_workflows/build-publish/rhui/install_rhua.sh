9e1e1764a1ad849e6b06c5e6a30141b7ee27626a
Liam Hopkins <liamh@google.com>
2021-12-30 14:13:38 -0800

    v1 for rhua workflow

--- daisy_workflows/build-publish/rhui/install_rhua.sh
+++ daisy_workflows/build-publish/rhui/install_rhua.sh
@@ -1,52 +1,72 @@
 #!/bin/bash
 
 set -e
+set -x
 echo "BuildStatus: Starting RHUA setup."
 
-SRC_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/attributes/daisy-sources-path)
-RH_USER=$(gcloud secrets versions access latest --secret rh_user)
-RH_PASS=$(gcloud secrets versions access latest --secret rh_pass)
-
 # Get sources from daisy workflow.
-mkdir /root/daisy_sources
-gsutil cp "${SRC_PATH}/" /root/daisy_sources/
+SRC_PATH=$(curl -f -H Metadata-Flavor:Google http://169.254.169.254/computeMetadata/v1/instance/attributes/daisy-sources-path)
+tempdir=$(mktemp -d /tmp/daisy-rhuaXXX)
+gsutil cp "${SRC_PATH}/*" $tempdir/
+gcloud secrets versions access latest --secret enrollment_cert > \
+  $tempdir/enrollment_cert.pem
+gcloud secrets versions access latest --secret entitlement_cert > \
+  $tempdir/entitlement_cert.pem
 
 # Get subscription manager from RHUIv3 hosted repo
 dnf --disablerepo='*' --enablerepo='rhui-rhel-8-for-x86_64-baseos-rhui-rpms' \
-  install subscription-manager
+  install -y subscription-manager
 
 # Remove RHUI config pointing to RHUIv3
 rpm -e google-rhui-client-rhel8
 
-# Register as a RHUI
-# TODO: This won't work without identity
-subscription-manager register --type=rhui --name=rhua-installer \
-  --user=$RH_USER --password $RH_PASSWORD
-#  --consumerid=cf64bd4c-0e1c-407b-a5c5-969768ff6d13
-# consumer ID means the literal same registration, just transferred to a new
-# host. only works if there's only one build happening at a time. precludes the
-# need to attach subscription by poolid
-
-# Attach RHUI subscription
-subscription-manager attach --pool=8a85f9a17c71102f017d05dbd9f72ee9
+# Download and import subscription certificate.
+subscription-manager import --certificate=$tempdir/enrollment_cert.pem
 
 # Enable repos for installing RHUA.
-subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rhui-rpms \
-  --enable=rhel-8-for-x86_64-appstream-rhui-rpms
+subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rhui-rpms
+subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rhui-rpms
 subscription-manager repos --enable=rhui-4-for-rhel-8-x86_64-rpms
 
+# Permit root login for Ansible
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
+sed -i"" -re '/PermitRootLogin/d' /etc/ssh/sshd_config
+systemctl restart sshd.service
+
 # Get and run rhui-installer.
-# TODO: add our SSL certs
-dnf install rhui-installer
-rhui-installer --answers-file /root/daisy_sources/answers.yaml
+dnf install -y rhui-installer
+rhui-installer -u root --answers-file $tempdir/answers.yaml
+dnf remove -y rhui-installer
 
-# Unregister so we don't use up licenses.
-subscription-manager unregister
+# Remove enrollment cert and RHUI repos from final image.
+subscription-manager remove --all
 
-# Add our repos.
-rhui-manager --noninteractive repo add_by_repo \
-  --repo_ids `paste -d "," /root/daisy_sources/reponames.txt`
+# Add content cert and managed repos.
+password=$(awk '/password/ { print $NF }' /etc/rhui/rhui-subscription-sync.conf)
+rhui-manager --noninteractive --user admin --password "$password" cert upload \
+  --cert $tempdir/entitlement_cert.pem
+rhui-manager --noninteractive --user admin --password "$password" repo \
+  add_by_repo --repo_ids $(paste -sd "," "${tempdir}/reponames.txt")
 
-# TODO: add NFS entry to fstab
+# Stop pulp services, which may hold open the mount point.
+systemctl stop 'pulpcore-worker@*' pulpcore-{api,content,resource-manager}
 
-echo BuildSuccess: RHUA software is installed and configured
+# Remove rhui-installer added fake mount point.
+umount /var/lib/rhui/remote_share
+sed -i"" -re '/remote_share/d' /etc/fstab
+
+# Add NFS entry to fstab.
+cat $tempdir/fstab >> /etc/fstab
+
+# Add NFS dependency to pulp units
+for unit in pulpcore-{worker@,api,content,resource-manager}; do
+  unitdir="/etc/systemd/system/${unit}.service.d"
+  [[ -d $unitdir ]] || mkdir $unitdir
+  cp $tempdir/depend-nfs.conf $unitdir/
+done
+systemctl daemon-reload
+
+# Delete installer resources.
+rm -rf $tempdir
+mv -f /etc/ssh/sshd_config.bak /etc/ssh/sshd_config
+echo BuildSuccess: RHUA setup complete.
