e474246d8178a2dadb96772a86f4a1e412f16ce8
Liam Hopkins <liamh@google.com>
2022-01-01 18:33:20 -0800

    use patched Ansible playbook

--- daisy_workflows/build-publish/rhui/install_rhua.sh
+++ daisy_workflows/build-publish/rhui/install_rhua.sh
@@ -1,26 +1,77 @@
 #!/bin/bash
 
-set -e
-set -x
-echo "BuildStatus: Starting RHUA setup."
+
+function build_success() {
+  echo "BuildSuccess: $@"
+  exit 0
+}
+
+function build_status() {
+  echo "BuildStatus: $@"
+}
+
+function build_fail() {
+  echo "BuildFailed: $@"
+  exit 1
+}
+
+function exit_error() {
+  build_fail "$0:$1 \"$BASH_COMMAND\" returned $?"
+}
+
+trap 'exit_error $LINENO' ERR
+
+build_status "Starting RHUA setup."
 
 # Get sources from daisy workflow.
-SRC_PATH=$(curl -f -H Metadata-Flavor:Google http://169.254.169.254/computeMetadata/v1/instance/attributes/daisy-sources-path)
+MDS_URL="http://169.254.169.254/computeMetadata/v1"
+SRC_PATH=$(curl -f -H Metadata-Flavor:Google \
+  ${MDS_URL}/instance/attributes/daisy-sources-path)
 tempdir=$(mktemp -d /tmp/daisy-rhuaXXX)
-gsutil cp "${SRC_PATH}/*" $tempdir/
+gsutil cp "${SRC_PATH}/rhua_artifacts/*" $tempdir/
+
+# Get secrets.
 gcloud secrets versions access latest --secret enrollment_cert > \
   $tempdir/enrollment_cert.pem
 gcloud secrets versions access latest --secret entitlement_cert > \
   $tempdir/entitlement_cert.pem
+gcloud secrets versions access latest --secret rhua_tls_cert > \
+  $tempdir/rhua.crt
+gcloud secrets versions access latest --secret rhua_tls_key > \
+  $tempdir/rhua.key
 
-# Get subscription manager from RHUIv3 hosted repo
+# RHUA node does not need the CA; it won't be issuing any new certs.
+# Not providing files causes new CA to be generated unnecessarily.
+echo "DUMMY CA KEY" > $tempdir/rhua_ca.key
+echo "DUMMY CA CERT" > $tempdir/rhua_ca.crt
+
+# Add cert entries to answers.yaml
+# We defined the tasks which use the new user_supplied_tls_{crt,key} fields in
+# rhua.patch. Indentation is important, we are appending to the 'rhua' mapping.
+cat >>$tempdir/answers.yaml <<EOF
+  user_supplied_ca_crt: $tempdir/rhua_ca.crt
+  user_supplied_ca_key: $tempdir/rhua_ca.key
+  user_supplied_tls_crt: $tempdir/rhua.crt
+  user_supplied_tls_key: $tempdir/rhua.key
+EOF
+
+# Generate 'secret' answers in a separate file as top level scalars (mimic
+# rhui-installer behavior).
+rhuipassword=$(openssl rand -hex 16)
+cat >>$tempdir/secret_answers.yaml <<EOF
+rhui_active_login_file: null
+rhui_manager_password: $rhuipassword
+rhui_manager_password_changed: true
+EOF
+
+# Get subscription manager from RHUI
 dnf --disablerepo='*' --enablerepo='rhui-rhel-8-for-x86_64-baseos-rhui-rpms' \
   install -y subscription-manager
 
 # Remove RHUI config pointing to RHUIv3
 rpm -e google-rhui-client-rhel8
 
-# Download and import subscription certificate.
+# Import subscription certificate.
 subscription-manager import --certificate=$tempdir/enrollment_cert.pem
 
 # Enable repos for installing RHUA.
@@ -28,37 +79,37 @@ subscription-manager repos --enable=rhel-8-for-x86_64-baseos-rhui-rpms
 subscription-manager repos --enable=rhel-8-for-x86_64-appstream-rhui-rpms
 subscription-manager repos --enable=rhui-4-for-rhel-8-x86_64-rpms
 
-# Permit root login for Ansible
-cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
-sed -i"" -re '/PermitRootLogin/d' /etc/ssh/sshd_config
-systemctl restart sshd.service
+# Get rhui-installer and patch Ansible playbook
+# The patch skips actually mounting NFS (update fstab only), and skips
+# generating a unique RHUA cert, instead copying our pre-generated cert.
+dnf install -y rhui-installer patch
+( cd /usr/share/rhui-installer; patch -b -p0 < $tempdir/rhua.patch; )
 
-# Get and run rhui-installer.
-dnf install -y rhui-installer
-rhui-installer -u root --answers-file $tempdir/answers.yaml
+# We don't use rhui-installer as it doesn't allow us to extend the answers file
+# and it assumes install-over-SSH. Instead, invoke ansible-playbook directly.
+build_status "Run Ansible playbook."
+ansible-playbook \
+  -i localhost, \
+  --extra-vars @$tempdir/answers.yaml \
+  --extra-vars @$tempdir/secret_answers.yaml \
+  /usr/share/rhui-installer/playbooks/rhua-provision.yml
 dnf remove -y rhui-installer
 
 # Remove enrollment cert and RHUI repos from final image.
 subscription-manager remove --all
 
 # Add content cert and managed repos.
+build_status "Add repos to RHUA."
 password=$(awk '/password/ { print $NF }' /etc/rhui/rhui-subscription-sync.conf)
 rhui-manager --noninteractive --user admin --password "$password" cert upload \
   --cert $tempdir/entitlement_cert.pem
 rhui-manager --noninteractive --user admin --password "$password" repo \
   add_by_repo --repo_ids $(paste -sd "," "${tempdir}/reponames.txt")
 
-# Stop pulp services, which may hold open the mount point.
-systemctl stop 'pulpcore-worker@*' pulpcore-{api,content,resource-manager}
-
-# Remove rhui-installer added fake mount point.
-umount /var/lib/rhui/remote_share
-sed -i"" -re '/remote_share/d' /etc/fstab
-
-# Add NFS entry to fstab.
-cat $tempdir/fstab >> /etc/fstab
-
 # Add NFS dependency to pulp units
+# We do this instead of patching the Ansible templates, as we need the
+# services up to run the above rhui-manager commands and the modification
+# prevents starting without NFS.
 for unit in pulpcore-{worker@,api,content,resource-manager}; do
   unitdir="/etc/systemd/system/${unit}.service.d"
   [[ -d $unitdir ]] || mkdir $unitdir
@@ -67,6 +118,8 @@ done
 systemctl daemon-reload
 
 # Delete installer resources.
+cp $tempdir/answers.yaml /root/.rhui/answers.yaml  # Expected to be found here.
 rm -rf $tempdir
-mv -f /etc/ssh/sshd_config.bak /etc/ssh/sshd_config
-echo BuildSuccess: RHUA setup complete.
+rm -rf /root/.ssh
+
+build_success "RHUA setup complete."
