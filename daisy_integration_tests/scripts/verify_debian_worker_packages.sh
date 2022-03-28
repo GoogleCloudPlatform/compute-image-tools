add53e17c6f20bb5a622e50a9bb09c9799367fef
Tony Zhang <tzz@google.com>
2022-03-28 15:13:35 -0700

    e2e test for debian-11-worker-arm64

---
+++ daisy_integration_tests/scripts/verify_debian_worker_packages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -o xtrace
+
+# This script checks whether debian worker packages are installed correctly.
+
+if ! qemu-img --version; then
+  echo "FAILED JO2Pd99h4qRK5HIpc5NP: qemu-utils"
+  exit
+fi
+
+if ! rsync --version; then
+  echo "FAILED JO2Pd99h4qRK5HIpc5NP: rsync"
+  exit
+fi
+
+if ! python3 --version; then
+  echo "FAILED JO2Pd99h4qRK5HIpc5NP: python3"
+  exit
+fi
+
+echo "SUCCESS JO2Pd99h4qRK5HIpc5NP"
