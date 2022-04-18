fba4414852fc4849e1dd55a9fd81628d45bfea92
Liam Hopkins <liamh@google.com>
2022-04-18 13:48:18 -0700

    Update RHUI health checks (#1905)
    * add service healthchecks
    
    * plugin refactor
    
    * flake8
    
    * add dave to OWNERS

--- daisy_workflows/build-publish/rhui/health_check.py
+++ daisy_workflows/build-publish/rhui/health_check.py
@@ -17,79 +17,118 @@ import argparse
 import logging
 import logging.handlers
 import os
+import subprocess
 import sys
 import tempfile
-import typing
+
+NFS_MOUNT = "/var/lib/rhui/remote_share"
 
 
-class HealthChecks:
-  nfs_mount: str
+class HealthCheck:
 
-  def __init__(
-      self,
-      nfs_mount: str,
-  ) -> None:
-    self.nfs_mount = nfs_mount
+  # Node types the health check supports; defaults to both types.
+  nodes = ["cds", "rhua"]
 
-  def mount_uses_nfs(self, logger: logging.Logger):
-    logger.info("checking that %s is mounted as NFS", self.nfs_mount)
+  def __init__(self):
+    self.logger = logging.getLogger(self.__class__.__name__)
+
+  def run(self, node: str):
+    if node in self.nodes:
+      self._run(node)
+
+
+class MountIsNFS(HealthCheck):
+
+  mount = NFS_MOUNT
+
+  def _run(self, node: str):
+    self.logger.info("checking that %s is mounted as NFS", self.mount)
     with open("/proc/mounts") as mounts:
-      for line in mounts:
-        parts = line.split()
-        mnt_point, mnt_type = parts[1], parts[2]
-        if mnt_point == self.nfs_mount:
-          if mnt_type == "nfs":
-            return
-          else:
-            raise EnvironmentError("Not mounted as nfs. mount: %s" % line)
-    raise EnvironmentError("%s not in /proc/mounts" % self.nfs_mount)
+      if f"{self.mount} nfs rw" not in mounts.read():
+        raise EnvironmentError("%s is not NFS or not mounted" % self.mount)
 
-  def mount_is_writable(self, logger: logging.Logger):
-    logger.info("checking that %s is writable", self.nfs_mount)
-    with tempfile.TemporaryFile(dir=self.nfs_mount) as f:
+
+class MountIsWritable(HealthCheck):
+
+  mount = NFS_MOUNT
+
+  def _run(self, node: str):
+    self.logger.info("checking that %s is writable", self.mount)
+    with tempfile.TemporaryFile(dir=self.mount) as f:
       f.write(b"content")
 
-  def mount_is_readable(self, logger: logging.Logger):
-    logger.info("checking that %s is readable", self.nfs_mount)
-    os.listdir(self.nfs_mount)
+
+class MountIsReadable(HealthCheck):
+
+  mount = NFS_MOUNT
+
+  def _run(self, node: str):
+    self.logger.info("checking that %s is readable", self.mount)
+    os.listdir(self.mount)
 
 
-def main(node_type: str, result_file: typing.TextIO, nfs_mount: str):
-  """Executes health check for node_type, and writes the overall
-  result to result_file ('OK' if all checks pass, otherwise 'ERROR').
-  """
-  result_file.truncate()
-  health_checks = HealthChecks(nfs_mount=nfs_mount)
-  checks = [
-    health_checks.mount_is_readable,
-    health_checks.mount_uses_nfs,
-  ]
-  if node_type == "rhua":
-    checks += [
-      health_checks.mount_is_writable,
-    ]
-  success = True
-  for func in checks:
-    logger = logging.getLogger(func.__name__)
-    try:
-      func(logger=logger)
-      logger.info("success")
-    except Exception as e:
-      success = False
-      logger.error(e)
-  if success:
-    result_file.write("HEALTHY\n")
-  else:
-    result_file.write("ERROR\n")
+class ServicesAreActive(HealthCheck):
+
+  cds_services = ["gunicorn-auth",
+                  "gunicorn-mirror",
+                  "gunicorn-content_manager",
+                  "nginx"]
+
+  rhua_services = ["postgresql",
+                   "redis",
+                   "pulpcore-api",
+                   "pulpcore-content",
+                   "pulpcore-resource-manager",
+                   "pulpcore-worker@1",
+                   "pulpcore-worker@2",
+                   "pulpcore-worker@3",
+                   "pulpcore-worker@4",
+                   "pulpcore-worker@5",
+                   "pulpcore-worker@6",
+                   "pulpcore-worker@7",
+                   "pulpcore-worker@8",
+                   "nginx"]
+
+  def _run(self, node: str):
+    self.logger.info("checking that %s services are active", node)
+    if node == "cds":
+      services = self.cds_services
+    if node == "rhua":
+      services = self.rhua_services
+    for service in services:
+      subprocess.run(["systemctl", "is-active", service],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     check=True)
 
 
-if __name__ == "__main__":
+class HealthCheckSuite:
+
+  checks = MountIsNFS, MountIsWritable, MountIsReadable, ServicesAreActive
+
+  def __init__(self, node: str):
+    """Executes health checks for node"""
+    self.run(node)
+
+  def run(self, node: str):
+    for check_type in self.checks:
+      check = check_type()
+      try:
+        check.run(node)
+        check.logger.info("success")
+      except Exception as e:
+        check.logger.error(e)
+        return False
+
+    return True
+
+
+def main():
   parser = argparse.ArgumentParser(description="Run health checks.")
   parser.add_argument(
     "--node",
     dest="node",
     type=str,
-    nargs=1,
     required=True,
     choices=["cds", "rhua"],
     help="type of node where check is running",
@@ -101,37 +140,21 @@ if __name__ == "__main__":
     type=argparse.FileType("w"),
     help="file to write result",
   )
-  parser.add_argument(
-    "--nfs_mount",
-    dest="nfs_mount",
-    type=str,
-    nargs=1,
-    required=True,
-    help="directory of NFS mount",
-  )
-  parser.add_argument(
-    "--log",
-    dest="log",
-    type=str,
-    nargs=1,
-    choices=["stdout", "syslog"],
-    default="syslog",
-    help="where to write logs",
-  )
   args = parser.parse_args()
-  if args.log[0] == "stdout":
-    handler = logging.StreamHandler(sys.stdout)
-  else:
-    handler = logging.handlers.SysLogHandler(address="/dev/log")
-    # syslog parses on the colon to determine the tag for the message.
-    handler.ident = "rhui-health-check: "
+
+  handler = logging.StreamHandler(sys.stdout)
   logging.basicConfig(
     level=logging.DEBUG,
     format="%(name)s - %(levelname)s - %(message)s",
     handlers=[handler],
   )
-  main(
-    node_type=args.node[0],
-    result_file=args.result_file,
-    nfs_mount=args.nfs_mount[0],
-  )
+
+  args.result_file.truncate()
+  if HealthCheckSuite(args.node):
+    args.result_file.write("HEALTHY\n")
+  else:
+    args.result_file.write("ERROR\n")
+
+
+if __name__ == "__main__":
+  main()
