31fec52b3f70d50ed5a229f1b154c1a5340bfe15
Anthony Lu <koln@google.com>
2022-11-28 20:33:41 +0000

    Disk export file name test change

--- image_test/sbom/test_sbom_export.sh
+++ image_test/sbom/test_sbom_export.sh
@@ -1,10 +1,10 @@
 URL="http://metadata/computeMetadata/v1/instance/attributes"
 SBOM_FILE_NAME=$(curl -f -H Metadata-Flavor:Google ${URL}/sbom-file-name)
 OUTS_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/outs-path)
-
+DISK_FILE_NAME=$(curl -f -H Metadata-Flavor:Google ${URL}/disk-file-name)
 
 GCS_PATH_SBOM=${OUTS_PATH}/${SBOM_FILE_NAME}
-GCS_PATH_OUTDISK=${OUTS_PATH}/export-image.tar.gz
+GCS_PATH_OUTDISK=${OUTS_PATH}/${DISK_FILE_NAME}
 # check if tar.gz file is there
 # check if sbom file is there
 # potentially run other script for sbom contents
