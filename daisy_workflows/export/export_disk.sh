60b4b0bab10680c0f648fa55508dced6ad32b979
Anthony Lu <koln@google.com>
2023-05-22 22:19:02 +0000

    Input param implementation for sbomutil sbom generation

--- daisy_workflows/export/export_disk.sh
+++ daisy_workflows/export/export_disk.sh
@@ -63,8 +63,11 @@ serialOutputPrefixedKeyValue "GCEExport" "target-size-gb" "${TARGET_SIZE_GB}"
 DESTINATION=$(curl -f -H Metadata-Flavor:Google ${URL}/destination)
 # Local sbom outspath
 SBOM_PATH=$(curl -f -H Metadata-Flavor:Google ${URL}/sbom-path)
+# References the executable file for sbom_util in the sources path.
+SBOM_UTIL_EXECUTABLE=$(curl -f -H Metadata-Flavor:Google ${URL}/sbom-util-executable)
 # User passed in value for sbom-util source. If empty, do not run sbom generation.
 SBOM_UTIL_SOURCE=$(curl -f -H Metadata-Flavor:Google ${URL}/sbom-util-source)
+
 # References the tar-gz for syft, if SBOM generation will run. 
 SYFT_TAR_FILE=$(curl -f -H Metadata-Flavor:Google ${URL}/syft-tar-file)
 # User passed in value for syft source, if empty then do not run SBOM generation.
@@ -81,7 +84,8 @@ function runSBOMGeneration() {
   mount -o bind,ro /dev /mnt/dev
   EXECUTION_MODE=$1
   if [ $EXECUTION_MODE == $SBOM_UTIL_EXECUTION_MODE ]; then
-    echo "ExportFailed: sbom-util sbom generation not yet implemented" 
+    gsutil cp $SBOM_UTIL_EXECUTABLE sbomutil
+    ./sbomutil --archetype=linux-image --comp_name=$SOURCE_DISK_NAME --output=./image.sbom.json
   elif [ $EXECUTION_MODE == $SYFT_EXECUTION_MODE ]; then
     gsutil cp $SYFT_TAR_FILE syft.tar.gz
     tar -xf syft.tar.gz
