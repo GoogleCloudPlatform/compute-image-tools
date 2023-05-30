cc70fd86c3f229560087b71a8df88d19e3c758ab
Zach Marano <zmarano@google.com>
2023-05-30 13:43:04 -0700

    Debian build fixes for Bookworm (#2155)
    * Re-factor our hostname configuration for upstream.
    
    * Fix for missing DNS search domains.

--- daisy_workflows/image_build/debian/build_fai.py
+++ daisy_workflows/image_build/debian/build_fai.py
@@ -53,7 +53,7 @@ def main():
   # Get Parameters.
   build_date = utils.GetMetadataAttribute(
       'build_date', raise_on_not_found=True)
-  debian_cloud_images_version = '695ae0c8660d5bbfdb4e01f4a5cc2bacbc06783b'
+  debian_cloud_images_version = 'dc1efc3d60666560042ea1ef4069a8984525e5b9'
   debian_version = utils.GetMetadataAttribute(
       'debian_version', raise_on_not_found=True)
   outs_path = utils.GetMetadataAttribute('daisy-outs-path',
@@ -89,8 +89,9 @@ def main():
   # Remove upstream test cases that won't work here.
   os.remove(config_space + 'hooks/tests.BASE')
 
-  # Remove static hostname file
-  os.remove(config_space + 'files/etc/hostname/CLOUD')
+  # Remove upstream netplan config until we can use it.
+  if debian_version == 'bookworm':
+    os.remove(config_space + 'files/etc/netplan/90-default.yaml/GCE')
 
   # Copy our classes to the FAI config space
   mycopytree('/files/fai_config', config_space)
