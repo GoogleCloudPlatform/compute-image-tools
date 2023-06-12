e90408aceb0320a829a084530b1f0a4f813c5a1d
Anthony Lu <koln@google.com>
2023-06-12 17:51:39 +0000

    remove multithreading for single file upload on gsutil

--- daisy_workflows/image_build/windows/bootstrap_install.ps1
+++ daisy_workflows/image_build/windows/bootstrap_install.ps1
@@ -120,7 +120,7 @@ function Generate-Sbom {
 
   Write-Output "Generating sbom."
   & "${script:components_dir}\sbomutil.exe" -archetype=windows-image -googet_path 'D:\ProgramData\GooGet' -extra_content="${script:components_dir}\windows.iso","${script:$script:driver_dir}\","${script:components_dir}\SetupComplete.cmd" -comp_name="${comp_name}" -output image.sbom.json
-  & 'gsutil' -m cp image.sbom.json $gs_path
+  & 'gsutil' cp image.sbom.json $gs_path
   Write-Output "Sbom file uploaded to $gs_path."
 }
 
