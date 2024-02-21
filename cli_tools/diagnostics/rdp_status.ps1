916ef2743beecdc364e43ab5d4193e7a355288cb
Eyram Ekpe <ekpeeyram@yahoo.com>
2024-02-21 19:04:54 +0100

    Update rdp_status.ps1 (#2245)
    * Update rdp_status.ps1
    
    * Update rdp_status.ps1
    
    * Update rdp_status.ps1
    
    * Update rdp_status.ps1
    
    * Update rdp_status.ps1

--- cli_tools/diagnostics/rdp_status.ps1
+++ cli_tools/diagnostics/rdp_status.ps1
@@ -3,8 +3,20 @@ $RDPServerPath = 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server'
 
 $items= @(
   @{
+    description="Verify that the Ethernet adapter is active, expect [Enabled]:";
+    cmd="netsh interface show interface"
+  },
+  @{
+    description="Verify that DHCP is enabled and IP configuration is correct, expect [DHCP Enabled: Yes]:";
+    cmd="netsh interface ipv4 show addresses"
+  },
+  @{
     description="Check the Remote Desktop Service status, expect [Running]:";
     cmd="(Get-Service -Name TermService).Status"
+  },
+  @{
+    description="Verify that Remote Desktop Session Host is enabled for multi-user connection, expect [installed]:";
+    cmd="(Get-WindowsFeature -Name RDS-RD-Server).InstallState"
   },
   @{
     description="Check that Remote Connections are enabled, expect [fDenyTSConnections: 0]:";
@@ -25,6 +37,14 @@ $items= @(
   @{
     description="Verify that MTU size is no greater than 1460, expect [MTU <= 1460, Interface:Ethernet]:";
     cmd="netsh interface ipv4 show subinterfaces"
+  },
+  @{
+    description="Verify that client-server seecurity negotiation is set to default value, expect [SecurityLayer REG_DWORD 0x0, 0x1, 0x2] depending on Security Layer configuration:";
+    cmd="reg query 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' /v SecurityLayer"
+  },
+  @{
+    description="Verify that Network-level UserAuthentication is set to default value, expect [UserAuthentication REG_DWORD 0x0 or 0x1]:";
+    cmd="reg query 'HKEY_LOCAL_MACHINE\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' /v UserAuthentication"
   }
 )
 
