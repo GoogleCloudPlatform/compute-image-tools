2acc0239fda8f56374bfc46d0d55cc168468efb5
Sejal Sharma <sejalsharma@google.com>
2024-04-12 19:23:36 +0000

    Add file header comment

--- daisy_workflows/image_build/install_package/cos/package_replacement/disable_readonly_rootfs.sh
+++ daisy_workflows/image_build/install_package/cos/package_replacement/disable_readonly_rootfs.sh
@@ -6,15 +6,20 @@ set -o errexit
 set -o pipefail
 set -x
 
+# Located in COS images, contains the func "enable_rw_mount".
+# https://chromium.googlesource.com/chromiumos/platform/vboot_reference/+/master/scripts/image_signing/common_minimal.sh
 source /usr/share/vboot/bin/common_minimal.sh
 
 # This procedure may or may not trigger an immediate reboot. Cos-customizer is
 # designed to re-run (and only from) the script that triggered the reboot, so
 # it is important that this procedure handles re-entry gracefully.
 disable_vboot() {
+
+  # Make a temp dir.
   local dir
   dir="$(mktemp -d)"
 
+  # Mount EFI on the temp dir and modify the grub.cfg file...
   mount /dev/disk/by-label/EFI-SYSTEM "${dir}"
   grub="${dir}/efi/boot/grub.cfg"
   if ! grep "defaultA=0" "${grub}" || ! grep "defaultB=1" "${grub}"; then
@@ -29,6 +34,8 @@ disable_vboot() {
     sync
   fi
 
+  # Call enable_rw_mount function, it is responsible for enabling the read-write
+  # mount and is sourced at the top of the file.
   local -r rootdev="$(rootdev -s)"
   sudo blockdev --setrw "${rootdev}"
   enable_rw_mount "${rootdev}"
