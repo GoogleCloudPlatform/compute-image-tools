52960bee5d434429a50188edb1fcc0796c2ff3b8
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-09-10 15:11:01 -0700

    Remove branched files
    Moved them one level up

--- daisy_workflows/image_build/accelerator_images/ubuntu/build-ubuntu-2204-accelerated.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -x
-export DEBIAN_FRONTEND=noninteractive
-apt update -y || echo "BuildFailure"
-apt upgrade -y || echo "BuildFailure"
-# DKMS - not suitable for prod
-apt -y install nvidia-driver-550-server rdma-core || echo "BuildFailure"
-echo "BuildSuccess"
