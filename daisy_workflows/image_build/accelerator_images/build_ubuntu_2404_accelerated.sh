e2cdb650f77e0e576c345c6377c052b1edc79b69
Amelia Crate <acrate@google.com>
2024-09-19 11:54:01 -0700

    Add a script to enable nvidia repos on ubuntu

--- daisy_workflows/image_build/accelerator_images/build_ubuntu_2404_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_ubuntu_2404_accelerated.sh
@@ -5,4 +5,11 @@ apt update -y || echo "BuildFailure"
 apt upgrade -y || echo "BuildFailure"
 # DKMS - not suitable for prod
 apt -y install nvidia-driver-550-server rdma-core || echo "BuildFailure"
+tee /usr/bin/add-nvidia-repositories << EOF
+#!/bin/bash
+set -e
+curl https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2404/x86_64/cuda-keyring_1.1-1_all.deb -o /tmp/cuda-keyring_1.1-1_all.deb
+dpkg -i /tmp/cuda-keyring_1.1-1_all.deb
+EOF
+chmod +x /usr/bin/add-nvidia-repositories || echo "BuildFailure"
 echo "BuildSuccess"
