d793d9f11820f510bb1eb85c182133a9612870fe
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2024-09-20 13:54:30 -0700

    Include mlx5_ib in ubuntu accelerators (#2362)

--- daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
@@ -5,6 +5,8 @@ apt update -y || echo "BuildFailure"
 apt upgrade -y || echo "BuildFailure"
 # DKMS - not suitable for prod
 apt -y install nvidia-driver-550-server rdma-core || echo "BuildFailure"
+# This is only for mlx5_ib - once that's in the main package stop including it
+apt install linux-modules-extra-$(uname -r) || echo "BuildFailure"
 tee /usr/bin/add-nvidia-repositories << EOF
 #!/bin/bash
 set -e
