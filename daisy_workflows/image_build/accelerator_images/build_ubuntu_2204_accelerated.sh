88e471d4cfb5f2066fdcd6c71d390737a7b78df9
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-09-20 14:49:30 -0700

    Fix build errors in accelerator images (#2364)
    * Add -y to apt command added to ubuntu
    * Fix image name reference in Rocky

--- daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
@@ -6,7 +6,7 @@ apt upgrade -y || echo "BuildFailure"
 # DKMS - not suitable for prod
 apt -y install nvidia-driver-550-server rdma-core || echo "BuildFailure"
 # This is only for mlx5_ib - once that's in the main package stop including it
-apt install linux-modules-extra-$(uname -r) || echo "BuildFailure"
+apt -y install linux-modules-extra-$(uname -r) || echo "BuildFailure"
 tee /usr/bin/add-nvidia-repositories << EOF
 #!/bin/bash
 set -e
