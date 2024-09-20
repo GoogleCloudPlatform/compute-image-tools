6434162612cd1d923ddf90ff64da4c13ff53fadf
James Jerger <35977367+jjerger@users.noreply.github.com>
2024-09-20 15:09:39 -0700

    Drop nvidia-smi from build script
    We aren't building with GPU machines so this is guaranteed to fail

--- daisy_workflows/image_build/accelerator_images/build_rocky_linux_8_optimized_gcp_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_rocky_linux_8_optimized_gcp_accelerated.sh
@@ -6,5 +6,4 @@ chmod +x ./nvidia.run || echo "BuildFailure"
 # DKMS - not suitable for prod
 ./nvidia.run -s --kernel-source-path=/usr/src/kernels/4.18.0-553.8.1.el8_10.cloud.0.1.x86_64/ || echo "BuildFailure"
 dnf install -y rdma-core || echo "BuildFailure"
-nvidia-smi || echo "BuildFailure"
 echo "BuildSuccess"
