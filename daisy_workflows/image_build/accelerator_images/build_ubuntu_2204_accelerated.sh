003164c96f9ca0cf223643774d59714dd9c22016
Amelia Crate <95060558+a-crate@users.noreply.github.com>
2024-09-24 10:03:34 -0700

    Install ubuntu-drivers-common and the precompiled nvidia driver. (#2367)
    * Install ubuntu-drivers-common and the precompiled nvidia driver.
    
    * Clean up when adding nvidia repositories

--- daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_ubuntu_2204_accelerated.sh
@@ -4,7 +4,7 @@ export DEBIAN_FRONTEND=noninteractive
 apt update -y || echo "BuildFailure"
 apt upgrade -y || echo "BuildFailure"
 # DKMS - not suitable for prod
-apt -y install nvidia-driver-550-server rdma-core || echo "BuildFailure"
+apt -y install ubuntu-drivers-common linux-modules-nvidia-550-server-gcp rdma-core || echo "BuildFailure"
 # This is only for mlx5_ib - once that's in the main package stop including it
 apt -y install linux-modules-extra-$(uname -r) || echo "BuildFailure"
 tee /usr/bin/add-nvidia-repositories << EOF
@@ -12,6 +12,7 @@ tee /usr/bin/add-nvidia-repositories << EOF
 set -e
 curl https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2204/x86_64/cuda-keyring_1.1-1_all.deb -o /tmp/cuda-keyring_1.1-1_all.deb
 dpkg -i /tmp/cuda-keyring_1.1-1_all.deb
+rm -f /tmp/cuda-keyring_1.1-1_all.deb
 EOF
 chmod +x /usr/bin/add-nvidia-repositories || echo "BuildFailure"
 echo "BuildSuccess"
