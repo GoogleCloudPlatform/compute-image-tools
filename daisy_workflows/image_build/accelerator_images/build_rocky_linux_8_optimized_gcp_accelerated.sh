3b87b0b1f08a854b86156746ef3b72671e0e1acd
Amelia Crate <acrate@google.com>
2024-10-03 16:50:14 -0700

    Fix kernel source path in accelerator scripts

--- daisy_workflows/image_build/accelerator_images/build_rocky_linux_8_optimized_gcp_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_rocky_linux_8_optimized_gcp_accelerated.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-rhel8.repo || echo "BuildFailure"
-dnf install -y gcc make kernel-devel || echo "BuildFailure"
+dnf install -y gcc make kernel-devel kernel || echo "BuildFailure"
+test -f /var/tmp/kernel-upgrade-done || sh -c 'touch /var/tmp/kernel-upgrade-done && sync && reboot'
 curl -L -o nvidia.run https://us.download.nvidia.com/tesla/550.90.12/NVIDIA-Linux-x86_64-550.90.12.run || echo "BuildFailure"
 chmod +x ./nvidia.run || echo "BuildFailure"
 # DKMS - not suitable for prod
-./nvidia.run -s --kernel-source-path=/usr/src/kernels/4.18.0-553.8.1.el8_10.cloud.0.1.x86_64/ || echo "BuildFailure"
+./nvidia.run -s --kernel-source-path=/usr/src/kernels/$(uname -r)/ || echo "BuildFailure"
 dnf install -y createrepo gdb-headless libtool autoconf rpm-build kernel-rpm-macros patch automake wget lsof tk gcc-gfortran tcl pciutils || echo "BuildFailure"
 wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-3.2.2.0/MLNX_OFED_LINUX-23.10-3.2.2.0-rhel8.9-x86_64.tgz || echo "BuildFailure"
 tar xf MLNX_OFED_LINUX-23.10-3.2.2.0-rhel8.9-x86_64.tgz || echo "BuildFailure"
--- daisy_workflows/image_build/accelerator_images/build_rocky_linux_9_optimized_gcp_accelerated.sh
+++ daisy_workflows/image_build/accelerator_images/build_rocky_linux_9_optimized_gcp_accelerated.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 dnf config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel9/x86_64/cuda-rhel9.repo || echo "BuildFailure"
-dnf install -y gcc make kernel-devel || echo "BuildFailure"
+dnf install -y gcc make kernel-devel kernel || echo "BuildFailure"
+test -f /var/tmp/kernel-upgrade-done || sh -c 'touch /var/tmp/kernel-upgrade-done && sync && reboot'
 curl -L -o nvidia.run https://us.download.nvidia.com/tesla/550.90.12/NVIDIA-Linux-x86_64-550.90.12.run || echo "BuildFailure"
 chmod +x ./nvidia.run || echo "BuildFailure"
 # DKMS - not suitable for prod
-./nvidia.run -s --kernel-source-path=/usr/src/kernels/5.14.0-427.28.1.el9_4.cloud.1.0.x86_64/ || echo "BuildFailure"
+./nvidia.run -s --kernel-source-path=/usr/src/kernels/$(uname -r)/ || echo "BuildFailure"
 dnf install -y perl-File-Find perl-File-Copy perl-File-Compare perl-sigtrap wget lsof tk gcc-gfortran tcl pciutils || echo "BuildFailure"
 wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-3.2.2.0/MLNX_OFED_LINUX-23.10-3.2.2.0-rhel9.4-x86_64.tgz || echo "BuildFailure"
 tar xf MLNX_OFED_LINUX-23.10-3.2.2.0-rhel9.4-x86_64.tgz || echo "BuildFailure"
