0f9746e15a817d36a26c0e68921305f50f8a1be0
Brian Katyl <bxk@google.com>
2024-11-12 09:40:06 -0700

    Update post_install.ps1
    Reverting time server address and GVNIC GQ for all due to a bug.

--- daisy_workflows/image_build/windows/post_install.ps1
+++ daisy_workflows/image_build/windows/post_install.ps1
@@ -302,7 +302,7 @@ function Setup-NTP {
   Run-Command $w32tm /register
 
   # Get time from GCE NTP server every 15 minutes.
-  Run-Command $w32tm /config '/manualpeerlist:169.254.169.254,0x1' /syncfromflags:manual
+  Run-Command $w32tm /config '/manualpeerlist:metadata.google.internal,0x1' /syncfromflags:manual
   Start-Sleep -s 300
   Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\W32Time\TimeProviders\NtpClient' `
     -Name SpecialPollInterval -Value 900
@@ -310,7 +310,7 @@ function Setup-NTP {
   $server_key = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers'
   $server_item = Get-Item $server_key
   $server_num = ($server_item.GetValueNames() | Measure-Object -Maximum).Maximum + 1
-  Set-ItemProperty -Path $server_key -Name $server_num -Value '169.254.169.254'
+  Set-ItemProperty -Path $server_key -Name $server_num -Value 'metadata.google.internal'
   Set-ItemProperty -Path $server_key -Name '(Default)' -Value $server_num
   # Configure to run automatically on every start.
   Set-Service W32Time -StartupType Automatic
@@ -540,12 +540,12 @@ function Install-DriverPackages {
   Write-Host 'Installing GCE Driver Packages...'
   # Install each individually in order to catch individual errors
   # if 2019/2022 install GQ else Install GVNIC DQ (2016/2025) 
-  if (([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -gt 14393) -and ([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -lt 26100)) {
-    Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install -sources https://packages.cloud.google.com/yuck/repos/google-compute-engine-driver-gvnic-gq-stable google-compute-engine-driver-gvnic
-  }
-  else {
-    Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-gvnic
-  }
+  # if (([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -gt 14393) -and ([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -lt 26100)) {
+  Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install -sources https://packages.cloud.google.com/yuck/repos/google-compute-engine-driver-gvnic-gq-stable google-compute-engine-driver-gvnic
+  # }
+  # else {
+  # Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-gvnic
+  # }
   Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-vioscsi
   Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-netkvm
   Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-pvpanic
