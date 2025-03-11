1d146ad217e61b4696629254963de4d990d4124c
Brian Katyl <brian@katyl.com>
2025-03-11 10:24:09 -0600

    Revert "Revert "Move to GVNIC DQ for all windows images (#2437)" (#2439)"
    This reverts commit b397651e3f7e4ddfd216b3aed325a59867cfd345.

--- daisy_workflows/image_build/windows/post_install.ps1
+++ daisy_workflows/image_build/windows/post_install.ps1
@@ -539,13 +539,7 @@ function Configure-RDP {
 function Install-DriverPackages {
   Write-Host 'Installing GCE Driver Packages...'
   # Install each individually in order to catch individual errors
-  # if 2019/2022 install GQ else Install GVNIC DQ (2016/2025) 
-  if (([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -gt 14393) -and ([System.Environment]::OSVersion.Version.Major -ge 10 -and [System.Environment]::OSVersion.Version.Build -lt 26100)) {
-    Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install -sources https://packages.cloud.google.com/yuck/repos/google-compute-engine-driver-gvnic-gq-stable google-compute-engine-driver-gvnic
-  }
-  else {
-    Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-gvnic
-  }
+  Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-gvnic
   Start-Sleep -s 60
   Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-vioscsi
   Run-Command 'C:\ProgramData\GooGet\googet.exe' -root 'C:\ProgramData\GooGet' -noconfirm install google-compute-engine-driver-netkvm
