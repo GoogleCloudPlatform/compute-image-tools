045f9b6f16f5336b236259b0d5a458b8e8f44ae8
Hank Huang <theorem@google.com>
2026-04-28 15:26:48 -0700

    fix typo

--- scripts/find-and-deprecate.sh
+++ scripts/find-and-deprecate.sh
@@ -6,26 +6,24 @@ YAML_FILE="cli_tools_cloudbuild.yaml"
 
 echo "--- Scanning $YAML_FILE for repositories ---"
 
-declare -A UNIQUE_REPOS
+declare -A REPOS
 
-# Read file line by line
 while read -r line; do
   # Look for the destination pattern
   # Regex matches --destination= then captures the URL up to the colon
   if [[ $line =~ --destination=([^:]+) ]]; then
-    # Get the captured group
     RAW_REPO="${BASH_REMATCH[1]}"
     
     # Replace the variable with the actual project ID
     REPO_PATH="${RAW_REPO//\$PROJECT_ID/$PROJECT_ID}"
     
-    UNIQUE_REPOS["$REPO_PATH"]=1
+    REPOS["$REPO_PATH"]=1
   fi
 done < "$YAML_FILE"
 
 SCRIPT_DIR=$(dirname "$0")
 
-for REPO in "${!UNIQUE_REPOS[@]}"; do
+for REPO in "${!REPOS[@]}"; do
   # Call the deprecate-images script to deprecate
   "$SCRIPT_DIR/deprecate-images.sh" "$REPO"
 done
